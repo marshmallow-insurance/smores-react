62129dcbec2a5ffe6e4976e51e0bdaef5ed619b5
Todor Tsankov <todor.tsankov@marshmallow.co>
2019-04-05 16:40:06 +0100

    📚 add Storybook setup

---
+++ .storybook/addons.js
@@ -0,0 +1,5 @@
+import 'storybook-addon-jsx/register';
+
+import '@storybook/addon-knobs/register';
+
+import '@storybook/addon-a11y/register';
---
+++ .storybook/config.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { configure, setAddon, addDecorator } from '@storybook/react';
+
+import JSXAddon from 'storybook-addon-jsx';
+import { withInfo } from '@storybook/addon-info';
+import { withKnobs } from '@storybook/addon-knobs/react';
+import { withA11y } from '@storybook/addon-a11y';
+
+import { theme } from '../src/theme';
+import { GlobalStyle } from './global-styles'
+
+setAddon(JSXAddon);
+
+addDecorator(
+  withInfo({
+    header: false,
+  })
+);
+addDecorator(withKnobs);
+addDecorator(withA11y)
+
+// automatically import all files ending in *.stories.js
+const req = require.context('../src', true, /.stories.js$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+addDecorator((story) => (
+  <ThemeProvider theme={theme}>
+    <div>
+      <GlobalStyle />
+      {story()}
+    </div>
+  </ThemeProvider>
+))
+
+configure(loadStories, module);
