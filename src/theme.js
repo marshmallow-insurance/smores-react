0a3de545877eb4aaef2271981d58c61452e5d5de
Todor Tsankov <todor.tsankov@marshmallow.co>
2019-04-05 16:39:34 +0100

    🎨 add theme palette

---
+++ src/theme.js
@@ -0,0 +1,60 @@
+const colors = {
+  white: '#ffffff',
+
+  //Grey
+  grey8: '#636768',
+  grey7: '#818181',
+  grey6: '#9E9E9E',
+  grey5: '#AEAEAE',
+  grey4: '#C1C1C1',
+  grey3: '#E5E5E5',
+  grey2: '#ECECEC',
+
+  //Pink
+  pink8: '#AB1D20',
+  pink7: '#B5445D',
+  pink6: '#FD5064',
+  pink5: '#FF4E75',
+  pink4: '',
+  pink3: '#FF7695',
+  pink2: '#FFB2B4',
+
+  //Green
+  green8: '#16716E',
+  green7: '#469389',
+  green6: '#4CB99F',
+  green5: '#55D6AA',
+  green4: '',
+  green3: '#A7D6C6',
+  green2: '',
+
+  //Blue
+  blue8: '#05090E',
+  blue7: '#112035',
+  blue6: '#363F59',
+  blue5: '#545E7E',
+  blue4: '#6F7AA0',
+  blue3: '#94A0C6',
+  blue2: '#ACB6D8',
+
+  //Yellow
+  yellow5: '#FFD53C',
+
+  //Red
+  red7: '#D82C2C',
+  red5: '#E06161',
+  red4: '#D7796A'
+};
+
+const font = {
+  family: {
+    normal: 'Gordita, sans-serif',
+    medium: 'GorditaMedium, sans-serif',
+    bold: 'GorditaBold, sans-serif',
+  }
+}
+
+export const theme = {
+  colors,
+  font
+}
