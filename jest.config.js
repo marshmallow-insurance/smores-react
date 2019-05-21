72bdaea9f09ab4b536ef96f425090dd14dbdcab2
Todor Tsankov <todor.tsankov@marshmallow.co>
2019-05-21 12:55:00 +0100

    🍀 include test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 50,
+      lines: 80,
+      functions: 80,
+    },
+  },
   //after Jest is loaded
   setupFilesAfterEnv: [require.resolve('./test/setup-test.js')],
 };
