cbb5a6d39c4447d863857b929199f080fd8d5c62
Todor Tsankov <todor.tsankov@marshmallow.co>
2019-06-06 16:15:31 +0100

    💌 add email Icon

--- src/Icon/Icon.tsx
+++ src/Icon/Icon.tsx
@@ -19,6 +19,22 @@ type Props = {
 
 export const Icon: FC<Props> = ({render, size = 32, color = 'blue7'}) => (
   <Container size={size} color={color}>
+    {// CONTACT - EMAIL
+    render === 'contact-email' && (
+      <svg
+        width="100%"
+        height="100%"
+        viewBox="0 0 32 32"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill={theme.colors[color]}
+          d="M25 6H7C5.346 6 4 7.346 4 9v14c0 1.654 1.346 3 3 3h18c1.654 0 3-1.346 3-3V9c0-1.654-1.346-3-3-3zm1 17c0 .551-.449 1-1 1H7c-.551 0-1-.449-1-1v-9.514l7.905 3.649a5 5 0 0 0 4.19-.001L26 13.486V23zm0-11.717l-8.743 4.035a2.995 2.995 0 0 1-2.515 0L6 11.283V9c0-.551.449-1 1-1h18c.551 0 1 .449 1 1v2.283z"
+        />
+      </svg>
+    )}
+
     {// CONTACT - AT
     render === 'contact-at' && (
       <svg
