92b55e98051e685a53ab9375fd96246ffd896682
Todor Tsankov <todor.tsankov@marshmallow.co>
2019-06-19 19:14:56 +0100

    add Dropdown as an export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './Button';
 export * from './Card';
 export * from './CardContent';
+export * from './Dropdown';
 export * from './Icon';
 export * from './Text';
 export * from './TextInput';
