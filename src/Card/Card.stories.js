fff9271f4b1db19f9d86e95a2f5a1d0637d5edd8
Jerome Lapostolet <jerome.lapostolet@marshmallow.co>
2019-07-08 10:30:54 +0100

    🔀 merge CardContent and Card together (#2)
    * 🔀 Merge CardContent and Card together
    
    * adjust Card props and stories
    
    * update tests
    
    * revert margin types back to string

---
+++ src/Card/Card.stories.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+
+import {text, number} from '@storybook/addon-knobs';
+
+import {Card} from '../Card';
+import {Text} from '../Text';
+
+storiesOf('Card', module)
+  .addWithJSX('default', () => (
+    <Card>
+      <Text tag="h1">Default padding</Text>
+    </Card>
+  ))
+  .addWithJSX('max width', () => (
+    <>
+      <Card>
+        <Text tag="h3">Very very very very long card without max width</Text>
+      </Card>
+      <Card maxWidth={text('Max Width', '250px')}>
+        <Text tag="h3">Very very very very long card with max width</Text>
+      </Card>
+    </>
+  ))
+  .addWithJSX('margins', () => (
+    <>
+      <Text tag="h3">Before margin</Text>
+      <Card
+        className="container"
+        marginX={number('Margin X', '50px')}
+        marginY={number('Margin Y', '30px')}
+      >
+        <Text tag="h3">Card with margin</Text>
+      </Card>
+      <Text tag="h3">After margin</Text>
+    </>
+  ));
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export * from './Button';
 export * from './Card';
-export * from './CardContent';
 export * from './Dropdown';
 export * from './Icon';
 export * from './Text';
