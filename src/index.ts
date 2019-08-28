f67dd0b396359096fae053c866a2b336b6703397
Todor Tsankov <todor.tsankov@marshmallow.co>
2019-08-28 15:07:50 +0100

    add SearchInput as export

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export * from './Card';
 export * from './Dropdown';
 export * from './Icon';
 export * from './Loader';
+export * from './SearchInput';
 export * from './Text';
 export * from './TextInput';
