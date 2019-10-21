cb5866e97d7e9e01349afb6284edd17c8db57c91
Alex Williams <alex.williams@marshmallow.co>
2019-10-21 13:41:02 +0100

    Add flex, space and measure utilities

---
+++ utils/index.js
@@ -0,0 +1,3 @@
+export {default as flex} from './flex';
+export {default as measure} from './measure';
+export {default as space} from './space';
---
+++ utils/measure.tsx
@@ -0,0 +1,25 @@
+type MeasureFN = (arg: MeasureProps) => string;
+
+export interface MeasureProps {
+  width?: string;
+  maxWidth?: string;
+  minWidth?: string;
+  height?: string;
+  maxHeight?: string;
+  minHeight?: string;
+}
+
+const measure: MeasureFN = (props: MeasureProps) => {
+  const {width, height, maxWidth, minWidth, minHeight, maxHeight} = props;
+
+  return `
+    ${width ? `width: ${width}` : ''}
+    ${maxWidth ? `max-width: ${maxWidth}` : ''}
+    ${minWidth ? `min-width: ${minWidth}` : ''}
+    ${height ? `height: ${height}` : ''}
+    ${minHeight ? `min-height: ${minHeight}` : ''}
+    ${maxHeight ? `max-height: ${maxHeight}` : ''}
+  `;
+};
+
+export default measure;
