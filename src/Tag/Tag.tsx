73fa62e56e7844d9e95dbdbde7849fff968cf658
Alex Williams <alex.williams@marshmallow.co>
2019-10-23 10:47:27 +0100

    Add fallback font-family

--- src/Tag/Tag.tsx
+++ src/Tag/Tag.tsx
@@ -7,19 +7,6 @@ interface IWrapper {
   color: string;
 }
 
-const Wrapper = styled.div<IWrapper>`
-  font-family: GorditaRegular;
-  font-size: 14px;
-  display: inline-block;
-  background-color: ${({color}) => theme.colors[color]};
-  color: ${theme.colors.white};
-  padding: 9px 16px 8px 16px;
-  border-radius: 8px;
-  margin-bottom: 16px;
-  margin-right: 16px;
-  text-transform: capitalize;
-`;
-
 type TagProps = {
   label: string;
   color?: string;
@@ -30,3 +17,16 @@ export const Tag: FC<TagProps> = ({label, color = 'grey6'}) => (
     <span>{label}</span>
   </Wrapper>
 );
+
+const Wrapper = styled.div<IWrapper>`
+  font-family: GorditaRegular, sans-serif;
+  font-size: 14px;
+  display: inline-block;
+  background-color: ${({color}) => theme.colors[color]};
+  color: ${theme.colors.white};
+  padding: 9px 16px 8px 16px;
+  border-radius: 8px;
+  margin-bottom: 16px;
+  margin-right: 16px;
+  text-transform: capitalize;
+`;
