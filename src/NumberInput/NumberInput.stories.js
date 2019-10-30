e21959324636bb15448bde3a1a5eb259356cfbf1
Alex Williams <alex.williams@marshmallow.co>
2019-10-30 18:07:46 +0000

    Add error and disabled stories

--- src/NumberInput/NumberInput.stories.js
+++ src/NumberInput/NumberInput.stories.js
@@ -26,6 +26,18 @@ storiesOf('NumberInput', module)
       onChange={str => {}}
     />
   ))
+  .addWithJSX('error', () => (
+    <NumberInput
+      id={text('id', 'total_amount')}
+      label={text('label', 'Number')}
+      name={text('name', 'numberInput')}
+      placeholder={text('placeholder', '0')}
+      required={boolean('required', true)}
+      onChange={str => {}}
+      error={boolean('error', true)}
+      errorMsg="Oh boy, something went wrong!"
+    />
+  ))
   .addWithJSX('currency', () => (
     <NumberInput
       id={text('id', 'currency')}
@@ -60,6 +72,18 @@ storiesOf('NumberInput', module)
       prefix={text('prefix', '+44')}
       required={boolean('required', true)}
       onChange={str => {}}
+    />
+  ))
+  .addWithJSX('disabled', () => (
+    <NumberInput
+      id={text('id', 'telephone')}
+      label={text('label', 'Telephone number')}
+      name={text('name', 'telephoneNumber')}
+      placeholder={text('placeholder', ' 7123 456789')}
+      prefix={text('prefix', '+44')}
+      required={boolean('required', true)}
+      onChange={str => {}}
+      disabled={true}
     />
   ))
   .addWithJSX('functional', () => <Container />);
