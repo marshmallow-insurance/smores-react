f290979288517758a8a81314b273a39797d3385b
Alex Williams <alex.williams@marshmallow.co>
2019-11-08 15:44:32 +0000

    Update spacing on the Checkbox as per the designs (#125)
    * Update spacing on the Checkbox
    * Update tests
    * Update designs again because #consistency
    * Reformat code
    * Update tests

--- src/CheckBox/CheckBox.tsx
+++ src/CheckBox/CheckBox.tsx
@@ -1,31 +1,32 @@
-import React, {FC, ReactNode} from 'react';
-import styled from 'styled-components';
+import React, { FC, ReactNode } from 'react'
+import styled from 'styled-components'
 
-import {Text} from '../Text';
-import {theme} from '../theme';
+import { Text } from '../Text'
+import { theme } from '../theme'
 
 type Props = {
-  id: string;
-  checked: boolean;
-  children: ReactNode;
-  toggle: () => void;
-};
+  id: string
+  checked: boolean
+  children: ReactNode
+  toggle: () => void
+}
 
-export const CheckBox: FC<Props> = ({id, checked, children, toggle}) => (
+export const CheckBox: FC<Props> = ({ id, checked, children, toggle }) => (
   <BoxContainer id={id}>
     <Text tag="span" typo="base-small">
       {children}
     </Text>
+
     <input type="checkbox" checked={checked} onChange={toggle} />
     <Checkmark />
   </BoxContainer>
-);
+)
 
 const Checkmark = styled.span`
   position: absolute;
   left: 0;
-  width: 16px;
-  height: 16px;
+  width: 24px;
+  height: 24px;
   background-color: ${theme.colors.grey2};
   border: solid 1px ${theme.colors.grey6};
   border-radius: 50%;
@@ -35,35 +36,23 @@ const Checkmark = styled.span`
     content: '';
     position: absolute;
     display: none;
-    top: 2px;
-    left: 5px;
-    width: 3px;
-    height: 6px;
+    top: 4px;
+    left: 8px;
+    width: 4px;
+    height: 9px;
     border: solid white;
     border-width: 0 2px 2px 0;
     -webkit-transform: rotate(45deg);
     -ms-transform: rotate(45deg);
     transform: rotate(45deg);
   }
-
-  @media (min-width: 768px) {
-    width: 24px;
-    height: 24px;
-
-    &:after {
-      top: 4px;
-      left: 8px;
-      width: 4px;
-      height: 9px;
-    }
-  }
-`;
+`
 
 const BoxContainer = styled.label`
   position: relative;
   display: flex;
   align-items: center;
-  padding-left: 24px;
+  padding-left: 32px;
   user-select: none;
   cursor: pointer;
 
@@ -90,4 +79,4 @@ const BoxContainer = styled.label`
   @media (min-width: 768px) {
     padding-left: 32px;
   }
-`;
+`
