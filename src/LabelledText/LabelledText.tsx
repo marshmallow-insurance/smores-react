6c530e61e3a321d886d2271f51fe48a723d31b8a
Alex Williams <alex.williams@marshmallow.co>
2020-01-02 16:03:07 +0000

    Add LabelledText component

---
+++ src/LabelledText/LabelledText.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react'
+import styled from 'styled-components'
+
+import { Text } from '../Text'
+
+type Props = {
+  children: any
+  label: string
+}
+
+export const LabelledText: FC<Props> = ({ label, children }) => (
+  <Container>
+    <Text tag="label" color="grey6" typo="label">
+      {label}
+    </Text>
+    {children}
+  </Container>
+)
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  label {
+    margin-bottom: 8px;
+  }
+`
