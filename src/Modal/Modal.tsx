7d040396b8e8a08c8a0a916bf3b6c71663c58967
githab <hab.kim@marshmallow.co>
2020-11-13 15:55:28 +0000

    minor css change for container

--- src/Modal/Modal.tsx
+++ src/Modal/Modal.tsx
@@ -16,11 +16,11 @@ interface IModalContainer {
 }
 
 type ModalProps = {
-  title?: string,
-  children?: ReactNode,
-  showModal?: boolean,
-  handleClick: () => void,
-  drawer?: boolean,
+  title?: string
+  children?: ReactNode
+  showModal?: boolean
+  handleClick: () => void
+  drawer?: boolean
   cross?: boolean
 }
 
@@ -30,32 +30,35 @@ export const Modal: FC<ModalProps> = ({
   showModal = false,
   handleClick,
   drawer = true,
-  cross = true
-}) => 
+  cross = true,
+}) => (
   <Wrapper showModal={showModal}>
     <Overlay />
     <Container drawer={drawer}>
       <Box flex alignItems="center" justifyContent="space-between">
-        <Text tag="h2" typo="header-small" align="left">{title}</Text>
-        {cross && 
+        <Text tag="h2" typo="header-small" align="left">
+          {title}
+        </Text>
+        {cross && (
           <IconContainer onClick={handleClick}>
             <Icon render="cross" color="blue7" size={32} />
           </IconContainer>
-        }
+        )}
       </Box>
       <Text tag="p" typo="desc-small" color="grey8" align="left">
         {children}
       </Text>
     </Container>
-</Wrapper>
+  </Wrapper>
+)
 
 const Wrapper = styled(Box)<IModalWrapper>(
-  ({showModal}) => css`
-    display: ${showModal ? 'block' : 'none'};
-    position: relative;
+  ({ showModal }) => css`
+    display: ${showModal ? 'contents' : 'none'};
+    position: absolute;
     height: calc(100vh);
     width: 100%;
-  `
+  `,
 )
 
 const Overlay = styled.div`
@@ -67,7 +70,7 @@ const Overlay = styled.div`
 `
 
 const Container = styled.div<IModalContainer>(
-  ({drawer}) => css`
+  ({ drawer }) => css`
     background: ${theme.colors.white};
     z-index: 999;
     position: absolute;
@@ -78,23 +81,22 @@ const Container = styled.div<IModalContainer>(
     max-width: 456px;
     left: calc(50% - 460px / 2);
     top: calc(50% - 265px / 2 + 0.5px);
-    
+
     ${drawer === true &&
-      css`
-        @media (max-width: 768px) {
-          max-width: none;
-          width: 100%;
-          left: 0;
-          border-radius: 16px 16px 0px 0px;
-          padding: 10% 24px;
-          overflow: scroll;
-          max-height: 90vh;
-          height: 70vh;
-          top: 30vh;
-        }
-      `
-    }
-  `
+    css`
+      @media (max-width: 768px) {
+        max-width: none;
+        width: 100%;
+        left: 0;
+        border-radius: 16px 16px 0px 0px;
+        padding: 10% 24px;
+        overflow: scroll;
+        max-height: 90vh;
+        height: 70vh;
+        top: 30vh;
+      }
+    `}
+  `,
 )
 
 const IconContainer = styled.div`
