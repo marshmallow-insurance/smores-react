323984e8a4cd41eda5935ef736b9662758f7397a
Francesco Leonardi <francesco.leonardi@marshmallow.co>
2021-06-14 16:32:42 +0100

    added import icons workflow for initial test (#1238)
    * added import icons workflow for initial test
    
    * added fix for storybook trim breaking the build with new typescript

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,10 @@
 module.exports = {
   stories: ['../src/**/*.stories.js'],
   addons: ['@storybook/addon-essentials'],
+  // This is needed until storybook will release a fix
+  // because it's breaking the build
+  //https://github.com/styleguidist/react-docgen-typescript/issues/356
+  typescript: {
+    reactDocgen: 'react-docgen',
+  },
 }
