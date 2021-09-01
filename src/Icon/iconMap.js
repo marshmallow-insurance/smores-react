2ac06f0c77378754e72fd90bd4532fb4f1fa07a3
Francesco Leonardi <francesco.leonardi@marshmallow.co>
2021-09-01 14:54:52 +0100

    fixed icon build (#1379)

---
+++ src/Icon/iconMap.js
@@ -0,0 +1,349 @@
+import { ReactComponent as Aa } from '../assets/icons/aa.svg'
+import { ReactComponent as AccountBreakdown } from '../assets/icons/account-breakdown.svg'
+import { ReactComponent as AccountMlp } from '../assets/icons/account-mlp.svg'
+import { ReactComponent as ActivityClipboard } from '../assets/icons/activity-clipboard.svg'
+import { ReactComponent as AddOns } from '../assets/icons/add-ons.svg'
+import { ReactComponent as Addons } from '../assets/icons/addons.svg'
+import { ReactComponent as AddressMta } from '../assets/icons/address-mta.svg'
+import { ReactComponent as AlertEngine } from '../assets/icons/alert-engine.svg'
+import { ReactComponent as Alert } from '../assets/icons/alert.svg'
+import { ReactComponent as Amex } from '../assets/icons/amex.svg'
+import { ReactComponent as ApplePay } from '../assets/icons/apple pay.svg'
+import { ReactComponent as ApprovedFile } from '../assets/icons/approved-file.svg'
+import { ReactComponent as ArrowDown } from '../assets/icons/arrow-down.svg'
+import { ReactComponent as ArrowRight } from '../assets/icons/arrow-right.svg'
+import { ReactComponent as ArrowUp } from '../assets/icons/arrow-up.svg'
+import { ReactComponent as Arrow } from '../assets/icons/arrow.svg'
+import { ReactComponent as At } from '../assets/icons/at.svg'
+import { ReactComponent as Axa } from '../assets/icons/axa.svg'
+import { ReactComponent as Bank } from '../assets/icons/bank.svg'
+import { ReactComponent as Bicycle } from '../assets/icons/bicycle.svg'
+import { ReactComponent as Boost } from '../assets/icons/boost.svg'
+import { ReactComponent as BreakdownAlt } from '../assets/icons/breakdown-alt.svg'
+import { ReactComponent as Breakdown } from '../assets/icons/breakdown.svg'
+import { ReactComponent as Briefcase } from '../assets/icons/briefcase.svg'
+import { ReactComponent as Burger } from '../assets/icons/burger.svg'
+import { ReactComponent as Calendar } from '../assets/icons/calendar.svg'
+import { ReactComponent as Camera } from '../assets/icons/camera.svg'
+import { ReactComponent as Cancel } from '../assets/icons/cancel.svg'
+import { ReactComponent as CarMta } from '../assets/icons/car-mta.svg'
+import { ReactComponent as CarUsage } from '../assets/icons/car-usage.svg'
+import { ReactComponent as Car } from '../assets/icons/car.svg'
+import { ReactComponent as Card } from '../assets/icons/card.svg'
+import { ReactComponent as CaretLeft } from '../assets/icons/caret-left.svg'
+import { ReactComponent as CaretRight } from '../assets/icons/caret-right.svg'
+import { ReactComponent as CaretUp } from '../assets/icons/caret-up.svg'
+import { ReactComponent as Caret } from '../assets/icons/caret.svg'
+import { ReactComponent as Charge } from '../assets/icons/charge.svg'
+import { ReactComponent as ChatBubble } from '../assets/icons/chat-bubble.svg'
+import { ReactComponent as Chat } from '../assets/icons/chat.svg'
+import { ReactComponent as Checkout } from '../assets/icons/checkout.svg'
+import { ReactComponent as City } from '../assets/icons/city.svg'
+import { ReactComponent as Claim } from '../assets/icons/claim.svg'
+import { ReactComponent as ClipboardCheck } from '../assets/icons/clipboard-check.svg'
+import { ReactComponent as Clock } from '../assets/icons/clock.svg'
+import { ReactComponent as ContactEmail } from '../assets/icons/contact-email.svg'
+import { ReactComponent as Copy } from '../assets/icons/copy.svg'
+import { ReactComponent as Crop } from '../assets/icons/crop.svg'
+import { ReactComponent as Cross } from '../assets/icons/cross.svg'
+import { ReactComponent as DashboardHigh } from '../assets/icons/dashboard-high.svg'
+import { ReactComponent as DirectDebit } from '../assets/icons/direct-debit.svg'
+import { ReactComponent as DocDownload } from '../assets/icons/doc-download.svg'
+import { ReactComponent as DocUpload } from '../assets/icons/doc-upload.svg'
+import { ReactComponent as Download } from '../assets/icons/download.svg'
+import { ReactComponent as Earth } from '../assets/icons/earth.svg'
+import { ReactComponent as Edit } from '../assets/icons/edit.svg'
+import { ReactComponent as EmailFilled } from '../assets/icons/email-filled.svg'
+import { ReactComponent as Engine } from '../assets/icons/engine.svg'
+import { ReactComponent as Europe } from '../assets/icons/europe.svg'
+import { ReactComponent as Expand } from '../assets/icons/expand.svg'
+import { ReactComponent as Facebook } from '../assets/icons/facebook.svg'
+import { ReactComponent as Fence } from '../assets/icons/fence.svg'
+import { ReactComponent as FileBadge2 } from '../assets/icons/file-badge-2.svg'
+import { ReactComponent as FileCabinet } from '../assets/icons/file-cabinet.svg'
+import { ReactComponent as File } from '../assets/icons/file.svg'
+import { ReactComponent as Fire } from '../assets/icons/fire.svg'
+import { ReactComponent as Flag } from '../assets/icons/flag.svg'
+import { ReactComponent as Folder } from '../assets/icons/folder.svg'
+import { ReactComponent as FraudAlert } from '../assets/icons/fraud-alert.svg'
+import { ReactComponent as Fuel } from '../assets/icons/fuel.svg'
+import { ReactComponent as Globe } from '../assets/icons/globe.svg'
+import { ReactComponent as GooglePlayBadge } from '../assets/icons/google-play-badge.svg'
+import { ReactComponent as Gpay } from '../assets/icons/gpay.svg'
+import { ReactComponent as HelpAboutus } from '../assets/icons/help-aboutus.svg'
+import { ReactComponent as HelpBuypolicy } from '../assets/icons/help-buypolicy.svg'
+import { ReactComponent as HelpCenter } from '../assets/icons/help-center.svg'
+import { ReactComponent as HelpIdcheck } from '../assets/icons/help-idcheck.svg'
+import { ReactComponent as HelpManagePolicy } from '../assets/icons/help-manage-policy.svg'
+import { ReactComponent as HelpNcd } from '../assets/icons/help-ncd.svg'
+import { ReactComponent as HelpVerification } from '../assets/icons/help-verification.svg'
+import { ReactComponent as HouseTree } from '../assets/icons/house-tree.svg'
+import { ReactComponent as House } from '../assets/icons/house.svg'
+import { ReactComponent as Id } from '../assets/icons/id.svg'
+import { ReactComponent as Info } from '../assets/icons/info.svg'
+import { ReactComponent as Injury } from '../assets/icons/injury.svg'
+import { ReactComponent as Instagram } from '../assets/icons/instagram.svg'
+import { ReactComponent as Intercom } from '../assets/icons/intercom.svg'
+import { ReactComponent as Intercom2 } from '../assets/icons/intercom2.svg'
+import { ReactComponent as IosStoreBadge } from '../assets/icons/ios-store-badge.svg'
+import { ReactComponent as Iphone } from '../assets/icons/iphone.svg'
+import { ReactComponent as Key } from '../assets/icons/key.svg'
+import { ReactComponent as Laptop } from '../assets/icons/laptop.svg'
+import { ReactComponent as Lightbulb } from '../assets/icons/lightbulb.svg'
+import { ReactComponent as Link } from '../assets/icons/link.svg'
+import { ReactComponent as Linkedin } from '../assets/icons/linkedin.svg'
+import { ReactComponent as LiveChat } from '../assets/icons/live-chat.svg'
+import { ReactComponent as LiveChat2 } from '../assets/icons/live-chat2.svg'
+import { ReactComponent as Location } from '../assets/icons/location.svg'
+import { ReactComponent as LogOut } from '../assets/icons/log-out.svg'
+import { ReactComponent as ManageDrivers } from '../assets/icons/manage-drivers.svg'
+import { ReactComponent as ManagePolicy } from '../assets/icons/manage-policy.svg'
+import { ReactComponent as Marshmallow } from '../assets/icons/marshmallow.svg'
+import { ReactComponent as Mastercard } from '../assets/icons/mastercard.svg'
+import { ReactComponent as Medical } from '../assets/icons/medical.svg'
+import { ReactComponent as MinusCircle } from '../assets/icons/minus-circle.svg'
+import { ReactComponent as Minus } from '../assets/icons/minus.svg'
+import { ReactComponent as MoneyBag } from '../assets/icons/money-bag.svg'
+import { ReactComponent as MoneyHeart } from '../assets/icons/money-heart.svg'
+import { ReactComponent as MoreDots } from '../assets/icons/more-dots.svg'
+import { ReactComponent as Mulsanne } from '../assets/icons/mulsanne.svg'
+import { ReactComponent as NewWindow } from '../assets/icons/new-window.svg'
+import { ReactComponent as NoExcess } from '../assets/icons/no-excess.svg'
+import { ReactComponent as Notification } from '../assets/icons/notification.svg'
+import { ReactComponent as Onfido } from '../assets/icons/onfido.svg'
+import { ReactComponent as PadlockOutline } from '../assets/icons/padlock-outline.svg'
+import { ReactComponent as Padlock } from '../assets/icons/padlock.svg'
+import { ReactComponent as People } from '../assets/icons/people.svg'
+import { ReactComponent as Person } from '../assets/icons/person.svg'
+import { ReactComponent as PhoneFilled } from '../assets/icons/phone-filled.svg'
+import { ReactComponent as Photo } from '../assets/icons/photo.svg'
+import { ReactComponent as PinCode } from '../assets/icons/pin-code.svg'
+import { ReactComponent as PlusCircle } from '../assets/icons/plus-circle.svg'
+import { ReactComponent as Plus } from '../assets/icons/plus.svg'
+import { ReactComponent as PolicyDetails } from '../assets/icons/policy-details.svg'
+import { ReactComponent as PolicyDoc } from '../assets/icons/policy-doc.svg'
+import { ReactComponent as PolicyDocs } from '../assets/icons/policy-docs.svg'
+import { ReactComponent as Policy } from '../assets/icons/policy.svg'
+import { ReactComponent as PoundBold } from '../assets/icons/pound-bold.svg'
+import { ReactComponent as PoundFilledCircle } from '../assets/icons/pound-filled-circle.svg'
+import { ReactComponent as PoundMedium } from '../assets/icons/pound-medium.svg'
+import { ReactComponent as PoundOutline } from '../assets/icons/pound-outline.svg'
+import { ReactComponent as PoundRegular } from '../assets/icons/pound-regular.svg'
+import { ReactComponent as Premfina } from '../assets/icons/premfina.svg'
+import { ReactComponent as Profile } from '../assets/icons/profile.svg'
+import { ReactComponent as Prohibited } from '../assets/icons/prohibited.svg'
+import { ReactComponent as Property } from '../assets/icons/property.svg'
+import { ReactComponent as QuestionHelp } from '../assets/icons/question-help.svg'
+import { ReactComponent as Rac } from '../assets/icons/rac.svg'
+import { ReactComponent as Ravelin } from '../assets/icons/ravelin.svg'
+import { ReactComponent as ReferAFriend } from '../assets/icons/refer-a-friend.svg'
+import { ReactComponent as Refund } from '../assets/icons/refund.svg'
+import { ReactComponent as RegPlate } from '../assets/icons/reg-plate.svg'
+import { ReactComponent as Renewal } from '../assets/icons/renewal.svg'
+import { ReactComponent as Return } from '../assets/icons/return.svg'
+import { ReactComponent as ScheduleQuote } from '../assets/icons/schedule-quote.svg'
+import { ReactComponent as Search } from '../assets/icons/search.svg'
+import { ReactComponent as Security } from '../assets/icons/security.svg'
+import { ReactComponent as SendMessage } from '../assets/icons/send-message.svg'
+import { ReactComponent as Settings } from '../assets/icons/settings.svg'
+import { ReactComponent as ShieldCheck } from '../assets/icons/shield-check.svg'
+import { ReactComponent as Shield } from '../assets/icons/shield.svg'
+import { ReactComponent as Shopping } from '../assets/icons/shopping.svg'
+import { ReactComponent as Sofa } from '../assets/icons/sofa.svg'
+import { ReactComponent as Stack } from '../assets/icons/stack.svg'
+import { ReactComponent as Stripe } from '../assets/icons/stripe.svg'
+import { ReactComponent as ThumbsUp } from '../assets/icons/thumbs-up.svg'
+import { ReactComponent as Tick } from '../assets/icons/tick.svg'
+import { ReactComponent as TowTruck } from '../assets/icons/tow-truck.svg'
+import { ReactComponent as Training } from '../assets/icons/training.svg'
+import { ReactComponent as Trash } from '../assets/icons/trash.svg'
+import { ReactComponent as Twitter } from '../assets/icons/twitter.svg'
+import { ReactComponent as UpDown } from '../assets/icons/up-down.svg'
+import { ReactComponent as UploadDocuments } from '../assets/icons/upload-documents.svg'
+import { ReactComponent as UploadEmail } from '../assets/icons/upload-email.svg'
+import { ReactComponent as Upload } from '../assets/icons/upload.svg'
+import { ReactComponent as Village } from '../assets/icons/village.svg'
+import { ReactComponent as Visa } from '../assets/icons/visa.svg'
+import { ReactComponent as Warning } from '../assets/icons/warning.svg'
+import { ReactComponent as Wave } from '../assets/icons/wave.svg'
+import { ReactComponent as Wellbeing } from '../assets/icons/wellbeing.svg'
+import { ReactComponent as WheelFlat } from '../assets/icons/wheel-flat.svg'
+import { ReactComponent as WindscreenRepair } from '../assets/icons/windscreen-repair.svg'
+import { ReactComponent as WindscreenReplacement } from '../assets/icons/windscreen-replacement.svg'
+import { ReactComponent as Windscreen } from '../assets/icons/windscreen.svg'
+
+export default {
+  aa: Aa,
+  'account-breakdown': AccountBreakdown,
+  'account-mlp': AccountMlp,
+  'activity-clipboard': ActivityClipboard,
+  'add-ons': AddOns,
+  addons: Addons,
+  'address-mta': AddressMta,
+  'alert-engine': AlertEngine,
+  alert: Alert,
+  amex: Amex,
+  'apple pay': ApplePay,
+  'approved-file': ApprovedFile,
+  'arrow-down': ArrowDown,
+  'arrow-right': ArrowRight,
+  'arrow-up': ArrowUp,
+  arrow: Arrow,
+  at: At,
+  axa: Axa,
+  bank: Bank,
+  bicycle: Bicycle,
+  boost: Boost,
+  'breakdown-alt': BreakdownAlt,
+  breakdown: Breakdown,
+  briefcase: Briefcase,
+  burger: Burger,
+  calendar: Calendar,
+  camera: Camera,
+  cancel: Cancel,
+  'car-mta': CarMta,
+  'car-usage': CarUsage,
+  car: Car,
+  card: Card,
+  'caret-left': CaretLeft,
+  'caret-right': CaretRight,
+  'caret-up': CaretUp,
+  caret: Caret,
+  charge: Charge,
+  'chat-bubble': ChatBubble,
+  chat: Chat,
+  checkout: Checkout,
+  city: City,
+  claim: Claim,
+  'clipboard-check': ClipboardCheck,
+  clock: Clock,
+  'contact-email': ContactEmail,
+  copy: Copy,
+  crop: Crop,
+  cross: Cross,
+  'dashboard-high': DashboardHigh,
+  'direct-debit': DirectDebit,
+  'doc-download': DocDownload,
+  'doc-upload': DocUpload,
+  download: Download,
+  earth: Earth,
+  edit: Edit,
+  'email-filled': EmailFilled,
+  engine: Engine,
+  europe: Europe,
+  expand: Expand,
+  facebook: Facebook,
+  fence: Fence,
+  'file-badge-2': FileBadge2,
+  'file-cabinet': FileCabinet,
+  file: File,
+  fire: Fire,
+  flag: Flag,
+  folder: Folder,
+  'fraud-alert': FraudAlert,
+  fuel: Fuel,
+  globe: Globe,
+  'google-play-badge': GooglePlayBadge,
+  gpay: Gpay,
+  'help-aboutus': HelpAboutus,
+  'help-buypolicy': HelpBuypolicy,
+  'help-center': HelpCenter,
+  'help-idcheck': HelpIdcheck,
+  'help-manage-policy': HelpManagePolicy,
+  'help-ncd': HelpNcd,
+  'help-verification': HelpVerification,
+  'house-tree': HouseTree,
+  house: House,
+  id: Id,
+  info: Info,
+  injury: Injury,
+  instagram: Instagram,
+  intercom: Intercom,
+  intercom2: Intercom2,
+  'ios-store-badge': IosStoreBadge,
+  iphone: Iphone,
+  key: Key,
+  laptop: Laptop,
+  lightbulb: Lightbulb,
+  link: Link,
+  linkedin: Linkedin,
+  'live-chat': LiveChat,
+  'live-chat2': LiveChat2,
+  location: Location,
+  'log-out': LogOut,
+  'manage-drivers': ManageDrivers,
+  'manage-policy': ManagePolicy,
+  marshmallow: Marshmallow,
+  mastercard: Mastercard,
+  medical: Medical,
+  'minus-circle': MinusCircle,
+  minus: Minus,
+  'money-bag': MoneyBag,
+  'money-heart': MoneyHeart,
+  'more-dots': MoreDots,
+  mulsanne: Mulsanne,
+  'new-window': NewWindow,
+  'no-excess': NoExcess,
+  notification: Notification,
+  onfido: Onfido,
+  'padlock-outline': PadlockOutline,
+  padlock: Padlock,
+  people: People,
+  person: Person,
+  'phone-filled': PhoneFilled,
+  photo: Photo,
+  'pin-code': PinCode,
+  'plus-circle': PlusCircle,
+  plus: Plus,
+  'policy-details': PolicyDetails,
+  'policy-doc': PolicyDoc,
+  'policy-docs': PolicyDocs,
+  policy: Policy,
+  'pound-bold': PoundBold,
+  'pound-filled-circle': PoundFilledCircle,
+  'pound-medium': PoundMedium,
+  'pound-outline': PoundOutline,
+  'pound-regular': PoundRegular,
+  premfina: Premfina,
+  profile: Profile,
+  prohibited: Prohibited,
+  property: Property,
+  'question-help': QuestionHelp,
+  rac: Rac,
+  ravelin: Ravelin,
+  'refer-a-friend': ReferAFriend,
+  refund: Refund,
+  'reg-plate': RegPlate,
+  renewal: Renewal,
+  return: Return,
+  'schedule-quote': ScheduleQuote,
+  search: Search,
+  security: Security,
+  'send-message': SendMessage,
+  settings: Settings,
+  'shield-check': ShieldCheck,
+  shield: Shield,
+  shopping: Shopping,
+  sofa: Sofa,
+  stack: Stack,
+  stripe: Stripe,
+  'thumbs-up': ThumbsUp,
+  tick: Tick,
+  'tow-truck': TowTruck,
+  training: Training,
+  trash: Trash,
+  twitter: Twitter,
+  'up-down': UpDown,
+  'upload-documents': UploadDocuments,
+  'upload-email': UploadEmail,
+  upload: Upload,
+  village: Village,
+  visa: Visa,
+  warning: Warning,
+  wave: Wave,
+  wellbeing: Wellbeing,
+  'wheel-flat': WheelFlat,
+  'windscreen-repair': WindscreenRepair,
+  'windscreen-replacement': WindscreenReplacement,
+  windscreen: Windscreen,
+}
