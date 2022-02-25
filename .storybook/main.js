e01d0003dee20aeb215b50b5a022a9e4a8f59a05
Mickael Zhang <mickael.zhang@marshmallow.co>
2022-02-25 10:30:02 +0000

    Storybook - add performance add-on (#1771)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,9 @@
 module.exports = {
   stories: ['../src/**/*.stories.js'],
-  addons: ['@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-essentials',
+    'storybook-addon-performance/register',
+  ],
   // This is needed until storybook will release a fix
   // because it's breaking the build
   //https://github.com/styleguidist/react-docgen-typescript/issues/356
