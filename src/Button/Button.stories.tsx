d25e87fd846a1fc80c40ce243ae77f09c18943ef
Mickael Zhang <mickael.zhang@marshmallow.co>
2022-04-20 15:44:11 +0100

    update button story

--- src/Button/Button.stories.tsx
+++ src/Button/Button.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { Button, ButtonProps } from './Button'
 import { InteractivePlayground } from './InteractivePlayground'
+import { CollectionPage } from './Collection'
 
 export default {
   title: 'Button',
@@ -16,80 +17,11 @@ const InteractivePlaygroundTemplate = (props: ButtonProps) => (
 )
 
 export const Interactive = InteractivePlaygroundTemplate.bind({})
+export const Collection = CollectionPage.bind({})
 
-export const Primary = Template.bind({})
+export const ForcedWidth = Template.bind({})
 
-Primary.args = {
-  primary: true,
-  'data-testid': 'test',
-  'aria-label': 'primary-button',
-}
-
-export const PrimaryDisabled = Template.bind({})
-
-PrimaryDisabled.args = {
-  primary: true,
-  disabled: true,
-  'aria-disabled': 'true',
-}
-
-export const PrimaryLoading = Template.bind({})
-
-PrimaryLoading.args = {
-  primary: true,
-  loading: true,
-}
-
-export const PrimaryForcedWidth = Template.bind({})
-
-PrimaryForcedWidth.args = {
+ForcedWidth.args = {
   primary: true,
   forcedWidth: '300px',
-}
-
-export const Secondary = Template.bind({})
-
-Secondary.args = {
-  secondary: true,
-}
-
-export const SecondaryDisabled = Template.bind({})
-
-SecondaryDisabled.args = {
-  secondary: true,
-  disabled: true,
-}
-
-export const SecondaryLoading = Template.bind({})
-
-SecondaryLoading.args = {
-  secondary: true,
-  loading: true,
-}
-
-export const Tertiary = Template.bind({})
-
-Tertiary.args = {
-  tertiary: true,
-}
-
-export const TertiaryDisabled = Template.bind({})
-
-TertiaryDisabled.args = {
-  tertiary: true,
-  disabled: true,
-}
-
-export const TertiaryLoading = Template.bind({})
-
-TertiaryLoading.args = {
-  tertiary: true,
-  loading: true,
-}
-
-export const Icon = Template.bind({})
-
-Icon.args = {
-  primary: true,
-  icon: 'info',
 }
