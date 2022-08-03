c42547d80ef302892132d87ca1ab7d2f77804aa7
John <hi@johnmuskett.com>
2022-08-03 14:29:53 +0100

    add story for assistive text

--- src/TextInput/TextInput.stories.tsx
+++ src/TextInput/TextInput.stories.tsx
@@ -140,6 +140,21 @@ Required.args = {
   required: true,
 }
 
+export const AssistiveText = Template.bind({})
+
+AssistiveText.args = {
+  id: 'textInput',
+  name: 'textInput',
+  placeholder: 'Enter name',
+  onChange: noop,
+  onInputChange: noop,
+  onBlur: noop,
+  label: 'Please enter your name',
+  outlined: true,
+  required: true,
+  assistiveText: 'Some more information...',
+}
+
 const WorkingExampleTemplate = () => <Container />
 
 export const WorkingExample = WorkingExampleTemplate.bind({})
