e3ab1b2039e2b8bd9b76c4f4c6855197265bfa8d
Francesco Leonardi <francesco.leonardi@marshmallow.co>
2023-02-06 16:35:34 +0000

    cleaned up extra describe

--- src/hooks/useScript/useScript.test.ts
+++ src/hooks/useScript/useScript.test.ts
@@ -2,22 +2,20 @@ import { renderHook, cleanup } from '@testing-library/react'
 import { useScript } from '.'
 
 describe('useScript', () => {
-  describe('append script', () => {
-    beforeEach(() => {
-      renderHook(() =>
-        useScript({
-          url: 'test',
-        }),
-      )
-    })
+  beforeEach(() => {
+    renderHook(() =>
+      useScript({
+        url: 'test',
+      }),
+    )
+  })
 
-    afterEach(() => {
-      cleanup()
-    })
+  afterEach(() => {
+    cleanup()
+  })
 
-    it('should append the script with the correct url to the body', () => {
-      const script = document.querySelector('script[src="test"]')
-      return expect(script).toBeInTheDocument()
-    })
+  it('should append the script with the correct url to the body', () => {
+    const script = document.querySelector('script[src="test"]')
+    return expect(script).toBeInTheDocument()
   })
 })
