907b179867258ca45b3929199c81b4a944491dd3
Francesco Leonardi <francesco.leonardi@marshmallow.co>
2023-03-07 10:16:27 +0000

    Revert "Modal animation" (#2571)
    Revert "Modal animation (#2559)"
    
    This reverts commit 1e8dda5ad2d3fab614df23ac8f5e97b77fa05c51.

--- src/Modal/Modal.tsx
+++ src/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode, useEffect, useRef, useState } from 'react'
+import React, { FC, ReactNode, useRef } from 'react'
 import styled, { css } from 'styled-components'
 import { theme } from '../theme'
 import { createPortal } from 'react-dom'
@@ -17,8 +17,6 @@ interface IModalContainer {
   drawer: boolean
   // modal width
   width: string
-  // showModal state
-  showModal: boolean
 }
 
 export type ModalProps = {
@@ -47,29 +45,13 @@ export const Modal: FC<ModalProps> = ({
   portalContainer = document.body,
 }) => {
   const modalRef = useRef<HTMLDivElement>(null)
-  const hasOpened = useRef(false)
-  const [opened, setOpened] = useState(false)
 
   useBodyScrollLock({ node: modalRef.current, showModal })
 
-  useEffect(() => {
-    if (modalRef.current === null) return
-
-    if (!hasOpened.current && showModal) {
-      hasOpened.current = true
-      setOpened(true)
-    }
-  }, [showModal])
-
   return createPortal(
-    <Wrapper
-      showModal={showModal}
-      ref={modalRef}
-      className={opened ? 'opened' : ''}
-    >
-      <Overlay showModal={showModal} onClick={handleClick} />
+    <Wrapper showModal={showModal} ref={modalRef}>
+      <Overlay onClick={handleClick} />
       <Container
-        showModal={showModal}
         drawer={drawer}
         width={width || '460px'}
         className={containerClass}
@@ -105,7 +87,7 @@ export const Modal: FC<ModalProps> = ({
 
 const Wrapper = styled(Box)<IModalWrapper>(
   ({ showModal }) => css`
-    display: flex;
+    display: ${showModal ? 'flex' : 'none'};
     position: absolute;
     z-index: 999;
     top: 0;
@@ -114,85 +96,22 @@ const Wrapper = styled(Box)<IModalWrapper>(
     width: 100%;
     justify-content: center;
     align-items: center;
-    transform: scale(0);
-    animation: ${showModal ? '' : 'quickScaleDown 0s .5s linear forwards'};
-
-    &.opened {
-      transform: scale(1);
-    }
-
-    @keyframes fadeIn {
-      0% {
-        opacity 0;
-      }
-      100% {
-        opacity: 0.4;
-      }
-    }
-
-    @keyframes fadeOut {
-      0% {
-        opacity: 0.4;
-      }
-      100% {
-        opacity 0;
-      }
-    }
-
-    @keyframes scaleUp {
-      0% {
-        transform: scale(0.8) translateY(1000px);
-        opacity: 0;
-      }
-      100% {
-        transform: scale(1) translateY(0px);
-        opacity: 1;
-      }
-    }
-
-    @keyframes scaleDown {
-      0% {
-        transform: scale(1) translateY(0px);
-        opacity: 1;
-      }
-      100% {
-        transform: scale(0.8) translateY(1000px);
-        opacity: 0;
-      }
-    }
-
-    @keyframes quickScaleDown {
-      0% {
-        transform: scale(1);
-      }
-      99.9% {
-        transform: scale(1);
-      }
-      100% {
-        transform: scale(0);
-      }
-    }
   `,
 )
 
-const Overlay = styled.div<IModalWrapper>(
-  ({ showModal }) => css`
-    position: fixed;
-    background: ${theme.colors.secondary};
-    cursor: pointer;
-    opacity: 0;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    animation: ${showModal
-      ? `fadeIn .5s cubic-bezier(0.165, 0.840, 0.440, 1.000) forwards`
-      : `fadeOut .5s cubic-bezier(0.165, 0.840, 0.440, 1.000) forwards`};
-  `,
-)
+const Overlay = styled.div`
+  position: fixed;
+  background: ${theme.colors.secondary};
+  cursor: pointer;
+  opacity: 0.4;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+`
 
 const Container = styled.div<IModalContainer>(
-  ({ drawer, width, showModal }) => css`
+  ({ drawer, width }) => css`
     background: ${theme.colors.white};
     border: 1px solid ${theme.colors.outline};
     box-sizing: border-box;
@@ -203,11 +122,6 @@ const Container = styled.div<IModalContainer>(
     position: fixed;
     max-height: calc(100vh - 64px);
     overflow: auto;
-    opacity: 0;
-
-    animation: ${showModal
-      ? `scaleUp .5s cubic-bezier(0.165, 0.840, 0.440, 1.000) forwards;`
-      : `scaleDown .5s cubic-bezier(0.165, 0.840, 0.440, 1.000) forwards`};
 
     ${drawer === true &&
     css`
