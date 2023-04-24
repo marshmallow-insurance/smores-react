ea607a2c433b5078e02f9e64e2efb58e42509535
Francesco Leonardi <francesco.leonardi@marshmallow.co>
2023-04-24 08:56:26 +0100

    Migrate Storybook to V7

---
+++ .storybook/main.ts
@@ -0,0 +1,19 @@
+import type { StorybookConfig } from '@storybook/react-webpack5'
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.tsx'],
+  addons: ['@storybook/addon-essentials'],
+  features: {
+    storyStoreV7: true,
+  },
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+    defaultName: 'Documentation',
+  },
+}
+
+export default config
