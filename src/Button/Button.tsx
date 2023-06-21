9da4955be77c9ab3f4f30a219039282f9f9c1fa1
Ella Jay Taylor <ella.taylor@marshmallow.co>
2023-06-21 17:48:14 +0100

    Add new colours and update snapshot

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,11 +1,9 @@
 import React, { FC, ReactNode, ButtonHTMLAttributes, forwardRef } from 'react'
 import styled, { css } from 'styled-components'
-import { darken } from 'polished'
 
-import { Color, theme } from '../theme'
+import { theme } from '../theme'
 import { focusOutline } from '../utils/focusOutline'
 import { useDeprecatedWarning } from '../utils/deprecated'
-import { LegacyButton } from './LegacyButton'
 import { Box } from '../Box'
 import { Loader } from '../Loader'
 import { Icon as IconComponent } from '../Icon'
@@ -15,16 +13,13 @@ type Props = {
   children: ReactNode
   id?: string
   className?: string
-  color?: Color
-  block?: boolean
-  inverted?: boolean
   disabled?: boolean
-  outlined?: boolean
   handleClick?: (e: React.FormEvent<HTMLButtonElement>) => void
   loading?: boolean
   primary?: boolean
   secondary?: boolean
   tertiary?: boolean
+  fallback?: boolean
   icon?: string
   forcedWidth?: string
   form?: string
@@ -42,16 +37,13 @@ export const Button: FC<ButtonProps> = forwardRef<
     children,
     id,
     className = '',
-    color = 'liquorice',
-    block = false,
-    inverted = false,
     disabled = false,
-    outlined = false,
     handleClick,
     loading = false,
     primary = false,
     secondary = false,
     tertiary = false,
+    fallback = false,
     icon = '',
     forcedWidth = '',
     form,
@@ -59,46 +51,26 @@ export const Button: FC<ButtonProps> = forwardRef<
     ...otherProps
   } = props
 
-  const isLegacyButton = !primary && !secondary && !tertiary
-
   useDeprecatedWarning({
-    enabled: isLegacyButton,
-    title: 'Legacy Button',
+    enabled: tertiary,
+    title: 'Tertiary prop usage',
     message:
-      "You're using the legacy Button component. Please use the new Button by providing one of the following props: 'primary', 'secondary', 'tertiary'.",
+      "You're using the tertiary prop which is now deprecated. Please use the new 'fallback' prop instead, or 'primary' or 'secondary' as appropriate.",
     componentProps: props,
   })
-
-  if (isLegacyButton) {
-    return (
-      <LegacyButton
-        id={id}
-        className={className}
-        color={color}
-        block={block}
-        inverted={inverted}
-        disabled={disabled}
-        outlined={outlined}
-        handleClick={(e) => {
-          handleClick && handleClick(e)
-        }}
-      >
-        {children}
-      </LegacyButton>
-    )
-  }
 
   return (
     <Container
       as="button"
       id={id}
       className={className}
-      disabled={disabled || loading}
+      disabled={disabled}
       onClick={handleClick}
       $loading={loading}
       primary={primary}
       secondary={secondary}
       tertiary={tertiary}
+      fallback={fallback}
       icon={icon}
       forcedWidth={forcedWidth}
       {...(form ? { form } : {})}
@@ -108,17 +80,11 @@ export const Button: FC<ButtonProps> = forwardRef<
     >
       {loading && (
         <LoaderContainer>
-          <Loader color={primary ? 'cream' : 'liquorice'} height="16" />
+          <Loader color={'liquorice'} height="16" />
         </LoaderContainer>
       )}
       <ContentContainer icon={icon} $loading={loading}>
-        {icon && (
-          <IconContainer
-            render={icon}
-            size={24}
-            color={primary ? 'cream' : 'liquorice'}
-          />
-        )}
+        {icon && <IconContainer render={icon} size={24} color={'liquorice'} />}
         <ChildrenContainer>{children}</ChildrenContainer>
       </ContentContainer>
     </Container>
@@ -130,23 +96,36 @@ Button.displayName = 'Button'
 type IButton = Required<
   Pick<
     ButtonProps,
-    'disabled' | 'primary' | 'secondary' | 'tertiary' | 'icon' | 'forcedWidth'
+    | 'disabled'
+    | 'primary'
+    | 'secondary'
+    | 'tertiary'
+    | 'icon'
+    | 'forcedWidth'
+    | 'fallback'
   >
 > & {
   $loading: NonNullable<ButtonProps['loading']>
 }
 
 const Container = styled(Box)<IButton>(
-  ({ disabled, $loading, primary, secondary, tertiary, forcedWidth }) => css`
+  ({
+    disabled,
+    $loading,
+    primary,
+    secondary,
+    tertiary,
+    forcedWidth,
+    fallback,
+  }) => css`
     ${focusOutline()}
     position: relative;
     background-color: ${theme.colors.marshmallowPink};
-    border: 2px solid;
     box-shadow: none;
     color: ${theme.colors.liquorice};
     padding: 0 20px;
     outline: none;
-    border-radius: 8px;
+    border-radius: 10000px;
     font-weight: ${theme.font.weight.medium};
     cursor: ${disabled || $loading ? 'not-allowed' : 'pointer'};
     line-height: 100%;
@@ -156,49 +135,35 @@ const Container = styled(Box)<IButton>(
 
     ${primary &&
     css`
-      color: ${theme.colors.cream};
-      border-color: ${theme.colors.marshmallowPink};
+      color: ${theme.colors.liquorice};
 
       &:hover {
-        background-color: ${!(disabled || $loading) &&
-        darken(0.1, theme.colors.marshmallowPink)};
-        border-color: ${!(disabled || $loading) &&
-        darken(0.1, theme.colors.marshmallowPink)};
+        background-color: ${!(disabled || $loading) && theme.colors.bubblegum};
       }
       &:active {
-        background-color: ${darken(0.1, theme.colors.marshmallowPink)};
-        border-color: ${darken(0.1, theme.colors.marshmallowPink)};
+        background-color: ${theme.colors.lollipop};
       }
     `}
     ${secondary &&
     css`
+      background-color: ${theme.colors.oatmeal};
+
+      &:hover {
+        background-color: ${!(disabled || $loading) && theme.colors.mascarpone};
+      }
+      &:active {
+        background-color: ${theme.colors.custard};
+      }
+    `}
+  ${(tertiary || fallback) &&
+    css`
       background-color: ${theme.colors.cream};
-      border-color: ${theme.colors.liquorice};
 
       &:hover {
         background-color: ${!(disabled || $loading) && theme.colors.coconut};
-        border: ${!(disabled || $loading) &&
-        `2px solid ${theme.colors.liquorice}`};
       }
       &:active {
-        background-color: ${theme.colors.coconut};
-        border: 2px solid ${theme.colors.liquorice};
-      }
-    `}
-  ${tertiary &&
-    css`
-      background-color: ${theme.colors.coconut};
-      border-color: ${theme.colors.coconut};
-
-      &:hover {
-        background-color: ${!(disabled || $loading) &&
-        darken(0.1, theme.colors.coconut)};
-        border-color: ${!(disabled || $loading) &&
-        darken(0.1, theme.colors.coconut)};
-      }
-      &:active {
-        background-color: ${darken(0.1, theme.colors.coconut)};
-        border-color: ${darken(0.1, theme.colors.coconut)};
+        background-color: ${theme.colors.mascarpone};
       }
     `}
   `,
