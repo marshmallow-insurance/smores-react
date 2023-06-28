eafde46694be4bacb1335133e46e1a77dddc95ec
rowanhen <57456071+rowanhen@users.noreply.github.com>
2023-06-28 10:08:05 +0100

    making input collection pages (#2824)

--- src/TextInput/TextInput.stories.tsx
+++ src/TextInput/TextInput.stories.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { TextInput, TextInputProps } from './TextInput'
-import { SupportMessage } from '../SupportMessage'
 import { Container } from './Container'
 import { noop } from '../utils/noop'
+import { CollectionPage } from './Collection'
 
 export default {
   title: 'Text Input',
@@ -47,126 +47,7 @@ TypePassword.args = {
   type: 'password',
 }
 
-export const Required = Template.bind({})
-
-Required.args = {
-  id: 'textInput',
-  name: 'textInput',
-  placeholder: 'Enter name',
-  onChange: noop,
-  onInputChange: noop,
-  onBlur: noop,
-  label: 'Please enter your name',
-  required: true,
-}
-
-export const WithError = Template.bind({})
-
-WithError.args = {
-  id: 'textInput',
-  name: 'textInput',
-  placeholder: 'Placeholder text',
-  onChange: noop,
-  onInputChange: noop,
-  onBlur: noop,
-  required: true,
-  error: true,
-  errorMsg: 'Oh boy, something went wrong!',
-}
-
-export const Disabled = Template.bind({})
-
-Disabled.args = {
-  id: 'textInput',
-  name: 'textInput',
-  placeholder: 'Placeholder text',
-  onChange: noop,
-  onInputChange: noop,
-  onBlur: noop,
-  label: 'label',
-  disabled: true,
-}
-
-export const DisabledFallback = Template.bind({})
-
-DisabledFallback.args = {
-  id: 'textInput',
-  name: 'textInput',
-  placeholder: 'Placeholder text',
-  onChange: noop,
-  onInputChange: noop,
-  onBlur: noop,
-  label: 'label',
-  fallback: true,
-  disabled: true,
-}
-
-export const WithLabel = Template.bind({})
-
-WithLabel.args = {
-  id: 'textInput',
-  name: 'textInput',
-  placeholder: 'Placeholder text',
-  onChange: noop,
-  onInputChange: noop,
-  onBlur: noop,
-  label: 'label',
-}
-
-export const WithFrontIcon = Template.bind({})
-
-WithFrontIcon.args = {
-  id: 'textInput',
-  name: 'textInput',
-  placeholder: 'Placeholder text',
-  onChange: noop,
-  onInputChange: noop,
-  onBlur: noop,
-  label: 'label',
-  frontIcon: 'at',
-}
-
-export const WithTrailingIcon = Template.bind({})
-
-WithTrailingIcon.args = {
-  id: 'textInput',
-  name: 'textInput',
-  placeholder: 'Placeholder text',
-  onChange: noop,
-  onInputChange: noop,
-  onBlur: noop,
-  label: 'label',
-  trailingIcon: 'at',
-}
-
-export const LabelAsTitle = Template.bind({})
-
-LabelAsTitle.args = {
-  id: 'textInput',
-  name: 'textInput',
-  placeholder: 'Placeholder text',
-  onChange: noop,
-  onInputChange: noop,
-  onBlur: noop,
-  label: 'Please enter your name',
-  required: true,
-  assistiveText: 'Some more information...',
-  renderAsTitle: true,
-}
-
-export const WithReactElementError = Template.bind({})
-
-WithReactElementError.args = {
-  id: 'textInput',
-  name: 'textInput',
-  label: 'with Support Message as Error',
-  placeholder: 'Placeholder text',
-  onChange: noop,
-  onInputChange: noop,
-  onBlur: noop,
-  error: true,
-  errorMsg: <SupportMessage type="warning" description="error!!" />,
-}
+export const Collection = CollectionPage.bind({})
 
 const WorkingExampleTemplate = () => <Container />
 
