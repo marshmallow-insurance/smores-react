eab7f5a9a50b8e823a4c2c93aeeb078b542bf692
rowanhen <rowan.henseleit@marshmallow.co>
2023-08-23 09:24:37 +0100

    reverting unneeded build fix attempts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -10,9 +10,6 @@ const config: StorybookConfig = {
     name: '@storybook/react-vite',
     options: {},
   },
-  core: {
-    builder: '@storybook/builder-vite',
-  },
   docs: {
     autodocs: true,
   },
