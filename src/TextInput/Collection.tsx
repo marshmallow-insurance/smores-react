df9515e5a562da1e451ad3a963e2cbcbe147919c
Mat Diak <mat.diak@marshmallow.co>
2023-11-10 14:57:42 +0000

    Feat: Add storybook for description with a link

--- src/TextInput/Collection.tsx
+++ src/TextInput/Collection.tsx
@@ -12,6 +12,15 @@ const variationList: TextInputProps[] = [
     id: 'days',
     placeholder: 'Enter name...',
     errorMsg: 'This field is required',
+    assistiveText: (
+      <>
+        This is a description with a{' '}
+        <a href="marshmallow.com" rel="noopener noreferrer" target="_blank">
+          link
+        </a>
+        .
+      </>
+    ),
     label: 'First name',
     onChange: noop,
     onInputChange: noop,
@@ -25,162 +34,190 @@ export const CollectionPage: FC = () => {
 
   return (
     <Box flex direction="column">
-      {variationList.map(({ id, placeholder, errorMsg, label }) => {
-        return (
-          <Box key={id} flex direction="column" mb="32px">
-            <Title tag="span" typo="header-medium">
-              TextInput variations
-            </Title>
-            <Row label="Generic">
-              <TextInput
-                id={id}
-                label={label}
-                value=""
-                placeholder={placeholder}
-                error={false}
-                errorMsg={errorMsg}
-                onChange={setValue}
-              />
-            </Row>
-            <Row label="Fallback">
-              <TextInput
-                id={id}
-                label={label}
-                value=""
-                placeholder={placeholder}
-                fallbackStyle={true}
-                error={false}
-                errorMsg={errorMsg}
-                onChange={setValue}
-              />
-            </Row>
-            <Row label="Disabled">
-              <TextInput
-                id={id}
-                label={label}
-                value=""
-                placeholder={placeholder}
-                disabled={true}
-                error={false}
-                errorMsg={errorMsg}
-                onChange={setValue}
-              />
-            </Row>
-            <Row label="Required">
-              <TextInput
-                id={id}
-                label={label}
-                value=""
-                required={true}
-                placeholder={placeholder}
-                error={false}
-                errorMsg={errorMsg}
-                onChange={setValue}
-              />
-            </Row>
-            <Row label="Leading Icon">
-              <TextInput
-                id={id}
-                label={label}
-                frontIcon="search"
-                value=""
-                placeholder={placeholder}
-                error={false}
-                errorMsg={errorMsg}
-                onChange={setValue}
-              />
-            </Row>
-            <Row label="Trailing Icon">
-              <TextInput
-                id={id}
-                label={label}
-                trailingIcon="car"
-                value=""
-                placeholder={placeholder}
-                error={false}
-                errorMsg={errorMsg}
-                onChange={setValue}
-              />
-            </Row>
-            <Row label="Assistive text">
-              <TextInput
-                id={id}
-                label={label}
-                value=""
-                placeholder={placeholder}
-                assistiveText="Some assistive text"
-                error={false}
-                errorMsg={errorMsg}
-                onChange={setValue}
-              />
-            </Row>
-            <Row label="Completed">
-              <TextInput
-                id={id}
-                label={label}
-                value=""
-                placeholder={placeholder}
-                error={false}
-                completed={true}
-                errorMsg={errorMsg}
-                onChange={setValue}
-              />
-            </Row>
-            <Row label="As title">
-              <TextInput
-                id={id}
-                label={label}
-                value=""
-                renderAsTitle={true}
-                placeholder={placeholder}
-                error={false}
-                errorMsg={errorMsg}
-                onChange={setValue}
-              />
-            </Row>
-            <Row label="Error">
-              <TextInput
-                id={id}
-                label={label}
-                value=""
-                required={true}
-                placeholder={placeholder}
-                assistiveText="Some assistive text"
-                error={true}
-                errorMsg={errorMsg}
-                onChange={setValue}
-              />
-            </Row>
-            <Row label="React element error">
-              <TextInput
-                id={id}
-                label={label}
-                required={true}
-                value=""
-                placeholder={placeholder}
-                error={true}
-                errorMsg={
-                  <SupportMessage type="warning" description="error!!" />
-                }
-                onChange={setValue}
-              />
-            </Row>
-            <Row label="Fallback Error">
-              <TextInput
-                id={id}
-                label={label}
-                required={true}
-                value=""
-                placeholder={placeholder}
-                fallbackStyle={true}
-                error={true}
-                errorMsg={errorMsg}
-                onChange={setValue}
-              />
-            </Row>
-          </Box>
-        )
-      })}
+      {variationList.map(
+        ({ id, placeholder, errorMsg, label, assistiveText }) => {
+          return (
+            <Box key={id} flex direction="column" mb="32px">
+              <Title tag="span" typo="header-medium">
+                TextInput variations
+              </Title>
+              <Row label="Generic">
+                <TextInput
+                  id={id}
+                  label={label}
+                  value=""
+                  placeholder={placeholder}
+                  error={false}
+                  errorMsg={errorMsg}
+                  onChange={setValue}
+                />
+              </Row>
+              <Row label="With label as title">
+                <TextInput
+                  id={id}
+                  label={label}
+                  value=""
+                  renderAsTitle
+                  placeholder={placeholder}
+                  error={false}
+                  errorMsg={errorMsg}
+                  onChange={setValue}
+                />
+              </Row>
+              <Row label="Fallback">
+                <TextInput
+                  id={id}
+                  label={label}
+                  value=""
+                  placeholder={placeholder}
+                  fallbackStyle={true}
+                  error={false}
+                  errorMsg={errorMsg}
+                  onChange={setValue}
+                />
+              </Row>
+              <Row label="Disabled">
+                <TextInput
+                  id={id}
+                  label={label}
+                  value=""
+                  placeholder={placeholder}
+                  disabled={true}
+                  error={false}
+                  errorMsg={errorMsg}
+                  onChange={setValue}
+                />
+              </Row>
+              <Row label="Required">
+                <TextInput
+                  id={id}
+                  label={label}
+                  value=""
+                  required={true}
+                  placeholder={placeholder}
+                  error={false}
+                  errorMsg={errorMsg}
+                  onChange={setValue}
+                />
+              </Row>
+              <Row label="Leading Icon">
+                <TextInput
+                  id={id}
+                  label={label}
+                  frontIcon="search"
+                  value=""
+                  placeholder={placeholder}
+                  error={false}
+                  errorMsg={errorMsg}
+                  onChange={setValue}
+                />
+              </Row>
+              <Row label="Trailing Icon">
+                <TextInput
+                  id={id}
+                  label={label}
+                  trailingIcon="car"
+                  value=""
+                  placeholder={placeholder}
+                  error={false}
+                  errorMsg={errorMsg}
+                  onChange={setValue}
+                />
+              </Row>
+              <Row label="Assistive text">
+                <TextInput
+                  id={id}
+                  label={label}
+                  value=""
+                  placeholder={placeholder}
+                  assistiveText="Some assistive text"
+                  error={false}
+                  errorMsg={errorMsg}
+                  onChange={setValue}
+                />
+              </Row>
+              <Row label="With label as title and assistive text with link">
+                <TextInput
+                  id={id}
+                  label={label}
+                  value=""
+                  renderAsTitle
+                  assistiveText={assistiveText}
+                  placeholder={placeholder}
+                  error={false}
+                  completed={true}
+                  errorMsg={errorMsg}
+                  onChange={setValue}
+                />
+              </Row>
+              <Row label="Completed">
+                <TextInput
+                  id={id}
+                  label={label}
+                  value=""
+                  placeholder={placeholder}
+                  error={false}
+                  completed={true}
+                  errorMsg={errorMsg}
+                  onChange={setValue}
+                />
+              </Row>
+              <Row label="As title">
+                <TextInput
+                  id={id}
+                  label={label}
+                  value=""
+                  renderAsTitle={true}
+                  placeholder={placeholder}
+                  error={false}
+                  errorMsg={errorMsg}
+                  onChange={setValue}
+                />
+              </Row>
+              <Row label="Error">
+                <TextInput
+                  id={id}
+                  label={label}
+                  value=""
+                  required={true}
+                  placeholder={placeholder}
+                  assistiveText="Some assistive text"
+                  error={true}
+                  errorMsg={errorMsg}
+                  onChange={setValue}
+                />
+              </Row>
+              <Row label="React element error">
+                <TextInput
+                  id={id}
+                  label={label}
+                  required={true}
+                  value=""
+                  placeholder={placeholder}
+                  error={true}
+                  errorMsg={
+                    <SupportMessage type="warning" description="error!!" />
+                  }
+                  onChange={setValue}
+                />
+              </Row>
+              <Row label="Fallback Error">
+                <TextInput
+                  id={id}
+                  label={label}
+                  required={true}
+                  value=""
+                  placeholder={placeholder}
+                  fallbackStyle={true}
+                  error={true}
+                  errorMsg={errorMsg}
+                  onChange={setValue}
+                />
+              </Row>
+            </Box>
+          )
+        },
+      )}
     </Box>
   )
 }
