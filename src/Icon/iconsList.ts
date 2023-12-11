51e523321a8ba0a0109371c804cd6821918e5ae0
rowanhen <rowan.henseleit@marshmallow.co>
2023-12-11 17:25:01 +0000

    creating iconList.ts and iconTypes.ts files

---
+++ src/Icon/iconsList.ts
@@ -0,0 +1,415 @@
+import { FC } from 'react'
+import { Color } from 'theme'
+import { Aa } from './assets/Aa'
+import { AaInverted } from './assets/AaInverted'
+import { AccidentalDamage } from './assets/AccidentalDamage'
+import { AccountBreakdown } from './assets/AccountBreakdown'
+import { AccountMlp } from './assets/AccountMlp'
+import { ActivityClipboard } from './assets/ActivityClipboard'
+import { AddOns } from './assets/AddOns'
+import { AddressMta } from './assets/AddressMta'
+import { AlbumAudio } from './assets/AlbumAudio'
+import { Alert } from './assets/Alert'
+import { AlertEngine } from './assets/AlertEngine'
+import { Amex } from './assets/Amex'
+import { ApplePay } from './assets/ApplePay'
+import { ApprovedFile } from './assets/ApprovedFile'
+import { Arrow } from './assets/Arrow'
+import { At } from './assets/At'
+import { Axa } from './assets/Axa'
+import { Bank } from './assets/Bank'
+import { Basket } from './assets/Basket'
+import { Bicycle } from './assets/Bicycle'
+import { Boost } from './assets/Boost'
+import { Breakdown } from './assets/Breakdown'
+import { BreakdownAlt } from './assets/BreakdownAlt'
+import { Briefcase } from './assets/Briefcase'
+import { Burger } from './assets/Burger'
+import { Calendar } from './assets/Calendar'
+import { Camera } from './assets/Camera'
+import { Cancel } from './assets/Cancel'
+import { CancelBw } from './assets/CancelBw'
+import { Car } from './assets/Car'
+import { CarMta } from './assets/CarMta'
+import { CarRepairs } from './assets/CarRepairs'
+import { CarReplacement } from './assets/CarReplacement'
+import { CarUsage } from './assets/CarUsage'
+import { Card } from './assets/Card'
+import { Caret } from './assets/Caret'
+import { Charge } from './assets/Charge'
+import { Chat } from './assets/Chat'
+import { ChatBubble } from './assets/ChatBubble'
+import { Checkout } from './assets/Checkout'
+import { CheckoutWithText } from './assets/CheckoutWithText'
+import { CircleTick } from './assets/CircleTick'
+import { City } from './assets/City'
+import { Claim } from './assets/Claim'
+import { ClaimLine } from './assets/ClaimLine'
+import { ClipboardCheck } from './assets/ClipboardCheck'
+import { Clock } from './assets/Clock'
+import { ClosePanel } from './assets/ClosePanel'
+import { Confused } from './assets/Confused'
+import { ConfusedBlue } from './assets/ConfusedBlue'
+import { ContactEmail } from './assets/ContactEmail'
+import { Copy } from './assets/Copy'
+import { Crop } from './assets/Crop'
+import { Cross } from './assets/Cross'
+import { DashboardHigh } from './assets/DashboardHigh'
+import { DirectDebit } from './assets/DirectDebit'
+import { DocDownload } from './assets/DocDownload'
+import { DocUpload } from './assets/DocUpload'
+import { Download } from './assets/Download'
+import { Earth } from './assets/Earth'
+import { Edit } from './assets/Edit'
+import { EditContact } from './assets/EditContact'
+import { EditOutline } from './assets/EditOutline'
+import { EmailFilled } from './assets/EmailFilled'
+import { Engine } from './assets/Engine'
+import { Europe } from './assets/Europe'
+import { Excluded } from './assets/Excluded'
+import { Expand } from './assets/Expand'
+import { Facebook } from './assets/Facebook'
+import { FallingItems } from './assets/FallingItems'
+import { Fence } from './assets/Fence'
+import { File } from './assets/File'
+import { FileBadge2 } from './assets/FileBadge2'
+import { FileCabinet } from './assets/FileCabinet'
+import { Filter } from './assets/Filter'
+import { Fire } from './assets/Fire'
+import { Flag } from './assets/Flag'
+import { Folder } from './assets/Folder'
+import { FraudAlert } from './assets/FraudAlert'
+import { FreezerFood } from './assets/FreezerFood'
+import { Fuel } from './assets/Fuel'
+import { Globe } from './assets/Globe'
+import { GooglePlayBadge } from './assets/GooglePlayBadge'
+import { Gpay } from './assets/Gpay'
+import { HelpAboutus } from './assets/HelpAboutus'
+import { HelpBuypolicy } from './assets/HelpBuypolicy'
+import { HelpCenter } from './assets/HelpCenter'
+import { HelpIdcheck } from './assets/HelpIdcheck'
+import { HelpManagePolicy } from './assets/HelpManagePolicy'
+import { HelpNcd } from './assets/HelpNcd'
+import { HelpVerification } from './assets/HelpVerification'
+import { HomeExcluded } from './assets/HomeExcluded'
+import { House } from './assets/House'
+import { HouseTree } from './assets/HouseTree'
+import { Id } from './assets/Id'
+import { Included } from './assets/Included'
+import { Info } from './assets/Info'
+import { Injury } from './assets/Injury'
+import { Instagram } from './assets/Instagram'
+import { Intercom } from './assets/Intercom'
+import { Intercom2 } from './assets/Intercom2'
+import { IosStoreBadge } from './assets/IosStoreBadge'
+import { Iphone } from './assets/Iphone'
+import { Jewellery } from './assets/Jewellery'
+import { Key } from './assets/Key'
+import { Laptop } from './assets/Laptop'
+import { Leaks } from './assets/Leaks'
+import { Lightbulb } from './assets/Lightbulb'
+import { Link } from './assets/Link'
+import { Linkedin } from './assets/Linkedin'
+import { LiveChat } from './assets/LiveChat'
+import { LiveChat2 } from './assets/LiveChat2'
+import { Location } from './assets/Location'
+import { LogOut } from './assets/LogOut'
+import { ManageDrivers } from './assets/ManageDrivers'
+import { ManagePolicy } from './assets/ManagePolicy'
+import { Marshmallow } from './assets/Marshmallow'
+import { Mastercard } from './assets/Mastercard'
+import { Maximise } from './assets/Maximise'
+import { Medical } from './assets/Medical'
+import { Minimise } from './assets/Minimise'
+import { Minus } from './assets/Minus'
+import { MinusCircle } from './assets/MinusCircle'
+import { MoneyBag } from './assets/MoneyBag'
+import { MoneyHeart } from './assets/MoneyHeart'
+import { MoreDots } from './assets/MoreDots'
+import { Mulsanne } from './assets/Mulsanne'
+import { NewWindow } from './assets/NewWindow'
+import { NoExcess } from './assets/NoExcess'
+import { Notes } from './assets/Notes'
+import { Notification } from './assets/Notification'
+import { Onfido } from './assets/Onfido'
+import { OpenPanel } from './assets/OpenPanel'
+import { Other } from './assets/Other'
+import { OutOfHome } from './assets/OutOfHome'
+import { Padlock } from './assets/Padlock'
+import { PadlockOutline } from './assets/PadlockOutline'
+import { People } from './assets/People'
+import { Person } from './assets/Person'
+import { PhoneFilled } from './assets/PhoneFilled'
+import { PhoneOutline } from './assets/PhoneOutline'
+import { Photo } from './assets/Photo'
+import { PinCode } from './assets/PinCode'
+import { Plus } from './assets/Plus'
+import { PlusCircle } from './assets/PlusCircle'
+import { Policy } from './assets/Policy'
+import { PolicyDetails } from './assets/PolicyDetails'
+import { PolicyDoc } from './assets/PolicyDoc'
+import { PolicyDocs } from './assets/PolicyDocs'
+import { PoundBold } from './assets/PoundBold'
+import { PoundFilledCircle } from './assets/PoundFilledCircle'
+import { PoundMedium } from './assets/PoundMedium'
+import { PoundOutline } from './assets/PoundOutline'
+import { PoundRegular } from './assets/PoundRegular'
+import { Premfina } from './assets/Premfina'
+import { Profile } from './assets/Profile'
+import { Prohibited } from './assets/Prohibited'
+import { Property } from './assets/Property'
+import { QuestionHelp } from './assets/QuestionHelp'
+import { Rac } from './assets/Rac'
+import { Ravelin } from './assets/Ravelin'
+import { ReferAFriend } from './assets/ReferAFriend'
+import { Refund } from './assets/Refund'
+import { RegPlate } from './assets/RegPlate'
+import { Renewal } from './assets/Renewal'
+import { RenewalLine } from './assets/RenewalLine'
+import { RepeatCharge } from './assets/RepeatCharge'
+import { Return } from './assets/Return'
+import { ScheduleQuote } from './assets/ScheduleQuote'
+import { Search } from './assets/Search'
+import { Security } from './assets/Security'
+import { SendMessage } from './assets/SendMessage'
+import { Settings } from './assets/Settings'
+import { Shield } from './assets/Shield'
+import { ShieldCheck } from './assets/ShieldCheck'
+import { Shopping } from './assets/Shopping'
+import { Sofa } from './assets/Sofa'
+import { Stack } from './assets/Stack'
+import { Storm } from './assets/Storm'
+import { Stripe } from './assets/Stripe'
+import { Subsidence } from './assets/Subsidence'
+import { Theft } from './assets/Theft'
+import { ThumbsUp } from './assets/ThumbsUp'
+import { Tick } from './assets/Tick'
+import { TowTruck } from './assets/TowTruck'
+import { TraceAndAccess } from './assets/TraceAndAccess'
+import { Training } from './assets/Training'
+import { Trash } from './assets/Trash'
+import { Twitter } from './assets/Twitter'
+import { UpDown } from './assets/UpDown'
+import { Upload } from './assets/Upload'
+import { UploadDocuments } from './assets/UploadDocuments'
+import { UploadEmail } from './assets/UploadEmail'
+import { Vandalism } from './assets/Vandalism'
+import { Village } from './assets/Village'
+import { Visa } from './assets/Visa'
+import { Warning } from './assets/Warning'
+import { Watch } from './assets/Watch'
+import { Wave } from './assets/Wave'
+import { Wellbeing } from './assets/Wellbeing'
+import { WheelFlat } from './assets/WheelFlat'
+import { Windscreen } from './assets/Windscreen'
+import { WindscreenRepair } from './assets/WindscreenRepair'
+import { WindscreenReplacement } from './assets/WindscreenReplacement'
+import { Icons } from './iconsTypes'
+
+export const iconList: Record<Icons, FC<{ color: Color }>> = {
+  'aa-inverted': AaInverted,
+  'accidental-damage': AccidentalDamage,
+  'account-breakdown': AccountBreakdown,
+  'account-mlp': AccountMlp,
+  'activity-clipboard': ActivityClipboard,
+  'add-ons': AddOns,
+  'address-mta': AddressMta,
+  'album-audio': AlbumAudio,
+  'alert-engine': AlertEngine,
+  'apple-pay': ApplePay,
+  'approved-file': ApprovedFile,
+  'breakdown-alt': BreakdownAlt,
+  'cancel-bw': CancelBw,
+  'car-mta': CarMta,
+  'car-repairs': CarRepairs,
+  'car-replacement': CarReplacement,
+  'car-usage': CarUsage,
+  'chat-bubble': ChatBubble,
+  'checkout-with-text': CheckoutWithText,
+  'circle-tick': CircleTick,
+  'claim-line': ClaimLine,
+  'clipboard-check': ClipboardCheck,
+  'close-panel': ClosePanel,
+  'confused-blue': ConfusedBlue,
+  'contact-email': ContactEmail,
+  'dashboard-high': DashboardHigh,
+  'direct-debit': DirectDebit,
+  'doc-download': DocDownload,
+  'doc-upload': DocUpload,
+  'edit-contact': EditContact,
+  'edit-outline': EditOutline,
+  'email-filled': EmailFilled,
+  'falling-items': FallingItems,
+  'file-badge-2': FileBadge2,
+  'file-cabinet': FileCabinet,
+  'fraud-alert': FraudAlert,
+  'freezer-food': FreezerFood,
+  'google-play-badge': GooglePlayBadge,
+  'help-aboutus': HelpAboutus,
+  'help-buypolicy': HelpBuypolicy,
+  'help-center': HelpCenter,
+  'help-idcheck': HelpIdcheck,
+  'help-manage-policy': HelpManagePolicy,
+  'help-ncd': HelpNcd,
+  'help-verification': HelpVerification,
+  'home-excluded': HomeExcluded,
+  'house-tree': HouseTree,
+  'ios-store-badge': IosStoreBadge,
+  'live-chat': LiveChat,
+  'live-chat2': LiveChat2,
+  'log-out': LogOut,
+  'manage-drivers': ManageDrivers,
+  'manage-policy': ManagePolicy,
+  'minus-circle': MinusCircle,
+  'money-bag': MoneyBag,
+  'money-heart': MoneyHeart,
+  'more-dots': MoreDots,
+  'new-window': NewWindow,
+  'no-excess': NoExcess,
+  'open-panel': OpenPanel,
+  'out-of-home': OutOfHome,
+  'padlock-outline': PadlockOutline,
+  'phone-filled': PhoneFilled,
+  'phone-outline': PhoneOutline,
+  'pin-code': PinCode,
+  'plus-circle': PlusCircle,
+  'policy-details': PolicyDetails,
+  'policy-doc': PolicyDoc,
+  'policy-docs': PolicyDocs,
+  'pound-bold': PoundBold,
+  'pound-filled-circle': PoundFilledCircle,
+  'pound-medium': PoundMedium,
+  'pound-outline': PoundOutline,
+  'pound-regular': PoundRegular,
+  'question-help': QuestionHelp,
+  'refer-a-friend': ReferAFriend,
+  'reg-plate': RegPlate,
+  'renewal-line': RenewalLine,
+  'repeat-charge': RepeatCharge,
+  'schedule-quote': ScheduleQuote,
+  'send-message': SendMessage,
+  'shield-check': ShieldCheck,
+  'thumbs-up': ThumbsUp,
+  'tow-truck': TowTruck,
+  'trace-and-access': TraceAndAccess,
+  'up-down': UpDown,
+  'upload-documents': UploadDocuments,
+  'upload-email': UploadEmail,
+  'wheel-flat': WheelFlat,
+  'windscreen-repair': WindscreenRepair,
+  'windscreen-replacement': WindscreenReplacement,
+  aa: Aa,
+  addons: AddOns,
+  alert: Alert,
+  amex: Amex,
+  arrow: Arrow,
+  at: At,
+  axa: Axa,
+  bank: Bank,
+  basket: Basket,
+  bicycle: Bicycle,
+  boost: Boost,
+  breakdown: Breakdown,
+  briefcase: Briefcase,
+  burger: Burger,
+  calendar: Calendar,
+  camera: Camera,
+  cancel: Cancel,
+  car: Car,
+  card: Card,
+  caret: Caret,
+  charge: Charge,
+  chat: Chat,
+  checkout: Checkout,
+  city: City,
+  claim: Claim,
+  clock: Clock,
+  confused: Confused,
+  copy: Copy,
+  crop: Crop,
+  cross: Cross,
+  download: Download,
+  earth: Earth,
+  edit: Edit,
+  engine: Engine,
+  europe: Europe,
+  excluded: Excluded,
+  expand: Expand,
+  facebook: Facebook,
+  fence: Fence,
+  file: File,
+  filter: Filter,
+  fire: Fire,
+  flag: Flag,
+  folder: Folder,
+  fuel: Fuel,
+  globe: Globe,
+  gpay: Gpay,
+  house: House,
+  id: Id,
+  included: Included,
+  info: Info,
+  injury: Injury,
+  instagram: Instagram,
+  intercom: Intercom,
+  intercom2: Intercom2,
+  iphone: Iphone,
+  jewellery: Jewellery,
+  key: Key,
+  laptop: Laptop,
+  leaks: Leaks,
+  lightbulb: Lightbulb,
+  link: Link,
+  linkedin: Linkedin,
+  location: Location,
+  marshmallow: Marshmallow,
+  mastercard: Mastercard,
+  maximise: Maximise,
+  medical: Medical,
+  minimise: Minimise,
+  minus: Minus,
+  mulsanne: Mulsanne,
+  notes: Notes,
+  notification: Notification,
+  onfido: Onfido,
+  other: Other,
+  padlock: Padlock,
+  people: People,
+  person: Person,
+  photo: Photo,
+  plus: Plus,
+  policy: Policy,
+  premfina: Premfina,
+  profile: Profile,
+  prohibited: Prohibited,
+  property: Property,
+  rac: Rac,
+  ravelin: Ravelin,
+  refund: Refund,
+  renewal: Renewal,
+  return: Return,
+  search: Search,
+  security: Security,
+  settings: Settings,
+  shield: Shield,
+  shopping: Shopping,
+  sofa: Sofa,
+  stack: Stack,
+  storm: Storm,
+  stripe: Stripe,
+  subsidence: Subsidence,
+  theft: Theft,
+  tick: Tick,
+  training: Training,
+  trash: Trash,
+  twitter: Twitter,
+  upload: Upload,
+  vandalism: Vandalism,
+  village: Village,
+  visa: Visa,
+  warning: Warning,
+  watch: Watch,
+  wave: Wave,
+  wellbeing: Wellbeing,
+  windscreen: Windscreen,
+} satisfies Record<Icons, FC<{ color: Color }>>
