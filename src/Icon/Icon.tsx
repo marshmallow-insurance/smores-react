c093e4822b760abfc3b9d0292c44f4196c91815e
urbnzs <zsuzsanna.urban@marshmallow.co>
2024-01-25 14:25:31 +0100

    fix: set fill and stroke colors from theme

--- src/Icon/Icon.tsx
+++ src/Icon/Icon.tsx
@@ -1101,11 +1101,11 @@ export const Icon: FC<IconProps> = ({
         fill="none"
       >
         <path
-          fill="#000"
+          fill={theme.colors[color]}
           d="M12.8 2.133h-2.133V1.067C10.667.477 10.19 0 9.601 0H6.4c-.59 0-1.067.478-1.067 1.067v1.066H3.2a1.6 1.6 0 0 0-1.6 1.6V14.4A1.6 1.6 0 0 0 3.2 16h9.6a1.6 1.6 0 0 0 1.6-1.6V3.733a1.6 1.6 0 0 0-1.6-1.6ZM6.4 1.067h3.2v1.066H6.4V1.067ZM13.334 14.4a.533.533 0 0 1-.533.533H3.2a.534.534 0 0 1-.533-.533V3.733c0-.294.239-.533.533-.533h1.6v.533c0 .59.479 1.067 1.067 1.067h4.267c.589 0 1.067-.478 1.067-1.067V3.2h1.6c.293 0 .532.239.532.533V14.4Z"
         />
         <path
-          fill="#000"
+          fill={theme.colors[color]}
           d="M11.2 6.4H4.8a.534.534 0 0 0 0 1.067h6.4a.533.533 0 1 0 0-1.067ZM11.2 8.533H4.8a.533.533 0 1 0 0 1.067h6.4a.533.533 0 1 0 0-1.067ZM11.2 10.667H4.8a.533.533 0 1 0 0 1.066h6.4a.533.533 0 1 0 0-1.066Z"
         />
       </svg>
@@ -1147,7 +1147,7 @@ export const Icon: FC<IconProps> = ({
         fill="none"
       >
         <path
-          fill="white"
+          fill={theme.colors[color]}
           d="M1.707 23.707 9 16.414V21a1 1 0 1 0 2 0v-7a.997.997 0 0 0-1-1H3a1 1 0 1 0 0 2h4.586L.293 22.293a.999.999 0 1 0 1.414 1.414Zm11.37-13.325A1.003 1.003 0 0 0 14 11h7a1 1 0 0 0 0-2h-4.586l7.293-7.293A.999.999 0 1 0 22.293.293L15 7.586V3a1 1 0 0 0-2 0v7c0 .13.027.26.077.382Z"
         />
       </svg>
@@ -2313,7 +2313,7 @@ export const Icon: FC<IconProps> = ({
         fill="none"
       >
         <g
-          stroke="#292924"
+          stroke={theme.colors[color]}
           strokeLinecap="round"
           strokeLinejoin="round"
           strokeMiterlimit="10"
@@ -2877,7 +2877,7 @@ export const Icon: FC<IconProps> = ({
         fill="none"
       >
         <path
-          fill="#fff"
+          fill={theme.colors[color]}
           fillRule="evenodd"
           d="M15 3H1v12h14V3ZM0 3V0h16v16H0V3Z"
           clipRule="evenodd"
