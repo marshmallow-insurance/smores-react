84f0d9d052529ce6a80689fc493c486c3365b5a4
Mat Diak <mat.diak@marshmallow.co>
2024-02-23 11:39:42 +0000

    Feat: makes placeholder option disabled (#3505)

--- src/Dropdown/Dropdown.tsx
+++ src/Dropdown/Dropdown.tsx
@@ -128,12 +128,12 @@ export const Dropdown = forwardRef(function Dropdown(
         >
           {hasOptGroups ? (
             <optgroup label={defaultOptionLabel()}>
-              <option value="" hidden={!showDefaultOption}>
+              <option value="" hidden={!showDefaultOption} disabled>
                 {defaultOptionLabel()}
               </option>
             </optgroup>
           ) : (
-            <option value="" hidden={!showDefaultOption}>
+            <option value="" hidden={!showDefaultOption} disabled>
               {defaultOptionLabel()}
             </option>
           )}
