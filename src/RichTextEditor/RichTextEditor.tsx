039c537a152eaefcd4c73e35b339146c9340b1fe
liampuk <liam.piesley@marshmallow.co>
2024-02-29 18:08:31 +0000

    working editor with lexical

--- src/RichTextEditor/RichTextEditor.tsx
+++ src/RichTextEditor/RichTextEditor.tsx
@@ -1,234 +1,82 @@
-import React, { FC, KeyboardEvent, useEffect, useRef, useState } from 'react'
+import React, { FC } from 'react'
 import { MarginProps } from '../utils/space'
 import styled from 'styled-components'
 import { Box } from '../Box'
 import { theme } from '../theme'
-import { Icon } from '../Icon'
-import { RichText } from '../RichText'
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+import CustomAutoLinkPlugin from './plugins/AutoLinkPlugin'
+import { AutoLinkNode, LinkNode } from "@lexical/link";
+import { ListNode, ListItemNode } from "@lexical/list";
+import { LinkPlugin } from "@lexical/react/LexicalLinkPlugin";
+import ToolbarPlugin from './plugins/ToolbarPlugin'
+import {HistoryPlugin} from '@lexical/react/LexicalHistoryPlugin';
+import {ListPlugin} from '@lexical/react/LexicalListPlugin';
+import { MarkdownShortcutPlugin } from "@lexical/react/LexicalMarkdownShortcutPlugin";
+import { TRANSFORMERS } from "@lexical/markdown";
+import { HeadingNode, QuoteNode } from "@lexical/rich-text";
+import { CodeHighlightNode, CodeNode } from "@lexical/code";
 
 export interface RichTextEditorProps extends MarginProps {
   htmlString: string
 }
 
 export const RichTextEditor: FC<RichTextEditorProps> = ({ htmlString, ...props }) => {
-  const [text, setText] = useState("<p>paragraph 1</p><p>paragraph 1</p><p>paragraph 3</p>")
-  const [selectionOffset, setSelectionOffset] = useState<{ lineOffset: number, charOffset: number }>({ lineOffset: 0, charOffset: 0 })
 
-  const elemRef = useRef<HTMLDivElement>(null)
-
-  const [boldActive, setBoldActive] = useState(false)
-  const [italicActive, setItalicActive] = useState(false)
-  const [bulletsActive, setBulletsActive] = useState(false)
-  const [linkActive, setLinkActive] = useState(false)
-  const [linkInput, setLinkInput] = useState("")
-
-  const allowedKeys = ['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown']
-  const disallowedKeys = ['Shift']
-
-  const handleChange = (e: KeyboardEvent<HTMLDivElement>) => {
-    console.log(e.key, e.altKey, e.ctrlKey, e.metaKey, e.shiftKey)
-    if (!allowedKeys.includes(e.key)) {
-      e.preventDefault()
-      if (!e.altKey && !e.ctrlKey && !e.metaKey && !disallowedKeys.includes(e.key)) {
-        const root = e.nativeEvent.target as HTMLDivElement
-        const rootClone = root.cloneNode(true)
-        let cloneNodes = Array.from(rootClone.childNodes) as Element[]
-        const domNodes = Array.from(root.children)
-        const selection = window.getSelection()
-        const caretElement = selection?.focusNode
-
-        if (selection && caretElement) {
-          const charOffset = selection.focusOffset
-          const lineOffset = domNodes.indexOf(caretElement.nodeName === 'P' ? caretElement : caretElement.parentElement)
-
-          if (e.key === 'Enter') {
-            console.log(cloneNodes.map(c => c))
-            console.log(lineOffset)
-            const newLineText = cloneNodes[lineOffset].innerHTML.length > 0 ? cloneNodes[lineOffset].innerHTML.substring(charOffset) : ''
-            cloneNodes[lineOffset].innerHTML = cloneNodes[lineOffset].innerHTML.substring(0, charOffset)
-            const newLineElement = document.createElement('p')
-            newLineElement.appendChild(document.createTextNode(newLineText))
-            cloneNodes = [...cloneNodes.slice(0, lineOffset + 1), newLineElement, ...cloneNodes.slice(lineOffset + 1)]
-            setSelectionOffset({ lineOffset: lineOffset + 1, charOffset: 0 })
-          } else if (e.key === 'Backspace') {
-            if (charOffset > 0) {
-              cloneNodes[lineOffset].innerHTML = cloneNodes[lineOffset].innerHTML.substring(0, charOffset - 1) + cloneNodes[lineOffset].innerHTML.substring(charOffset)
-              setSelectionOffset({ lineOffset: lineOffset, charOffset: charOffset - 1 })
-            } else {
-              if (lineOffset !== 0) {
-                const prevOffset = cloneNodes[lineOffset - 1].innerHTML.length
-                cloneNodes[lineOffset - 1].innerHTML = cloneNodes[lineOffset - 1].innerHTML + cloneNodes[lineOffset].innerHTML
-                cloneNodes.splice(lineOffset, 1)
-                setSelectionOffset({ lineOffset: lineOffset - 1, charOffset: prevOffset })
-              }
-            }
-          } else {
-            cloneNodes[lineOffset].innerHTML = cloneNodes[lineOffset].innerHTML.substring(0, charOffset) + e.key + cloneNodes[lineOffset].innerHTML.substring(charOffset)
-            setSelectionOffset({ lineOffset: lineOffset, charOffset: charOffset + 1 })
-          }
-
-          setText(cloneNodes.map(node => node.outerHTML).reduce((a, b) => a + b))
-        }
-      }
-    }
+  const initialConfig = {
+    namespace: 'MyEditor',
+    onError: () => { },
+    nodes: [
+      AutoLinkNode,
+      LinkNode,
+      ListNode,
+      ListItemNode,
+      HeadingNode,
+      QuoteNode,
+      CodeHighlightNode,
+      CodeNode
+    ]
   }
-
-  useEffect(() => {
-    const selection = window.getSelection()
-    if (selection && elemRef.current) {
-      const range = document.createRange()
-      range.setStart(elemRef.current.children[selectionOffset.lineOffset].firstChild || elemRef.current.children[selectionOffset.lineOffset] as Node, selectionOffset.charOffset)
-      range.collapse(true)
-
-      selection.removeAllRanges()
-      selection.addRange(range)
-    }
-  }, [text])
 
   return (
     <Container>
-      <Controls>
-        <Bold active={boldActive} onClick={() => setBoldActive(!boldActive)}>B</Bold>
-        <Italic active={italicActive} onClick={() => setItalicActive(!italicActive)}>i</Italic>
-        <EditorButton active={bulletsActive} onClick={() => setBulletsActive(!bulletsActive)}>
-          <Icon render="bullets" />
-        </EditorButton>
-        <Link active={linkActive} onClick={() => setLinkActive(!linkActive)}>
-          <Icon render="link" />
-          <LinkInput placeholder='Enter url' value={linkInput} onChange={e => setLinkInput(e.target.value)} onClick={e => e.stopPropagation()} />
-          <LinkSubmit render="tick" />
-        </Link>
-      </Controls>
       <Editor>
-        <EditableRichText ref={elemRef} contentEditable onKeyDown={(e: KeyboardEvent<HTMLDivElement>) => handleChange(e)} htmlString={text} />
+        <LexicalComposer initialConfig={initialConfig}>
+          <ToolbarPlugin />
+          <RichTextPlugin
+            contentEditable={<ContentEditable className="editor-input" />}
+            placeholder={<div></div>}
+            ErrorBoundary={LexicalErrorBoundary}
+          />
+          <LinkPlugin />
+          <ListPlugin />
+          <HistoryPlugin />
+          <CustomAutoLinkPlugin />
+          <MarkdownShortcutPlugin transformers={TRANSFORMERS} />
+        </LexicalComposer>
       </Editor>
     </Container>
   )
 }
 
 const Editor = styled(Box)`
-  background-color: ${theme.colors.cream};
-  border-radius: 16px;
-  border: 2px solid ${theme.colors.oatmeal};
-  padding: 16px;
-
-  p:after {
-    content:"";
-    display:inline-block;
-    width:0px;
-    /* outline:2px solid red; // debug */
+  .editor-input {
+    margin-top: 16px;
+    background-color: ${theme.colors.cream};
+    border-radius: 12px;
+    border: 2px solid ${theme.colors.oatmeal};
+    padding: 16px;
+    * {
+      all: revert;
+      outline: none;
+    }
   }
-`
-
-const EditableRichText = styled(RichText)`
-  height: 300px;
-  overflow: scroll;
-  outline: none;
 `
 
 const Container = styled(Box)`
   background-color: ${theme.colors.coconut};
   border-radius: 16px;
-  padding: 16px;
-`
-
-const Controls = styled(Box)`
-  padding: 0 0 16px 0;
-  display: flex;
-  gap: 12px;
-`
-
-const EditorButton = styled(Box) <{ active: boolean }>`
-  height: 50px;
-  width: 50px;
-  line-height: 50px;
   padding: 12px;
-  border-radius: 25px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 24px;
-  cursor: pointer;
-  background-color: ${theme.colors.custard};
-
-  ${({ active }) => active && `
-    background-color: ${theme.colors.fairyFloss};
-  `}
-
-  :hover {
-    filter: brightness(0.95);
-
-    ${({ active }) => active && `
-      background-color: ${theme.colors.fairyFloss};
-    `}
-  }
 `
-
-const Link = styled(EditorButton)`
-  transition: width .3s;
-  ${({ active }) => active && `width: 400px;`}
-  justify-content: left;
-  overflow: hidden;
-  gap: 16px;
-  padding-right: 6px;
-
-  ${({ active }) => active && `
-    background-color: ${theme.colors.fairyFloss};
-
-    :hover {
-      filter: none;
-    }
-  `}
-`
-
-const LinkSubmit = styled(Icon)`
-  background-color: ${theme.colors.marshmallowPink};
-  height: 40px;
-  width: 40px;
-  border-radius: 20px;
-  padding: 6px;
-
-  :hover {
-    background-color: ${theme.colors.lollipop};
-  }
-`
-
-const LinkInput = styled.input`
-  width: inherit;
-  height: 40px;
-  border: none;
-  border-radius: 20px;
-  padding-left: 12px;
-  padding-right: 12px;
-  font-size: 16px;
-
-  :hover {
-    outline: 2px solid ${theme.colors.bubblegum};
-  }
-
-  :focus {
-    outline: 2px solid ${theme.colors.marshmallowPink};
-  }
-`
-
-const Bold = styled(EditorButton)`
-  font-weight: 500;
-`
-
-const Italic = styled(EditorButton)`
-  font-style: italic;
-  font-weight: 500;
-  text-indent: -4px;
-`
-
-// const Editor = styled(Textarea)`
-//   width: 100%;
-
-//   div {
-//     display: flex;
-//   }
-
-//   :focus {
-//     outline: none;
-//   }
-// `
---
+++ src/RichTextEditor/plugins/ToolbarPlugin.tsx
@@ -0,0 +1,281 @@
+import React from "react";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import { useCallback, useEffect, useRef, useState } from "react";
+import {
+  CAN_REDO_COMMAND,
+  CAN_UNDO_COMMAND,
+  SELECTION_CHANGE_COMMAND,
+  FORMAT_TEXT_COMMAND,
+  $getSelection,
+  $isRangeSelection,
+  RangeSelection,
+} from "lexical";
+import { $isLinkNode, TOGGLE_LINK_COMMAND } from "@lexical/link";
+import {
+  $isAtNodeEnd
+} from "@lexical/selection";
+import { $getNearestNodeOfType, mergeRegister } from "@lexical/utils";
+import {
+  INSERT_UNORDERED_LIST_COMMAND,
+  REMOVE_LIST_COMMAND,
+  $isListNode,
+  ListNode
+} from "@lexical/list";
+import {
+  $isHeadingNode
+} from "@lexical/rich-text";
+import styled from "styled-components";
+import { Box } from "../../Box";
+import { theme } from "../../theme";
+import { Icon } from "../../Icon";
+
+const LowPriority = 1;
+
+function getSelectedNode(selection: RangeSelection) {
+  const anchor = selection.anchor;
+  const focus = selection.focus;
+  const anchorNode = selection.anchor.getNode();
+  const focusNode = selection.focus.getNode();
+  if (anchorNode === focusNode) {
+    return anchorNode;
+  }
+  const isBackward = selection.isBackward();
+  if (isBackward) {
+    return $isAtNodeEnd(focus) ? anchorNode : focusNode;
+  } else {
+    return $isAtNodeEnd(anchor) ? focusNode : anchorNode;
+  }
+}
+
+export default function ToolbarPlugin() {
+  const [editor] = useLexicalComposerContext();
+  const toolbarRef = useRef(null);
+  const [blockType, setBlockType] = useState("paragraph");
+  const [isLink, setIsLink] = useState(false);
+  const [isBold, setIsBold] = useState(false);
+  const [isItalic, setIsItalic] = useState(false);
+  const [linkURL, setLinkURL] = useState("")
+  const [prevLinkURL, setPrevLinkURL] = useState("")
+
+  const formatBulletList = () => {
+    if (blockType !== "ul") {
+      editor.dispatchCommand(INSERT_UNORDERED_LIST_COMMAND, undefined)
+    } else {
+      editor.dispatchCommand(REMOVE_LIST_COMMAND, undefined);
+    }
+
+  };
+
+  const updateToolbar = useCallback(() => {
+    const selection = $getSelection();
+    if ($isRangeSelection(selection)) {
+      const anchorNode = selection.anchor.getNode();
+      const element =
+        anchorNode.getKey() === "root"
+          ? anchorNode
+          : anchorNode.getTopLevelElementOrThrow();
+      const elementKey = element.getKey();
+      const elementDOM = editor.getElementByKey(elementKey);
+      if (elementDOM !== null) {
+        if ($isListNode(element)) {
+          const parentList = $getNearestNodeOfType(anchorNode, ListNode);
+          const type = parentList ? parentList.getTag() : element.getTag();
+          setBlockType(type);
+        } else {
+          const type = $isHeadingNode(element)
+            ? element.getTag()
+            : element.getType();
+          setBlockType(type);
+        }
+      }
+      // Update text format
+      setIsBold(selection.hasFormat("bold"));
+      setIsItalic(selection.hasFormat("italic"));
+
+      // Update links
+      const node = getSelectedNode(selection);
+      const parent = node.getParent();
+      if ($isLinkNode(parent)) {
+        setIsLink(true);
+        setLinkURL(parent.getURL())
+      } else if ($isLinkNode(node)) {
+        setIsLink(true);
+        setLinkURL(node.getURL())
+      } else {
+        setIsLink(false);
+        setLinkURL("")
+      }
+    }
+  }, [editor]);
+
+  const urlInputRef = useRef<HTMLInputElement | null>(null)
+
+  useEffect(() => {
+    if (prevLinkURL !== "" && isLink && urlInputRef.current) {
+      urlInputRef.current.focus()
+    }
+    setPrevLinkURL(linkURL)
+  }, [linkURL])
+
+  useEffect(() => {
+    return mergeRegister(
+      editor.registerUpdateListener(({ editorState }) => {
+        editorState.read(() => {
+          updateToolbar();
+        });
+      }),
+      editor.registerCommand(
+        SELECTION_CHANGE_COMMAND,
+        (_payload, newEditor) => {
+          updateToolbar();
+          return false;
+        },
+        LowPriority
+      ),
+      editor.registerCommand(
+        CAN_UNDO_COMMAND,
+        (payload) => {
+          return false;
+        },
+        LowPriority
+      ),
+      editor.registerCommand(
+        CAN_REDO_COMMAND,
+        (payload) => {
+          return false;
+        },
+        LowPriority
+      )
+    );
+  }, [editor, updateToolbar]);
+
+  return (
+    <Toolbar className="toolbar" ref={toolbarRef}>
+
+      <Bold
+        onClick={() => {
+          editor.dispatchCommand(FORMAT_TEXT_COMMAND, "bold");
+        }}
+        active={isBold}
+      >
+        B
+      </Bold>
+      <Italic
+        onClick={() => {
+          editor.dispatchCommand(FORMAT_TEXT_COMMAND, "italic");
+        }}
+        active={isItalic}
+      >
+        i
+      </Italic>
+      <EditorButton active={blockType === "ul"} onClick={() => formatBulletList()}>
+        <Icon render="bullets" />
+      </EditorButton>
+
+      <Link active={isLink} onClick={() => !isLink ? editor.dispatchCommand(TOGGLE_LINK_COMMAND, "https://") : editor.dispatchCommand(TOGGLE_LINK_COMMAND, null)}>
+        <Box ml={{ custom: "-4px" }}>
+          <Icon render="link" />
+        </Box>
+        <LinkInput ref={urlInputRef} placeholder='Enter url' value={linkURL} onChange={e => {
+          editor.dispatchCommand(TOGGLE_LINK_COMMAND, e.target.value)
+        }} onClick={e => e.stopPropagation()} />
+        <Box onClick={e => {
+          e.stopPropagation()
+          window.open(linkURL, '_blank')
+        }}>
+          <LinkOpen render="new-window" />
+        </Box>
+      </Link>
+    </Toolbar>
+  );
+}
+
+
+const EditorButton = styled(Box) <{ active: boolean }>`
+  height: 40px;
+  width: 40px;
+  line-height: 50px;
+  padding: 12px;
+  border-radius: 25px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 24px;
+  cursor: pointer;
+  background-color: ${theme.colors.custard};
+
+  ${({ active }) => active && `
+    background-color: ${theme.colors.fairyFloss};
+  `}
+
+  :hover {
+    filter: brightness(0.95);
+
+    ${({ active }) => active && `
+      background-color: ${theme.colors.fairyFloss};
+    `}
+  }
+`
+
+const Bold = styled(EditorButton)`
+  font-weight: 500;
+`
+
+const Italic = styled(EditorButton)`
+  font-style: italic;
+  font-weight: 500;
+  text-indent: -4px;
+`
+
+const Toolbar = styled(Box)`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+`
+
+const Link = styled(EditorButton)`
+  transition: width .3s;
+  ${({ active }) => active && `width: 360px;`}
+  justify-content: left;
+  overflow: hidden;
+  gap: 12px;
+  padding-right: 5px;
+
+  ${({ active }) => active && `
+    background-color: ${theme.colors.fairyFloss};
+
+    :hover {
+      filter: none;
+    }
+  `}
+`
+
+const LinkOpen = styled(Icon)`
+  background-color: ${theme.colors.marshmallowPink};
+  height: 32px;
+  width: 32px;
+  border-radius: 20px;
+  padding: 6px;
+
+  :hover {
+    background-color: ${theme.colors.lollipop};
+  }
+`
+
+const LinkInput = styled.input`
+  width: inherit;
+  height: 32px;
+  border: none;
+  border-radius: 20px;
+  padding-left: 12px;
+  padding-right: 12px;
+  font-size: 16px;
+
+  :hover {
+    outline: 2px solid ${theme.colors.bubblegum};
+  }
+
+  :focus {
+    outline: 2px solid ${theme.colors.marshmallowPink};
+  }
+`
