f60e381b1b2059187d93295a6b4d5b4a7d370451
liampuk <liam.piesley@marshmallow.co>
2024-03-04 17:41:41 +0000

    Deploying to gh-pages from @ marshmallow-insurance/smores-react@c3fa22df5a7908453b4e87efb50eb6858012a239 🚀

--- assets/preview-BmJNM6ZM.js
+++
@@ -1,7 +0,0 @@
-import{_ as t}from"./iframe-TSbYQGcM.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-DQzi8YfY.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6,7]),import.meta.url);return new r}}};export{_ as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-DQzi8YfY.js","./iframe-TSbYQGcM.js","./index-BBkUAzwr.js","./react-18-B-OKcmzb.js","./index-PqR-_bA4.js","./polished.esm-D0h1-lPB.js","./_getPrototype-WWAa0_g7.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
