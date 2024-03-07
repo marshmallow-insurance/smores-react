e4b5743556fa617c37ac24ba79966ba04238cfe5
Liam Piesley <liam.piesley@marshmallow.co>
2024-03-07 16:02:14 +0000

    Fix prevent rte toolbar buttons squishing (#3548)
    * fix: release @lexica/react package.json change and RichTextEditor outline prop
    
    * fix: add build, check types and test action on pr
    
    * run on all prs
    
    * fix build
    
    * fix: prevent RTE toolbar buttons squishing when expanding link
    
    * fix error caused by tabbing to link input

--- src/RichTextEditor/plugins/ToolbarPlugin.tsx
+++ src/RichTextEditor/plugins/ToolbarPlugin.tsx
@@ -176,7 +176,7 @@ export default function ToolbarPlugin() {
         <Box ml={{ custom: "-4px" }}>
           <Icon render="link" />
         </Box>
-        <LinkInput ref={urlInputRef} placeholder='Enter url' value={linkURL} onChange={e => {
+        <LinkInput tabIndex={-1} ref={urlInputRef} placeholder='Enter url' value={linkURL} onChange={e => {
           editor.dispatchCommand(TOGGLE_LINK_COMMAND, e.target.value)
         }} onClick={e => e.stopPropagation()} />
         <Box onClick={e => {
@@ -203,6 +203,7 @@ const EditorButton = styled(Box) <{ $active: boolean }>`
   font-size: 24px;
   cursor: pointer;
   background-color: ${theme.colors.custard};
+  flex-shrink: 0;
 
   ${({ $active }) => $active && `
     background-color: ${theme.colors.fairyFloss};
@@ -240,6 +241,7 @@ const Link = styled(EditorButton)`
   overflow: hidden;
   gap: 12px;
   padding-right: 5px;
+  flex-shrink: 1;
 
   ${({ $active }) => $active && `
     background-color: ${theme.colors.fairyFloss};
