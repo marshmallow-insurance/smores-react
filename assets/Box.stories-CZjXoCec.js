2865b6bc0a38b402a1b735ae30cd4d19fe13ad79
liampuk <liam.piesley@marshmallow.co>
2024-03-07 16:03:13 +0000

    Deploying to gh-pages from @ marshmallow-insurance/smores-react@e4b5743556fa617c37ac24ba79966ba04238cfe5 🚀

---
+++ assets/Box.stories-CZjXoCec.js
@@ -0,0 +1,15 @@
+import{j as e,a as m}from"./styled-components.browser.esm-CskBwHif.js";import{I as x}from"./Icon-BYvmBiBo.js";import{T as n}from"./Text-Bg2AI3kI.js";import{t as f}from"./theme-D50qMPxU.js";import{B as o}from"./Box-C7hh6OC3.js";import"./index-BBkUAzwr.js";import"./polished.esm-D0h1-lPB.js";import"./focusOutline-JAHtHrmh.js";const j={title:"Box",component:o},h=u=>e(o,{...u,children:e(n,{children:"Some lovely content."})}),r=h.bind({});r.args={flex:!0,direction:"row",justifyContent:"flex-start",alignItems:"flex-starts",m:"16px",p:"16px"};r.argTypes={theme:{table:{disable:!0}},as:{table:{disable:!0}},forwardedAs:{table:{disable:!0}}};const t=()=>m(o,{px:{small:"12px",medium:"24px"},pt:{small:"8px",medium:"16px"},pb:{small:"24px",medium:"32px"},style:{backgroundColor:f.colors.coconut,borderRadius:12},children:[m(o,{flex:!0,direction:{small:"column",medium:"row"},gap:{small:{custom:2},medium:"8px"},alignItems:{medium:"center"},mb:"8px",children:[e(x,{render:"info",size:24}),e(n,{typo:"heading-small",children:"Card content."})]}),e(n,{color:"sesame",children:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s."})]}),b=()=>m(o,{flex:!0,direction:{small:"column",medium:"row"},gap:{small:"12px",medium:"16px"},children:[e(t,{}),e(t,{}),e(t,{})]}),s=b.bind({});var a,i,l;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`(props: BoxProps) => <Box {...props}>
+    <Text>Some lovely content.</Text>
+  </Box>`,...(l=(i=r.parameters)==null?void 0:i.docs)==null?void 0:l.source}}};var p,d,c;s.parameters={...s.parameters,docs:{...(p=s.parameters)==null?void 0:p.docs,source:{originalSource:`() => {
+  return <Box flex direction={{
+    small: 'column',
+    medium: 'row'
+  }} gap={{
+    small: '12px',
+    medium: '16px'
+  }}>
+      <Card />
+      <Card />
+      <Card />
+    </Box>;
+}`,...(c=(d=s.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};const R=["Default","ResponsiveProps"];export{r as Default,s as ResponsiveProps,R as __namedExportsOrder,j as default};
---
+++ assets/Tooltip.stories-CF0KDi6q.js
@@ -0,0 +1,61 @@
+import{s as z,j as o,a as c}from"./styled-components.browser.esm-CskBwHif.js";import{r as q}from"./index-BBkUAzwr.js";import{B as e}from"./Box-C7hh6OC3.js";import{B as L}from"./Button-BL2-l4wJ.js";import{M as P}from"./Modal-Bg9Vin4o.js";import{T as t}from"./Text-Bg2AI3kI.js";import{t as D}from"./theme-D50qMPxU.js";import{T as p}from"./Tooltip-DRJ92zGz.js";import"./Icon-BYvmBiBo.js";import"./Loader-BwvOIFW8.js";import"./focusOutline-JAHtHrmh.js";import"./index-PqR-_bA4.js";import"./IconStrict-DznEAtdO.js";import"./polished.esm-D0h1-lPB.js";import"./v4-D8aEg3BZ.js";const ro={title:"Tooltip",component:p},d=r=>o(e,{px:{custom:250},py:{custom:150},flex:!0,children:o(p,{...r,children:o(e,{children:"Harry Hill"})})}),I=r=>c(R,{my:"64px",children:[o(p,{...r,children:o(e,{children:"Harry Hill"})}),o(j,{mt:"12px",children:"I am some super long text, that should be clipped"}),o(e,{my:"64px",children:o(p,{...r,children:o(e,{children:"Harry Hill"})})})]}),R=z(e)`
+  height: 100px;
+  width: 200px;
+  overflow: scroll;
+  background: ${D.colors.blueberry};
+`,j=z(t)`
+  white-space: nowrap;
+`,l=d.bind({});l.args={title:"Harry Hill",content:"Tooltip content, this is some text, maybe even more text if you feel like it.",size:"small",underline:!0,fallbackStyle:!1};const n=d.bind({}),_=o(t,{typo:"desc-light",color:"liquorice",children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."});n.args={title:"Long text example",content:_,size:"large",underline:!0,shadow:!1};const s=d.bind({}),m=c(e,{children:[o(t,{typo:"desc-light",color:"liquorice",children:"20 Cherry Blossom Lane,"}),o(t,{typo:"desc-light",color:"liquorice",children:"Aintree,"}),o(t,{typo:"desc-light",color:"liquorice",children:"Manchester,"}),o(t,{typo:"desc-light",color:"liquorice",children:"T: 07123456789"})]});s.args={title:"React node example",content:m,size:"large",underline:!0,shadow:!1};const i=I.bind({});i.args={title:"React node example",content:m,size:"large",underline:!0};const N=r=>{const[x,O]=q.useState(!1),u=()=>{O(!x)};return c(e,{height:"900px",children:[o(P,{handleClick:u,showModal:x,children:o(p,{...r,zIndex:1e3,children:o(e,{children:"Harry Hill"})})}),o(L,{primary:!0,handleClick:u,children:"Show Modal with Mobile Drawer"})]})},a=N.bind({});a.args={title:"Modal example",content:m,size:"large",underline:!0};var h,T,y;l.parameters={...l.parameters,docs:{...(h=l.parameters)==null?void 0:h.docs,source:{originalSource:`(props: TooltipProps) => <Box px={{
+  custom: 250
+}} py={{
+  custom: 150
+}} flex>
+    <Tooltip {...props}>
+      <Box>Harry Hill</Box>
+    </Tooltip>
+  </Box>`,...(y=(T=l.parameters)==null?void 0:T.docs)==null?void 0:y.source}}};var B,g,f;n.parameters={...n.parameters,docs:{...(B=n.parameters)==null?void 0:B.docs,source:{originalSource:`(props: TooltipProps) => <Box px={{
+  custom: 250
+}} py={{
+  custom: 150
+}} flex>
+    <Tooltip {...props}>
+      <Box>Harry Hill</Box>
+    </Tooltip>
+  </Box>`,...(f=(g=n.parameters)==null?void 0:g.docs)==null?void 0:f.source}}};var w,H,M;s.parameters={...s.parameters,docs:{...(w=s.parameters)==null?void 0:w.docs,source:{originalSource:`(props: TooltipProps) => <Box px={{
+  custom: 250
+}} py={{
+  custom: 150
+}} flex>
+    <Tooltip {...props}>
+      <Box>Harry Hill</Box>
+    </Tooltip>
+  </Box>`,...(M=(H=s.parameters)==null?void 0:H.docs)==null?void 0:M.source}}};var b,S,C;i.parameters={...i.parameters,docs:{...(b=i.parameters)==null?void 0:b.docs,source:{originalSource:`(props: TooltipProps) => <OverflowHiddenBox my="64px">
+    <Tooltip {...props}>
+      <Box>Harry Hill</Box>
+    </Tooltip>
+
+    <ClippedText mt="12px">
+      I am some super long text, that should be clipped
+    </ClippedText>
+
+    <Box my="64px">
+      <Tooltip {...props}>
+        <Box>Harry Hill</Box>
+      </Tooltip>
+    </Box>
+  </OverflowHiddenBox>`,...(C=(S=i.parameters)==null?void 0:S.docs)==null?void 0:C.source}}};var E,k,v;a.parameters={...a.parameters,docs:{...(E=a.parameters)==null?void 0:E.docs,source:{originalSource:`(props: TooltipProps) => {
+  const [showModal, setShowModal] = useState(false);
+  const handleClick = () => {
+    setShowModal(!showModal);
+  };
+  return <Box height="900px">
+      <Modal handleClick={handleClick} showModal={showModal}>
+        <Tooltip {...props} zIndex={1000}>
+          <Box>Harry Hill</Box>
+        </Tooltip>
+      </Modal>
+      <Button primary handleClick={handleClick}>
+        Show Modal with Mobile Drawer
+      </Button>
+    </Box>;
+}`,...(v=(k=a.parameters)==null?void 0:k.docs)==null?void 0:v.source}}};const to=["Default","LongTextExample","ReactNodeExample","OverflowExample","ModalExample"];export{l as Default,n as LongTextExample,a as ModalExample,i as OverflowExample,s as ReactNodeExample,to as __namedExportsOrder,ro as default};
