e3000eca94f107c0cff74a88905b6d08f3daed04
FrelliBB <francesco.borgbonaci@marshmallow.co>
2024-03-14 12:30:22 +0000

    Deploying to gh-pages from @ marshmallow-insurance/smores-react@ef9bd8d186790748f1f90fa463e6b874f5a4db6f 🚀

---
+++ assets/Icon.stories-B0Zyx19N.js
@@ -0,0 +1,7 @@
+import{s as l,j as t,a as d}from"./styled-components.browser.esm-CskBwHif.js";import{i as u,I as s}from"./Icon-CpMNXCuZ.js";import"./index-BBkUAzwr.js";import"./Box-Ck8x80mg.js";import"./theme-D50qMPxU.js";const f=()=>t("div",{children:Object.keys(u).map(e=>d(b,{children:[t("span",{children:e}),t(s,{render:e})]},e))}),b=l.div`
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10px;
+  width: 250px;
+`,_={title:"Icon",component:s},x=e=>t(s,{...e}),r=x.bind({});r.args={render:"info",size:32,color:"liquorice",rotate:"0",mt:"0",mr:"0",mb:"0",ml:"0"};const g=()=>t(f,{}),o=g.bind({});o.args={size:24,color:"liquorice",rotate:"0",mt:"0",mr:"0",mb:"",ml:"0"};var a,n,c;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:"(props: IconProps) => <Icon {...props} />",...(c=(n=r.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};var i,m,p;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:"() => <Container />",...(p=(m=o.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const q=["Default","Collection"];export{o as Collection,r as Default,q as __namedExportsOrder,_ as default};
