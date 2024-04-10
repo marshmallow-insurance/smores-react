868f4af712eb52aaf3b85528ca1cf1480ea201ba
rowanhen <rowan.henseleit@marshmallow.co>
2024-04-10 11:42:10 +0100

    remove mention of smores

---
+++ releaserc.config.js
@@ -0,0 +1,101 @@
+const ref = process.env.GITHUB_REF
+const branch = ref.split('/').pop()
+
+const config = {
+  branches: [
+    'main',
+    {
+      name: 'crumbs-*',
+      prerelease: true,
+    },
+  ],
+  repositoryUrl: 'https://github.com/marshmallow-insurance/frames.git',
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { type: 'feat', release: 'minor' },
+          { type: 'fix', release: 'patch' },
+          { type: 'docs', release: 'patch' },
+          { type: 'bump', release: 'patch' },
+          { type: 'dependabot', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
+          { type: 'style', release: 'patch' },
+          { type: 'perf', release: 'patch' },
+          { type: 'test', release: 'patch' },
+          { type: 'revert', release: 'patch' },
+        ],
+        parserOpts: {
+          noteKeywords: [
+            'BREAKING CHANGE',
+            'BREAKING-CHANGE',
+            'BREAKING CHANGES',
+            'BREAKING-CHANGES',
+          ],
+        },
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        presetConfig: {
+          types: [
+            { type: 'feat', section: 'Features' },
+            { type: 'fix', section: 'Bug Fixes' },
+            { type: 'docs', section: 'Documentation' },
+            { type: 'bump', hidden: true },
+            { type: 'dependabot', hidden: true },
+            { type: 'style', section: 'Styles' },
+            { type: 'refactor', section: 'Refactors' },
+            { type: 'perf', section: 'Performance Improvements' },
+            { type: 'test', section: 'Tests' },
+            { type: 'revert', hidden: true },
+            { type: '*', section: 'Others' },
+          ],
+        },
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: ['dist'],
+      },
+    ],
+    ['@semantic-release/changelog'],
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: true,
+        pkgRoot: '.',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+}
+
+const isPrereleaseBranch = config.branches.some(
+  (b) => typeof b === 'object' && b.name === branch && b.prerelease,
+)
+
+if (!isPrereleaseBranch) {
+  config.plugins.push([
+    '@semantic-release/git',
+    {
+      assets: ['CHANGELOG.md'],
+      message:
+        'chore(release): ${nextRelease.version} \n\n${nextRelease.notes}',
+    },
+  ])
+}
+
+export default config
