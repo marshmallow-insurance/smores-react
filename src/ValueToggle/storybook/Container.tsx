22339dab61669694640891ffdac4cbdabe70c7dd
snegyeliczky <sandor.negyeliczky@marshmallow.co>
2024-05-03 10:04:27 +0200

    feat(Container): comment

--- src/ValueToggle/storybook/Container.tsx
+++ src/ValueToggle/storybook/Container.tsx
@@ -2,6 +2,8 @@ import { ValueToggle, ValueToggleProps } from '../ValueToggle'
 import React from 'react'
 
 export const Container = <T,>(props: ValueToggleProps<T>) => {
+  // recommend to use a useState or a form to store the selected value above the component
   const [value, setValue] = React.useState(props.value)
+
   return <ValueToggle {...props} value={value} onChange={setValue} />
 }
