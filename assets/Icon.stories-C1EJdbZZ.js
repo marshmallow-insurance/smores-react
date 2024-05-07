8cde5659381fd98ca1b53d872e10493280472f68
matdiak <mat.diak@marshmallow.co>
2024-05-07 12:03:58 +0000

    Deploying to gh-pages from @ marshmallow-insurance/smores-react@ec2308a5f75e2c79e6b5c922d53e4e265c7ea378 🚀

---
+++ assets/Icon.stories-C1EJdbZZ.js
@@ -0,0 +1,7 @@
+import{R as e}from"./index-BBkUAzwr.js";import{i as d,I as a}from"./Icon-pvgJCDYX.js";import{u}from"./styled-components.browser.esm-uaFiJyE8.js";import"./Box-BsRHaABa.js";import"./theme-D50qMPxU.js";const l=()=>e.createElement("div",null,Object.keys(d).map(t=>e.createElement(x,{key:t},e.createElement("span",null,t),e.createElement(a,{render:t})))),x=u.div`
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10px;
+  width: 250px;
+`;l.__docgenInfo={description:"",methods:[],displayName:"Container"};const _={title:"Icon",component:a},f=t=>e.createElement(a,{...t}),r=f.bind({});r.args={render:"info",size:32,color:"liquorice",rotate:"0",mt:"8px",mr:"8px",mb:"8px",ml:"8px"};const b=()=>e.createElement(l,null),o=b.bind({});o.args={size:24,color:"liquorice",rotate:"0",mt:"8px",mr:"8px",mb:"8px",ml:"8px"};var n,s,c;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:"(props: IconProps) => <Icon {...props} />",...(c=(s=r.parameters)==null?void 0:s.docs)==null?void 0:c.source}}};var p,m,i;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:"() => <Container />",...(i=(m=o.parameters)==null?void 0:m.docs)==null?void 0:i.source}}};const h=["Default","Collection"];export{o as Collection,r as Default,h as __namedExportsOrder,_ as default};
