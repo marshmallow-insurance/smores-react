2576357886656b63c021f859daae19b5bef2e23e
rowanhen <rowan.henseleit@marshmallow.co>
2024-05-13 15:55:25 +0100

    chore: remove unneeded snapshot tests

--- src/Card/__tests__/Card.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react'
-import React from 'react'
-
-import { Card } from '../Card'
-
-test('renders', () => {
-  const { container } = render(<Card>Text inside</Card>)
-  expect(container.firstChild).toMatchSnapshot()
-})
--- src/IconStrict/__tests__/IconStrict.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react'
-import React from 'react'
-
-import { IconStrict } from '../IconStrict'
-
-test('renders', () => {
-  const { container } = render(<IconStrict render="at" size={48} />)
-  expect(container.firstChild).toMatchSnapshot()
-})
