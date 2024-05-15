af53b1c0e9da1c46ae9d63b76c89ceef6a37edc6
rowanhen <rowan.henseleit@marshmallow.co>
2024-05-15 18:07:55 +0100

    chore: re-deleting snapshot tests

--- src/Button/__tests__/Button.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react'
-import React from 'react'
-
-import { Button } from '../Button'
-
-test('renders', () => {
-  const { container } = render(<Button>Test</Button>)
-  expect(container.firstChild).toMatchSnapshot()
-})
