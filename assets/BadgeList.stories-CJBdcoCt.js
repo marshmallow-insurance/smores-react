bd9e6588246eb1e1583c260d7aa8dd064f17c7ff
david-gomes5 <63399235+david-gomes5@users.noreply.github.com>
2024-06-04 14:41:43 +0000

    Deploying to gh-pages from @ marshmallow-insurance/smores-react@0f59cf4800a6fe9e0c8a47a2d4a3992eebd2f458 🚀

---
+++ assets/BadgeList.stories-CJBdcoCt.js
@@ -0,0 +1,39 @@
+import{R as n}from"./index-uCp2LrAq.js";import{B as s,b as o}from"./storybook-data-DYoXhi4e.js";import{B as g}from"./index-CdIuDNv2.js";import{p as f}from"./styled-components.browser.esm-Bmiumwae.js";import"./index-4gTQz8uc.js";import{T as h}from"./Tooltip-DxAQigke.js";import"./_commonjsHelpers-BosuxZz1.js";import"./theme-BeIETfSH.js";import"./index-BOkhicXD.js";import"./index-BJvyI_-D.js";import"./Text-CyPVEhU2.js";import"./polished.esm-DlUGb9xG.js";import"./index-DfzELUFi.js";import"./Icon-BayaxWfO.js";import"./focusOutline-BRfd_C63.js";import"./index-D0L42zHO.js";import"./index-BERGJJ82.js";function e(){var A="/home/runner/work/smores-react/smores-react/src/BadgeList/BadgeList.tsx",a="aa9026b38c77f501763d145f48b5409be347e89a",l=window,t="__coverage__",v={path:"/home/runner/work/smores-react/smores-react/src/BadgeList/BadgeList.tsx",statementMap:{0:{start:{line:7,column:25},end:{line:7,column:48}},1:{start:{line:8,column:2},end:{line:14,column:6}},2:{start:{line:8,column:117},end:{line:14,column:3}},3:{start:{line:16,column:20},end:{line:29,column:1}},4:{start:{line:17,column:2},end:{line:27,column:3}},5:{start:{line:18,column:4},end:{line:26,column:7}},6:{start:{line:28,column:2},end:{line:28,column:82}},7:{start:{line:30,column:18},end:{line:41,column:1}},8:{start:{line:42,column:0},end:{line:42,column:3040}}},fnMap:{0:{name:"BadgeList",decl:{start:{line:6,column:16},end:{line:6,column:25}},loc:{start:{line:6,column:38},end:{line:15,column:1}},line:6},1:{name:"(anonymous_1)",decl:{start:{line:8,column:83},end:{line:8,column:84}},loc:{start:{line:8,column:117},end:{line:14,column:3}},line:8},2:{name:"(anonymous_2)",decl:{start:{line:16,column:20},end:{line:16,column:21}},loc:{start:{line:16,column:58},end:{line:29,column:1}},line:16}},branchMap:{0:{loc:{start:{line:17,column:2},end:{line:27,column:3}},type:"if",locations:[{start:{line:17,column:2},end:{line:27,column:3}},{start:{line:void 0,column:void 0},end:{line:void 0,column:void 0}}],line:17},1:{loc:{start:{line:21,column:18},end:{line:21,column:46}},type:"binary-expr",locations:[{start:{line:21,column:18},end:{line:21,column:34}},{start:{line:21,column:38},end:{line:21,column:46}}],line:21}},s:{0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0},f:{0:0,1:0,2:0},b:{0:[0,0],1:[0,0]},inputSourceMap:{version:3,file:null,sources:["/home/runner/work/smores-react/smores-react/src/BadgeList/BadgeList.tsx"],names:[],mappings:"AAAA,OAAA,WAAA;AACA,SAAA,aAAA;AACA,SAAA,WAAA;AACA,OAAA,YAAA;AACA,SAAA,eAAA;AAaA,gBAAA,UAAA,EAAA,OAAA,GAAA;AACA,QAAA,iBAAA,OAAA,SAAA,KAAA;AAEA,SACA,oCAAA,aAAA,MAAA,QACA,OAAA,IAAA,CAAA,OAAA,UACA;AAAA,IAAA;AAAA;AAAA,MACA,KAAA,MAAA;AAAA,MACA,OAAA,EAAA,GAAA,OAAA,QAAA,iBAAA,QAAA,GAAA;AAAA;AAAA,EACA,CACA,CACA;AAEA;AAMA,MAAA,cAAA,CAAA,EAAA,OAAA,EAAA,SAAA,GAAA,MAAA,EAAA,MAAA;AACA,MAAA,SAAA;AACA,WACA,oCAAA,SAAA,OAAA,EAAA,QAAA,MAAA,OAAA,KACA;AAAA,MAAA;AAAA;AAAA,QACA,UAAA,QAAA,YAAA;AAAA,QACA,OAAA,SAAA;AAAA,QACA,SAAA,QAAA;AAAA;AAAA,MAEA,oCAAA,SAAA,KAAA,MAAA,KAAA,GAAA,OAAA;AAAA,IACA,CACA;AAAA,EAEA;AAEA,SAAA,oCAAA,SAAA,KAAA,MAAA,KAAA,GAAA,OAAA;AACA;AAEA,MAAA,YAAA,OAAA,GAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;;"},_coverageSchema:"1a1c01bbd47fc00a2c39e90264f33305004495a9",hash:"aa9026b38c77f501763d145f48b5409be347e89a"},r=l[t]||(l[t]={});(!r[A]||r[A].hash!==a)&&(r[A]=v);var m=r[A];return e=function(){return m},m}e();function p({badges:A}){e().f[0]++;const a=(e().s[0]++,A.length*10+10);return e().s[1]++,n.createElement(C,{flex:!0},A.map((l,t)=>(e().f[1]++,e().s[2]++,n.createElement(b,{key:l.src,badge:{...l,zIndex:a-t*10}}))))}e().s[3]++;const b=({badge:{tooltip:A,...a}})=>(e().f[2]++,e().s[4]++,A?(e().b[0][0]++,e().s[5]++,n.createElement("div",{style:{zIndex:a.zIndex}},n.createElement(h,{position:(e().b[1][0]++,A.position??(e().b[1][1]++,"bottom")),title:A==null?void 0:A.title,content:A.content},n.createElement(s,{key:a.src,...a})))):(e().b[0][1]++,e().s[6]++,n.createElement(s,{key:a.src,...a}))),C=(e().s[7]++,f(g)`
+  & > * {
+    transition: margin-right 0.2s ease-in-out;
+    margin-right: -10px;
+  }
+
+  &:hover {
+    & > * {
+      margin-right: 5px;
+    }
+  }
+`);e().s[8]++;p.__docgenInfo={description:"",methods:[],displayName:"BadgeList",props:{badges:{required:!0,tsType:{name:"Array",elements:[{name:"Omit",elements:[{name:"intersection",raw:"BadgeProps & { tooltip?: WithTooltip }",elements:[{name:"signature",type:"object",raw:`{
+    borderColour?: Color;
+    size?: BadgeSize;
+    src: string;
+    disabled?: boolean;
+    zIndex?: number;
+}`,signature:{properties:[{key:"borderColour",value:{name:"union",raw:"keyof typeof colors",elements:[{name:"literal",value:"lollipop"},{name:"literal",value:"marshmallowPink"},{name:"literal",value:"bubblegum"},{name:"literal",value:"fairyFloss"},{name:"literal",value:"boba"},{name:"literal",value:"liquorice"},{name:"literal",value:"sesame"},{name:"literal",value:"chia"},{name:"literal",value:"custard"},{name:"literal",value:"mascarpone"},{name:"literal",value:"coconut"},{name:"literal",value:"cream"},{name:"literal",value:"spearmint"},{name:"literal",value:"feijoa"},{name:"literal",value:"blueberry"},{name:"literal",value:"macaroon"},{name:"literal",value:"pistachio"},{name:"literal",value:"matcha"},{name:"literal",value:"caramel"},{name:"literal",value:"peanut"},{name:"literal",value:"marzipan"},{name:"literal",value:"oatmeal"},{name:"literal",value:"strawberry"},{name:"literal",value:"watermelon"},{name:"literal",value:"apple"},{name:"literal",value:"mint"},{name:"literal",value:"lemon"},{name:"literal",value:"sherbert"},{name:"literal",value:"tangerine"},{name:"literal",value:"compareTheMarket"},{name:"literal",value:"confused"},{name:"literal",value:"onfido"},{name:"literal",value:"twitter"},{name:"literal",value:"premfina"},{name:"literal",value:"checkout"},{name:"literal",value:"facebook"},{name:"literal",value:"stripe"},{name:"literal",value:"intercom"},{name:"literal",value:"ravelin"},{name:"literal",value:"rac"},{name:"literal",value:"hometree"}],required:!1}},{key:"size",value:{name:"BadgeSize",required:!1}},{key:"src",value:{name:"string",required:!0}},{key:"disabled",value:{name:"boolean",required:!1}},{key:"zIndex",value:{name:"number",required:!1}}]}},{name:"signature",type:"object",raw:"{ tooltip?: WithTooltip }",signature:{properties:[{key:"tooltip",value:{name:"WithTooltip",required:!1}}]}}]},{name:"literal",value:"'zIndex'"}],raw:"Omit<BadgeListBadge, 'zIndex'>"}],raw:"Omit<BadgeListBadge, 'zIndex'>[]"},description:""}}};const K={title:"BadgeList",component:p},i={args:{badges:[{src:o,tooltip:{title:"This is a tooltip",content:"This is the content of the tooltip"}},{src:o,disabled:!0},{src:o,disabled:!0,tooltip:{title:"This is a tooltip",content:"This is the content of the tooltip"}}]}};var c,u,d;i.parameters={...i.parameters,docs:{...(c=i.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    badges: [{
+      src: BadgeSrcExample,
+      tooltip: {
+        title: 'This is a tooltip',
+        content: 'This is the content of the tooltip'
+      }
+    }, {
+      src: BadgeSrcExample,
+      disabled: true
+    }, {
+      src: BadgeSrcExample,
+      disabled: true,
+      tooltip: {
+        title: 'This is a tooltip',
+        content: 'This is the content of the tooltip'
+      }
+    }]
+  }
+}`,...(d=(u=i.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};const W=["Primary"];export{i as Primary,W as __namedExportsOrder,K as default};
+//# sourceMappingURL=BadgeList.stories-CJBdcoCt.js.map
