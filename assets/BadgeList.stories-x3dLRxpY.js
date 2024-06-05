1e0f939301c5f6c570d0e573f88b9f1b1098e9d4
barnimolnar <barnabas.molnar@marshmallow.co>
2024-06-05 11:48:03 +0000

    Deploying to gh-pages from @ marshmallow-insurance/smores-react@3decd361886ba329c7acd40cfa7d61ceacc20004 🚀

---
+++ assets/BadgeList.stories-x3dLRxpY.js
@@ -0,0 +1,108 @@
+import{R as t,r as T}from"./index-uCp2LrAq.js";import{B as d,b as l,a as y}from"./storybook-data-B9cfw7Z9.js";import{B as Q}from"./index-CdIuDNv2.js";import{p as M}from"./styled-components.browser.esm-Bmiumwae.js";import"./index-B3fF4qW5.js";import"./index-BJvyI_-D.js";import{t as O}from"./theme-BeIETfSH.js";import{T as I}from"./Tooltip-Dccp39qn.js";import{T as z}from"./Text-CyPVEhU2.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BOkhicXD.js";import"./index-D0L42zHO.js";import"./index-BERGJJ82.js";import"./polished.esm-DlUGb9xG.js";import"./index-DfzELUFi.js";import"./Icon-BayaxWfO.js";import"./focusOutline-BRfd_C63.js";function A(){var e="/home/runner/work/smores-react/smores-react/src/BadgeList/BadgeList.tsx",n="55da2f2a4c87378aad9881dc48d7d81b14f6d469",s=window,i="__coverage__",r={path:"/home/runner/work/smores-react/smores-react/src/BadgeList/BadgeList.tsx",statementMap:{0:{start:{line:9,column:25},end:{line:9,column:48}},1:{start:{line:12,column:4},end:{line:12,column:72}},2:{start:{line:14,column:20},end:{line:14,column:46}},3:{start:{line:15,column:2},end:{line:34,column:5}},4:{start:{line:15,column:137},end:{line:25,column:3}},5:{start:{line:37,column:2},end:{line:37,column:181}},6:{start:{line:39,column:29},end:{line:47,column:1}},7:{start:{line:48,column:20},end:{line:83,column:1}},8:{start:{line:49,column:30},end:{line:49,column:45}},9:{start:{line:50,column:27},end:{line:50,column:47}},10:{start:{line:50,column:33},end:{line:50,column:47}},11:{start:{line:51,column:27},end:{line:51,column:48}},12:{start:{line:51,column:33},end:{line:51,column:48}},13:{start:{line:52,column:21},end:{line:52,column:57}},14:{start:{line:53,column:2},end:{line:72,column:3}},15:{start:{line:54,column:4},end:{line:71,column:6}},16:{start:{line:73,column:2},end:{line:82,column:4}},17:{start:{line:84,column:18},end:{line:96,column:1}},18:{start:{line:97,column:0},end:{line:97,column:3329}}},fnMap:{0:{name:"BadgeList",decl:{start:{line:8,column:16},end:{line:8,column:25}},loc:{start:{line:8,column:51},end:{line:35,column:1}},line:8},1:{name:"(anonymous_1)",decl:{start:{line:15,column:103},end:{line:15,column:104}},loc:{start:{line:15,column:137},end:{line:25,column:3}},line:15},2:{name:"ExcessBadge",decl:{start:{line:36,column:9},end:{line:36,column:20}},loc:{start:{line:36,column:33},end:{line:38,column:1}},line:36},3:{name:"(anonymous_3)",decl:{start:{line:48,column:20},end:{line:48,column:21}},loc:{start:{line:48,column:58},end:{line:83,column:1}},line:48},4:{name:"(anonymous_4)",decl:{start:{line:50,column:27},end:{line:50,column:28}},loc:{start:{line:50,column:33},end:{line:50,column:47}},line:50},5:{name:"(anonymous_5)",decl:{start:{line:51,column:27},end:{line:51,column:28}},loc:{start:{line:51,column:33},end:{line:51,column:48}},line:51}},branchMap:{0:{loc:{start:{line:12,column:4},end:{line:12,column:72}},type:"binary-expr",locations:[{start:{line:12,column:4},end:{line:12,column:9}},{start:{line:12,column:14},end:{line:12,column:71}}],line:12},1:{loc:{start:{line:12,column:14},end:{line:12,column:71}},type:"cond-expr",locations:[{start:{line:12,column:42},end:{line:12,column:67}},{start:{line:12,column:70},end:{line:12,column:71}}],line:12},2:{loc:{start:{line:14,column:20},end:{line:14,column:46}},type:"cond-expr",locations:[{start:{line:14,column:28},end:{line:14,column:37}},{start:{line:14,column:40},end:{line:14,column:46}}],line:14},3:{loc:{start:{line:18,column:11},end:{line:18,column:60}},type:"cond-expr",locations:[{start:{line:18,column:43},end:{line:18,column:52}},{start:{line:18,column:55},end:{line:18,column:60}}],line:18},4:{loc:{start:{line:25,column:6},end:{line:34,column:3}},type:"binary-expr",locations:[{start:{line:25,column:6},end:{line:25,column:28}},{start:{line:25,column:32},end:{line:25,column:52}},{start:{line:25,column:72},end:{line:34,column:3}}],line:25},5:{loc:{start:{line:52,column:22},end:{line:52,column:46}},type:"cond-expr",locations:[{start:{line:52,column:32},end:{line:52,column:41}},{start:{line:52,column:44},end:{line:52,column:46}}],line:52},6:{loc:{start:{line:53,column:2},end:{line:72,column:3}},type:"if",locations:[{start:{line:53,column:2},end:{line:72,column:3}},{start:{line:void 0,column:void 0},end:{line:void 0,column:void 0}}],line:53},7:{loc:{start:{line:65,column:20},end:{line:65,column:48}},type:"binary-expr",locations:[{start:{line:65,column:20},end:{line:65,column:36}},{start:{line:65,column:40},end:{line:65,column:48}}],line:65}},s:{0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0,15:0,16:0,17:0,18:0},f:{0:0,1:0,2:0,3:0,4:0,5:0},b:{0:[0,0],1:[0,0],2:[0,0],3:[0,0],4:[0,0,0],5:[0,0],6:[0,0],7:[0,0]},inputSourceMap:{version:3,file:null,sources:["/home/runner/work/smores-react/smores-react/src/BadgeList/BadgeList.tsx"],names:[],mappings:"AAAA,OAAA,SAAA,gBAAA;AACA,SAAA,aAAA;AACA,SAAA,WAAA;AACA,OAAA,YAAA;AACA,SAAA,eAAA;AAEA,SAAA,YAAA;AACA,SAAA,aAAA;AAcA,gBAAA,UAAA,EAAA,QAAA,OAAA,KAAA,GAAA;AACA,QAAA,iBAAA,OAAA,SAAA,KAAA;AACA,QAAA;AAAA;AAAA,IAEA,UAAA,OAAA,SAAA,IAAA,QAAA,OAAA,SAAA,QAAA,IAAA;AAAA;AAEA,QAAA,YAAA,QAAA,QAAA,IAAA;AAEA,SACA,oCAAA,aAAA,MAAA,QAEA,OAAA,MAAA,GAAA,SAAA,EAAA,IAAA,CAAA,OAAA,UACA;AAAA,IAAA;AAAA;AAAA,MACA,KAAA,OAAA,MAAA,QAAA,WAAA,MAAA,MAAA;AAAA,MACA,OAAA;AAAA,QACA,GAAA;AAAA,QACA,QAAA,iBAAA,QAAA;AAAA,QACA;AAAA,MACA;AAAA;AAAA,EACA,CACA,GAEA,gBAAA,UAAA,QAAA,WAAA,KACA;AAAA,IAAA;AAAA;AAAA,MACA,cAAA;AAAA,MACA;AAAA,MACA,KAAA,oCAAA,eAAA,QAAA,aAAA;AAAA,MACA,QAAA;AAAA,MACA,UAAA;AAAA;AAAA,EACA,CAEA;AAEA;AAMA,SAAA,YAAA,EAAA,OAAA,GAAA;AACA,SACA,oCAAA,4BACA,oCAAA,QAAA,MAAA,WAAA,OAAA,EAAA,YAAA,OAAA,KAAA,KACA,MACA,CACA;AAEA;AAEA,MAAA,uBAAA,OAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA,sBAMA,MAAA,OAAA,OAAA;AAAA;AAAA;AAQA,MAAA,cAAA,CAAA,EAAA,OAAA,EAAA,SAAA,GAAA,MAAA,EAAA,MAAA;AACA,QAAA,CAAA,SAAA,QAAA,IAAA,SAAA,KAAA;AAEA,QAAA,mBAAA,MAAA,SAAA,IAAA;AACA,QAAA,mBAAA,MAAA,SAAA,KAAA;AAEA,QAAA,aAAA,CAAA,UAAA,YAAA,EAAA,EAAA,KAAA,GAAA;AAEA,MAAA,SAAA;AACA,WACA;AAAA,MAAA;AAAA;AAAA,QACA,WAAA;AAAA,QACA,OAAA,EAAA,QAAA,MAAA,OAAA;AAAA,QACA,cAAA;AAAA,QACA,cAAA;AAAA;AAAA,MAEA;AAAA,QAAA;AAAA;AAAA,UACA,UAAA,QAAA,YAAA;AAAA,UACA,OAAA,SAAA;AAAA,UACA,SAAA,QAAA;AAAA;AAAA,QAEA,oCAAA,SAAA,GAAA,OAAA;AAAA,MACA;AAAA,IACA;AAAA,EAEA;AAEA,SACA;AAAA,IAAA;AAAA;AAAA,MACA,WAAA;AAAA,MACA,OAAA,EAAA,QAAA,MAAA,OAAA;AAAA,MACA,cAAA;AAAA,MACA,cAAA;AAAA;AAAA,IAEA,oCAAA,SAAA,GAAA,OAAA;AAAA,EACA;AAEA;AAEA,MAAA,YAAA,OAAA,GAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;;"},_coverageSchema:"1a1c01bbd47fc00a2c39e90264f33305004495a9",hash:"55da2f2a4c87378aad9881dc48d7d81b14f6d469"},o=s[i]||(s[i]={});(!o[e]||o[e].hash!==n)&&(o[e]=r);var a=o[e];return A=function(){return a},a}A();function x({badges:e,limit:n,size:s}){A().f[0]++;const i=(A().s[0]++,e.length*10+10),r=(A().s[1]++,A().b[0][0]++,n&&(A().b[0][1]++,e.length+1>n?(A().b[1][0]++,e.length-n+1):(A().b[1][1]++,0))),o=(A().s[2]++,n?(A().b[2][0]++,n-1):(A().b[2][1]++,void 0));return A().s[3]++,t.createElement(_,{flex:!0},e.slice(0,o).map((a,p)=>(A().f[1]++,A().s[4]++,t.createElement(k,{key:typeof a.src=="string"?(A().b[3][0]++,a.src):(A().b[3][1]++,p),badge:{...a,zIndex:i-p*10,size:s}}))),(A().b[4][0]++,r!==void 0&&(A().b[4][1]++,!!r)&&(A().b[4][2]++,t.createElement(d,{borderColour:"oatmeal",size:s,src:t.createElement(w,{excess:r}),zIndex:i,disabled:!0}))))}function w({excess:e}){return A().f[2]++,A().s[5]++,t.createElement(L,null,t.createElement(z,{typo:"caption",style:{fontWeight:"bold"}},"+",e))}const L=(A().s[6]++,M.div`
+  display: flex;
+  align-items: center;
+  place-content: center;
+  width: 100%;
+  height: 100%;
+  background-color: ${O.colors.oatmeal};
+  pointer-events: none;
+`);A().s[7]++;const k=({badge:{tooltip:e,...n}})=>{A().f[3]++;const[s,i]=(A().s[8]++,T.useState(!1));A().s[9]++;const r=()=>(A().f[4]++,A().s[10]++,i(!0));A().s[11]++;const o=()=>(A().f[5]++,A().s[12]++,i(!1)),a=(A().s[13]++,[s?(A().b[5][0]++,"hovered"):(A().b[5][1]++,"")].join(" "));return A().s[14]++,e?(A().b[6][0]++,A().s[15]++,t.createElement("div",{className:a,style:{zIndex:n.zIndex},onMouseEnter:r,onMouseLeave:o},t.createElement(I,{position:(A().b[7][0]++,e.position??(A().b[7][1]++,"bottom")),title:e==null?void 0:e.title,content:e.content},t.createElement(d,{...n})))):(A().b[6][1]++,A().s[16]++,t.createElement("div",{className:a,style:{zIndex:n.zIndex},onMouseEnter:r,onMouseLeave:o},t.createElement(d,{...n})))},_=(A().s[17]++,M(Q)`
+  & > * {
+    transition: margin 0.2s ease-in-out, padding 0.2s ease-in-out;
+    margin-right: -15px;
+  }
+
+  &:hover > *.hovered:not(:first-child) {
+    padding-left: 10px;
+    &:not(:last-child) {
+      padding-right: 0px;
+    }
+  }
+`);A().s[18]++;x.__docgenInfo={description:"",methods:[],displayName:"BadgeList",props:{size:{required:!1,tsType:{name:"BadgeSize",raw:"BadgeProps['size']"},description:""},limit:{required:!1,tsType:{name:"number"},description:""},badges:{required:!0,tsType:{name:"Array",elements:[{name:"Omit",elements:[{name:"intersection",raw:"BadgeProps & { tooltip?: WithTooltip }",elements:[{name:"signature",type:"object",raw:`{
+  src: string | JSX.Element
+  borderColour?: Color
+  size?: BadgeSize
+  disabled?: boolean
+  zIndex?: number
+}`,signature:{properties:[{key:"src",value:{name:"union",raw:"string | JSX.Element",elements:[{name:"string"},{name:"JSX.Element"}],required:!0}},{key:"borderColour",value:{name:"union",raw:"keyof typeof colors",elements:[{name:"literal",value:"lollipop"},{name:"literal",value:"marshmallowPink"},{name:"literal",value:"bubblegum"},{name:"literal",value:"fairyFloss"},{name:"literal",value:"boba"},{name:"literal",value:"liquorice"},{name:"literal",value:"sesame"},{name:"literal",value:"chia"},{name:"literal",value:"custard"},{name:"literal",value:"mascarpone"},{name:"literal",value:"coconut"},{name:"literal",value:"cream"},{name:"literal",value:"spearmint"},{name:"literal",value:"feijoa"},{name:"literal",value:"blueberry"},{name:"literal",value:"macaroon"},{name:"literal",value:"pistachio"},{name:"literal",value:"matcha"},{name:"literal",value:"caramel"},{name:"literal",value:"peanut"},{name:"literal",value:"marzipan"},{name:"literal",value:"oatmeal"},{name:"literal",value:"strawberry"},{name:"literal",value:"watermelon"},{name:"literal",value:"apple"},{name:"literal",value:"mint"},{name:"literal",value:"lemon"},{name:"literal",value:"sherbert"},{name:"literal",value:"tangerine"},{name:"literal",value:"compareTheMarket"},{name:"literal",value:"confused"},{name:"literal",value:"onfido"},{name:"literal",value:"twitter"},{name:"literal",value:"premfina"},{name:"literal",value:"checkout"},{name:"literal",value:"facebook"},{name:"literal",value:"stripe"},{name:"literal",value:"intercom"},{name:"literal",value:"ravelin"},{name:"literal",value:"rac"},{name:"literal",value:"hometree"}],required:!1}},{key:"size",value:{name:"BadgeSize",required:!1}},{key:"disabled",value:{name:"boolean",required:!1}},{key:"zIndex",value:{name:"number",required:!1}}]}},{name:"signature",type:"object",raw:"{ tooltip?: WithTooltip }",signature:{properties:[{key:"tooltip",value:{name:"WithTooltip",required:!1}}]}}]},{name:"literal",value:"'zIndex'"}],raw:"Omit<BadgeListBadge, 'zIndex'>"}],raw:"Omit<BadgeListBadge, 'zIndex'>[]"},description:""}}};const V={title:"BadgeList",component:x},c={args:{badges:[{src:l,tooltip:{title:"This is a tooltip",content:"This is the content of the tooltip"}},{src:l,disabled:!0},{src:l,disabled:!0,tooltip:{title:"This is a tooltip",content:"This is the content of the tooltip"}}]}},m={args:{size:y.Md,badges:[{src:l,tooltip:{title:"This is a tooltip",content:"This is the content of the tooltip"}},{src:l,disabled:!0},{src:l,disabled:!0,tooltip:{title:"This is a tooltip",content:"This is the content of the tooltip"}}]}},u={args:{limit:4,badges:[{src:l,tooltip:{title:"This is a tooltip",content:"This is the content of the tooltip"}},{src:l,disabled:!0,tooltip:{title:"This is a tooltip",content:"This is the content of the tooltip"}},{src:l,disabled:!0,tooltip:{title:"This is a tooltip",content:"This is the content of the tooltip"}},{src:l,disabled:!0,tooltip:{title:"This is a tooltip",content:"This is the content of the tooltip"}},{src:l,disabled:!0,tooltip:{title:"This is a tooltip",content:"This is the content of the tooltip"}}]}};var h,g,v;c.parameters={...c.parameters,docs:{...(h=c.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    badges: [{
+      src: BadgeSrcExample,
+      tooltip: {
+        title: 'This is a tooltip',
+        content: 'This is the content of the tooltip'
+      }
+    }, {
+      src: BadgeSrcExample,
+      disabled: true
+    }, {
+      src: BadgeSrcExample,
+      disabled: true,
+      tooltip: {
+        title: 'This is a tooltip',
+        content: 'This is the content of the tooltip'
+      }
+    }]
+  }
+}`,...(v=(g=c.parameters)==null?void 0:g.docs)==null?void 0:v.source}}};var f,b,E;m.parameters={...m.parameters,docs:{...(f=m.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    size: BadgeSize.Md,
+    badges: [{
+      src: BadgeSrcExample,
+      tooltip: {
+        title: 'This is a tooltip',
+        content: 'This is the content of the tooltip'
+      }
+    }, {
+      src: BadgeSrcExample,
+      disabled: true
+    }, {
+      src: BadgeSrcExample,
+      disabled: true,
+      tooltip: {
+        title: 'This is a tooltip',
+        content: 'This is the content of the tooltip'
+      }
+    }]
+  }
+}`,...(E=(b=m.parameters)==null?void 0:b.docs)==null?void 0:E.source}}};var C,B,S;u.parameters={...u.parameters,docs:{...(C=u.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    limit: 4,
+    badges: [{
+      src: BadgeSrcExample,
+      tooltip: {
+        title: 'This is a tooltip',
+        content: 'This is the content of the tooltip'
+      }
+    }, {
+      src: BadgeSrcExample,
+      disabled: true,
+      tooltip: {
+        title: 'This is a tooltip',
+        content: 'This is the content of the tooltip'
+      }
+    }, {
+      src: BadgeSrcExample,
+      disabled: true,
+      tooltip: {
+        title: 'This is a tooltip',
+        content: 'This is the content of the tooltip'
+      }
+    }, {
+      src: BadgeSrcExample,
+      disabled: true,
+      tooltip: {
+        title: 'This is a tooltip',
+        content: 'This is the content of the tooltip'
+      }
+    }, {
+      src: BadgeSrcExample,
+      disabled: true,
+      tooltip: {
+        title: 'This is a tooltip',
+        content: 'This is the content of the tooltip'
+      }
+    }]
+  }
+}`,...(S=(B=u.parameters)==null?void 0:B.docs)==null?void 0:S.source}}};const AA=["Primary","MediumSize","BadgeLimit"];export{u as BadgeLimit,m as MediumSize,c as Primary,AA as __namedExportsOrder,V as default};
+//# sourceMappingURL=BadgeList.stories-x3dLRxpY.js.map
