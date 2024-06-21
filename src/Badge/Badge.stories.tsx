b6b497fe42fcaaf46b9fb863e499c5dce3c10a85
rowanhen <rowan.henseleit@marshmallow.co>
2024-06-21 09:42:07 +0100

    chore: add alt text to badge story to prevent accessibility test failing

--- src/Badge/Badge.stories.tsx
+++ src/Badge/Badge.stories.tsx
@@ -3,28 +3,29 @@ import { Badge, BadgeSize } from './Badge'
 import { BadgeSrcExample } from './storybook-data'
 
 const meta: Meta<typeof Badge> = {
-    title: 'Badge',
-    component: Badge,
-    args: {
-    },
+  title: 'Badge',
+  component: Badge,
+  args: {
+    title: 'badge',
+  },
 }
 
 export default meta
 type Story = StoryObj<typeof Badge>
 
 export const Primary: Story = {
-    args: {
-        borderColour: "lollipop",
-        size: BadgeSize.Lg,
-        src: BadgeSrcExample
-    },
+  args: {
+    borderColour: 'lollipop',
+    size: BadgeSize.Lg,
+    src: BadgeSrcExample,
+  },
 }
 
 export const Disabled: Story = {
-    args: {
-        borderColour: "lollipop",
-        size: BadgeSize.Lg,
-        src: BadgeSrcExample,
-        disabled: true
-    },
+  args: {
+    borderColour: 'lollipop',
+    size: BadgeSize.Lg,
+    src: BadgeSrcExample,
+    disabled: true,
+  },
 }
