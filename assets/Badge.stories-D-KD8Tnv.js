0bea1e1be41b90882e8c5296a735f9d5b3630bb1
liampuk <liam.piesley@marshmallow.co>
2024-08-02 14:26:00 +0000

    Deploying to gh-pages from @ marshmallow-insurance/smores-react@b4bf52107bec7db8183f0a671aedba3fa3265abc 🚀

---
+++ assets/Badge.stories-D-KD8Tnv.js
@@ -0,0 +1,15 @@
+import{B as c,a as m,g as l}from"./exampleImg-D1uJQJZ2.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./styled-components.browser.esm-C_MULHfC.js";import"./theme-BeIETfSH.js";import"./index-wFHcz20e.js";import"./Text-CUtnjvlQ.js";import"./index-BdjIJMAY.js";import"./polished.esm-D0h1-lPB.js";import"./index-BTU799Km.js";import"./Icon-DEo-Nyik.js";import"./focusOutline-QAmRMp8e.js";const I={title:"Badge",component:c,args:{title:"badge"}},r={args:{borderColour:"lollipop",size:m.Lg,src:l().src}},e={args:{borderColour:"lollipop",size:m.Lg,src:l().src,disabled:!0}};var o,s,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    borderColour: "lollipop",
+    size: BadgeSize.Lg,
+    src: getExampleImg().src
+  }
+}`,...(a=(s=r.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};var t,i,p;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    borderColour: "lollipop",
+    size: BadgeSize.Lg,
+    src: getExampleImg().src,
+    disabled: true
+  }
+}`,...(p=(i=e.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};const f=["Primary","Disabled"];export{e as Disabled,r as Primary,f as __namedExportsOrder,I as default};
+//# sourceMappingURL=Badge.stories-D-KD8Tnv.js.map
---
+++ assets/CommonInput-DqWFayZt.js
@@ -0,0 +1,58 @@
+import{d as c,l as u}from"./styled-components.browser.esm-C_MULHfC.js";import"./index-BTU799Km.js";import{t as e}from"./theme-BeIETfSH.js";import{I as a}from"./Icon-DEo-Nyik.js";function n(){var A="/home/runner/work/smores-react/smores-react/src/fields/components/CommonInput.tsx",o="5fcf48c90f30ebf7cf0061646337f0707e551ec3",t=window,i="__coverage__",m={path:"/home/runner/work/smores-react/smores-react/src/fields/components/CommonInput.tsx",statementMap:{0:{start:{line:4,column:21},end:{line:47,column:1}},1:{start:{line:6,column:27},end:{line:6,column:81}},2:{start:{line:10,column:30},end:{line:10,column:66}},3:{start:{line:11,column:31},end:{line:11,column:53}},4:{start:{line:13,column:46},end:{line:13,column:104}},5:{start:{line:15,column:22},end:{line:15,column:77}},6:{start:{line:17,column:30},end:{line:22,column:5}},7:{start:{line:27,column:24},end:{line:29,column:5}},8:{start:{line:31,column:27},end:{line:33,column:5}},9:{start:{line:48,column:31},end:{line:55,column:1}},10:{start:{line:53,column:32},end:{line:53,column:55}},11:{start:{line:56,column:34},end:{line:62,column:1}},12:{start:{line:60,column:32},end:{line:60,column:55}}},fnMap:{0:{name:"(anonymous_0)",decl:{start:{line:6,column:11},end:{line:6,column:12}},loc:{start:{line:6,column:27},end:{line:6,column:81}},line:6},1:{name:"(anonymous_1)",decl:{start:{line:10,column:12},end:{line:10,column:13}},loc:{start:{line:10,column:30},end:{line:10,column:66}},line:10},2:{name:"(anonymous_2)",decl:{start:{line:11,column:13},end:{line:11,column:14}},loc:{start:{line:11,column:31},end:{line:11,column:53}},line:11},3:{name:"(anonymous_3)",decl:{start:{line:13,column:22},end:{line:13,column:23}},loc:{start:{line:13,column:46},end:{line:13,column:104}},line:13},4:{name:"(anonymous_4)",decl:{start:{line:15,column:6},end:{line:15,column:7}},loc:{start:{line:15,column:22},end:{line:15,column:77}},line:15},5:{name:"(anonymous_5)",decl:{start:{line:17,column:4},end:{line:17,column:5}},loc:{start:{line:17,column:30},end:{line:22,column:5}},line:17},6:{name:"(anonymous_6)",decl:{start:{line:27,column:4},end:{line:27,column:5}},loc:{start:{line:27,column:24},end:{line:29,column:5}},line:27},7:{name:"(anonymous_7)",decl:{start:{line:31,column:4},end:{line:31,column:5}},loc:{start:{line:31,column:27},end:{line:33,column:5}},line:31},8:{name:"(anonymous_8)",decl:{start:{line:53,column:13},end:{line:53,column:14}},loc:{start:{line:53,column:32},end:{line:53,column:55}},line:53},9:{name:"(anonymous_9)",decl:{start:{line:60,column:13},end:{line:60,column:14}},loc:{start:{line:60,column:32},end:{line:60,column:55}},line:60}},branchMap:{0:{loc:{start:{line:6,column:43},end:{line:6,column:78}},type:"cond-expr",locations:[{start:{line:6,column:52},end:{line:6,column:64}},{start:{line:6,column:67},end:{line:6,column:78}}],line:6},1:{loc:{start:{line:10,column:30},end:{line:10,column:66}},type:"cond-expr",locations:[{start:{line:10,column:41},end:{line:10,column:54}},{start:{line:10,column:57},end:{line:10,column:66}}],line:10},2:{loc:{start:{line:11,column:31},end:{line:11,column:53}},type:"cond-expr",locations:[{start:{line:11,column:42},end:{line:11,column:47}},{start:{line:11,column:50},end:{line:11,column:53}}],line:11},3:{loc:{start:{line:13,column:46},end:{line:13,column:104}},type:"cond-expr",locations:[{start:{line:13,column:63},end:{line:13,column:83}},{start:{line:13,column:86},end:{line:13,column:104}}],line:13},4:{loc:{start:{line:15,column:22},end:{line:15,column:77}},type:"cond-expr",locations:[{start:{line:15,column:31},end:{line:15,column:54}},{start:{line:15,column:57},end:{line:15,column:77}}],line:15},5:{loc:{start:{line:17,column:30},end:{line:22,column:5}},type:"binary-expr",locations:[{start:{line:17,column:30},end:{line:17,column:39}},{start:{line:17,column:43},end:{line:17,column:50}},{start:{line:17,column:54},end:{line:22,column:5}}],line:17},6:{loc:{start:{line:27,column:24},end:{line:29,column:5}},type:"binary-expr",locations:[{start:{line:27,column:24},end:{line:27,column:34}},{start:{line:27,column:38},end:{line:29,column:5}}],line:27},7:{loc:{start:{line:31,column:27},end:{line:33,column:5}},type:"binary-expr",locations:[{start:{line:31,column:27},end:{line:31,column:40}},{start:{line:31,column:44},end:{line:33,column:5}}],line:31},8:{loc:{start:{line:53,column:32},end:{line:53,column:55}},type:"cond-expr",locations:[{start:{line:53,column:44},end:{line:53,column:49}},{start:{line:53,column:52},end:{line:53,column:55}}],line:53},9:{loc:{start:{line:60,column:32},end:{line:60,column:55}},type:"cond-expr",locations:[{start:{line:60,column:44},end:{line:60,column:49}},{start:{line:60,column:52},end:{line:60,column:55}}],line:60}},s:{0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0},f:{0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0},b:{0:[0,0],1:[0,0],2:[0,0],3:[0,0],4:[0,0],5:[0,0,0],6:[0,0],7:[0,0],8:[0,0],9:[0,0]},inputSourceMap:{version:3,file:null,sources:["/home/runner/work/smores-react/smores-react/src/fields/components/CommonInput.tsx"],names:[],mappings:"AAAA,OAAO,UAAU,WAAW;AAC5B,SAAS,YAAY;AAErB,SAAS,aAAa;AAiBf,aAAM,QAAQ,OAAO;AAAA;AAAA,WAEjB,CAAC,EAAE,OAAO,MACjB,MAAM,OAAO,GAAG,SAAS,eAAe,WAAW,EAAE,CAAC;AAAA;AAAA;AAAA;AAAA,YAI9C,CAAC,EAAE,SAAS,MAAO,WAAW,gBAAgB,SAAU;AAAA,aACvD,CAAC,EAAE,SAAS,MAAO,WAAW,QAAQ,GAAI;AAAA;AAAA,sBAEjC,CAAC,EAAE,eAAe,MACpC,iBAAiB,MAAM,OAAO,UAAU,MAAM,OAAO,KAAK;AAAA;AAAA,MAExD,CAAC,EAAE,OAAO,MAAO,SAAS,MAAM,OAAO,aAAa,MAAM,OAAO,OAAQ;AAAA;AAAA,IAE3E,CAAC,EAAE,UAAU,OAAO,MACpB,CAAC,YACD,CAAC,UACD;AAAA;AAAA;AAAA,wBAGoB,MAAM,OAAO,QAAQ;AAAA;AAAA,KAExC;AAAA;AAAA;AAAA;AAAA;AAAA,IAKD,CAAC,EAAE,WAAW,MACd,cACA;AAAA;AAAA,KAEC;AAAA;AAAA,IAED,CAAC,EAAE,cAAc,MACjB,iBACA;AAAA;AAAA,KAEC;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA,aAYQ,MAAM,OAAO,MAAM;AAAA;AAAA;AAIzB,aAAM,kBAAkB,OAAO,IAAI;AAAA;AAAA;AAAA;AAAA;AAAA,aAK7B,CAAC,EAAE,UAAU,MAAO,YAAY,QAAQ,GAAI;AAAA,WAC9C,MAAM,OAAO,SAAS;AAAA;AAG1B,aAAM,qBAAqB,OAAO,IAAI;AAAA;AAAA;AAAA;AAAA,aAIhC,CAAC,EAAE,UAAU,MAAO,YAAY,QAAQ,GAAI;AAAA,WAC9C,MAAM,OAAO,SAAS;AAAA;"},_coverageSchema:"1a1c01bbd47fc00a2c39e90264f33305004495a9",hash:"5fcf48c90f30ebf7cf0061646337f0707e551ec3"},l=t[i]||(t[i]={});(!l[A]||l[A].hash!==o)&&(l[A]=m);var r=l[A];return n=function(){return r},r}n();const b=(n().s[0]++,c.input`
+  border: none;
+  color: ${({$error:A})=>(n().f[0]++,n().s[1]++,e.colors[`${A?(n().b[0][0]++,"strawberry"):(n().b[0][1]++,"liquorice")}`])};
+  font-size: 16px;
+  width: 100%;
+  outline: none;
+  cursor: ${({disabled:A})=>(n().f[1]++,n().s[2]++,A?(n().b[1][0]++,"not-allowed"):(n().b[1][1]++,"initial"))};
+  opacity: ${({disabled:A})=>(n().f[2]++,n().s[3]++,A?(n().b[2][0]++,"0.5"):(n().b[2][1]++,"1"))};
+  padding: 18px 14px;
+  background-color: ${({$fallbackStyle:A})=>(n().f[3]++,n().s[4]++,A?(n().b[3][0]++,e.colors.custard):(n().b[3][1]++,e.colors.cream))};
+  border: 2px solid
+    ${({$error:A})=>(n().f[4]++,n().s[5]++,A?(n().b[4][0]++,e.colors.strawberry):(n().b[4][1]++,e.colors.oatmeal))};
+
+  ${({disabled:A,$error:o})=>(n().f[5]++,n().s[6]++,n().b[5][0]++,!A&&(n().b[5][1]++,!o)&&(n().b[5][2]++,u`
+      &:hover,
+      &:focus-within {
+        border-color: ${e.colors.marzipan};
+      }
+    `))}
+
+  border-radius: 12px;
+  height: auto;
+
+  ${({$frontIcon:A})=>(n().f[6]++,n().s[7]++,n().b[6][0]++,A&&(n().b[6][1]++,`
+      padding-left: 42px;
+    `))}
+
+  ${({$trailingIcon:A})=>(n().f[7]++,n().s[8]++,n().b[7][0]++,A&&(n().b[7][1]++,`
+      padding-right: 42px;
+    `))}
+
+  /* Remove the spinner on Firefox and Webkit browsers */
+  -moz-appearance: textfield;
+
+  &::-webkit-outer-spin-button,
+  &::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  &::placeholder {
+    color: ${e.colors.sesame};
+  }
+`),f=(n().s[9]++,c(a)`
+  position: relative;
+  left: 36px;
+  margin-left: -24px;
+  z-index: 1;
+  opacity: ${({$disabled:A})=>(n().f[8]++,n().s[10]++,A?(n().b[8][0]++,"0.5"):(n().b[8][1]++,"1"))};
+  color: ${e.colors.liquorice};
+`),M=(n().s[11]++,c(a)`
+  position: relative;
+  right: 36px;
+  margin-right: -36px;
+  opacity: ${({$disabled:A})=>(n().f[9]++,n().s[12]++,A?(n().b[9][0]++,"0.5"):(n().b[9][1]++,"1"))};
+  color: ${e.colors.liquorice};
+`);export{b as I,f as S,M as a};
+//# sourceMappingURL=CommonInput-DqWFayZt.js.map
