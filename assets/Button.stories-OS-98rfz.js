9becdc7e3ed83e700661385b48a0205075a13c12
rowanhen <57456071+rowanhen@users.noreply.github.com>
2024-10-16 10:45:51 +0000

    Deploying to gh-pages from @ marshmallow-insurance/smores-react@06c5c294b56a863022a29a56012e05168269a9c4 🚀

---
+++ assets/Button.stories-OS-98rfz.js
@@ -0,0 +1,105 @@
+import{R as t,r as le}from"./index-Cs7sjTYM.js";import{f as w,w as C,e as s,u as Ae}from"./index-DcZGqPg1.js";import{t as M}from"./theme-CvrCV7IQ.js";import{B as c}from"./Button-BetN5fjA.js";import{d as h}from"./styled-components.browser.esm-B8Od9Tyf.js";import{B as u}from"./index-Dg0pwggo.js";import"./index-z9UWbxVJ.js";import{T as te}from"./Text-BBNNikpA.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-CgqzGSAy.js";import"./Icon-CgRafFBu.js";import"./index-CNxMDpx6.js";import"./Loader-CPumWuhs.js";import"./focusOutline-Cx-Os6e7.js";import"./polished.esm-D0h1-lPB.js";function A(){var e="/home/runner/work/smores-react/smores-react/src/Button/storybook/Collection.tsx",n="3f2591f00538d97a89c2c148d24099bed55a2a57",a=window,l="__coverage__",S={path:"/home/runner/work/smores-react/smores-react/src/Button/storybook/Collection.tsx",statementMap:{0:{start:{line:6,column:26},end:{line:12,column:1}},1:{start:{line:13,column:30},end:{line:15,column:1}},2:{start:{line:14,column:2},end:{line:14,column:1318}},3:{start:{line:14,column:239},end:{line:14,column:292}},4:{start:{line:14,column:408},end:{line:14,column:476}},5:{start:{line:14,column:593},end:{line:14,column:662}},6:{start:{line:14,column:848},end:{line:14,column:915}},7:{start:{line:14,column:1031},end:{line:14,column:1113}},8:{start:{line:14,column:1230},end:{line:14,column:1313}},9:{start:{line:16,column:16},end:{line:21,column:1}},10:{start:{line:22,column:16},end:{line:27,column:1}},11:{start:{line:26,column:2},end:{line:26,column:264}},12:{start:{line:28,column:15},end:{line:32,column:1}},13:{start:{line:33,column:12},end:{line:38,column:1}},14:{start:{line:37,column:2},end:{line:37,column:222}},15:{start:{line:39,column:19},end:{line:44,column:1}},16:{start:{line:45,column:0},end:{line:45,column:100}}},fnMap:{0:{name:"(anonymous_0)",decl:{start:{line:13,column:30},end:{line:13,column:31}},loc:{start:{line:13,column:36},end:{line:15,column:1}},line:13},1:{name:"(anonymous_1)",decl:{start:{line:14,column:205},end:{line:14,column:206}},loc:{start:{line:14,column:239},end:{line:14,column:292}},line:14},2:{name:"(anonymous_2)",decl:{start:{line:14,column:374},end:{line:14,column:375}},loc:{start:{line:14,column:408},end:{line:14,column:476}},line:14},3:{name:"(anonymous_3)",decl:{start:{line:14,column:559},end:{line:14,column:560}},loc:{start:{line:14,column:593},end:{line:14,column:662}},line:14},4:{name:"(anonymous_4)",decl:{start:{line:14,column:814},end:{line:14,column:815}},loc:{start:{line:14,column:848},end:{line:14,column:915}},line:14},5:{name:"(anonymous_5)",decl:{start:{line:14,column:997},end:{line:14,column:998}},loc:{start:{line:14,column:1031},end:{line:14,column:1113}},line:14},6:{name:"(anonymous_6)",decl:{start:{line:14,column:1196},end:{line:14,column:1197}},loc:{start:{line:14,column:1230},end:{line:14,column:1313}},line:14},7:{name:"(anonymous_7)",decl:{start:{line:22,column:16},end:{line:22,column:17}},loc:{start:{line:25,column:6},end:{line:27,column:1}},line:25},8:{name:"(anonymous_8)",decl:{start:{line:33,column:12},end:{line:33,column:13}},loc:{start:{line:36,column:6},end:{line:38,column:1}},line:36}},branchMap:{},s:{0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0,15:0,16:0},f:{0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0},b:{},inputSourceMap:{version:3,file:null,sources:["/home/runner/work/smores-react/smores-react/src/Button/storybook/Collection.tsx"],names:[],mappings:"AAAA,OAAO,WAA8B;AACrC,OAAO,YAAY;AAEnB,SAAS,WAAW;AACpB,SAAS,YAAY;AACrB,SAAS,cAA2B;AAE7B,aAAM,aAAiC;AAAA,EAC5C,EAAE,SAAS,MAAM,UAAU,OAAO;AAAA,EAClC,EAAE,WAAW,MAAM,UAAU,OAAO;AAAA,EACpC,EAAE,eAAe,MAAM,UAAU,SAAS;AAAA,EAC1C,EAAE,SAAS,MAAM,UAAU,OAAO;AAAA,EAClC,EAAE,aAAa,MAAM,UAAU,QAAQ;AACzC;AAEO,aAAM,iBAAqB,MAAM;AACtC,SACE,oCAAC,eACC,oCAAC,WAAQ,OAAM,aACb,oCAAC,OAAI,OAAM,aACR,WAAW,IAAI,CAAC,OAAO,UACtB,oCAAC,UAAO,KAAK,OAAQ,GAAG,OAAO,CAChC,CACH,GACA,oCAAC,OAAI,OAAM,aACR,WAAW,IAAI,CAAC,OAAO,UACtB,oCAAC,UAAO,KAAK,OAAQ,GAAG,OAAO,SAAO,MAAC,CACxC,CACH,GACA,oCAAC,OAAI,OAAM,cACR,WAAW,IAAI,CAAC,OAAO,UACtB,oCAAC,UAAO,KAAK,OAAQ,GAAG,OAAO,UAAQ,MAAC,CACzC,CACH,CACF,GACA,oCAAC,WAAQ,OAAM,eACb,oCAAC,OAAI,OAAM,aACR,WAAW,IAAI,CAAC,OAAO,UACtB,oCAAC,UAAO,KAAK,OAAQ,GAAG,OAAO,MAAK,QAAO,CAC5C,CACH,GACA,oCAAC,OAAI,OAAM,aACR,WAAW,IAAI,CAAC,OAAO,UACtB,oCAAC,UAAO,KAAK,OAAQ,GAAG,OAAO,SAAO,MAAC,MAAK,QAAO,CACpD,CACH,GACA,oCAAC,OAAI,OAAM,cACR,WAAW,IAAI,CAAC,OAAO,UACtB,oCAAC,UAAO,KAAK,OAAQ,GAAG,OAAO,UAAQ,MAAC,MAAK,QAAO,CACrD,CACH,CACF,CACF;AAEJ;AAEA,MAAM,UAAU,OAAO,GAAG;AAAA;AAAA;AAAA;AAAA;AAAA;AAO1B,MAAM,UAAuD,CAAC;AAAA,EAC5D;AAAA,EACA;AACF,MAAM;AACJ,SACE,oCAAC,WACC,oCAAC,OAAI,IAAG,UACN,oCAAC,QAAK,KAAI,QAAO,MAAK,kBACnB,KACH,CACF,GACA,oCAAC,cAAQ,QAAS,CACpB;AAEJ;AAEA,MAAM,SAAS,OAAO,GAAG;AAAA;AAAA;AAAA;AAAA;AAMzB,MAAM,MAAmD,CAAC;AAAA,EACxD;AAAA,EACA;AACF,MAAM;AACJ,SACE,oCAAC,kBACC,oCAAC,OAAI,OAAM,UACT,oCAAC,QAAK,KAAI,QAAO,MAAK,iBACnB,KACH,CACF,GACC,QACH;AAEJ;AAEA,MAAM,aAAa,OAAO,GAAG;AAAA;AAAA;AAAA;AAAA;AAAA;;"},_coverageSchema:"1a1c01bbd47fc00a2c39e90264f33305004495a9",hash:"3f2591f00538d97a89c2c148d24099bed55a2a57"},r=a[l]||(a[l]={});(!r[e]||r[e].hash!==n)&&(r[e]=S);var d=r[e];return A=function(){return d},d}A();const i=(A().s[0]++,[{primary:!0,children:"Save"},{secondary:!0,children:"Edit"},{fallbackStyle:!0,children:"Cancel"},{textBtn:!0,children:"View"},{smallButton:!0,children:"Small"}]);A().s[1]++;const ae=()=>(A().f[0]++,A().s[2]++,t.createElement(re,null,t.createElement(k,{label:"Default"},t.createElement(m,{label:"Default"},i.map((e,n)=>(A().f[1]++,A().s[3]++,t.createElement(c,{key:n,...e})))),t.createElement(m,{label:"Loading"},i.map((e,n)=>(A().f[2]++,A().s[4]++,t.createElement(c,{key:n,...e,loading:!0})))),t.createElement(m,{label:"Disabled"},i.map((e,n)=>(A().f[3]++,A().s[5]++,t.createElement(c,{key:n,...e,disabled:!0}))))),t.createElement(k,{label:"With icon"},t.createElement(m,{label:"Default"},i.map((e,n)=>(A().f[4]++,A().s[6]++,t.createElement(c,{key:n,...e,icon:"info"})))),t.createElement(m,{label:"Loading"},i.map((e,n)=>(A().f[5]++,A().s[7]++,t.createElement(c,{key:n,...e,loading:!0,icon:"info"})))),t.createElement(m,{label:"Disabled"},i.map((e,n)=>(A().f[6]++,A().s[8]++,t.createElement(c,{key:n,...e,disabled:!0,icon:"info"}))))))),re=(A().s[9]++,h(u)`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 40px;
+`);A().s[10]++;const k=({label:e,children:n})=>(A().f[7]++,A().s[11]++,t.createElement(u,null,t.createElement(u,{mb:"16px"},t.createElement(te,{tag:"span",typo:"header-small"},e)),t.createElement(ce,null,n))),ce=(A().s[12]++,h(u)`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`);A().s[13]++;const m=({label:e,children:n})=>(A().f[8]++,A().s[14]++,t.createElement(se,null,t.createElement(u,{width:"80px"},t.createElement(te,{tag:"span",typo:"label-large"},e)),n)),se=(A().s[15]++,h(u)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 16px;
+`);A().s[16]++;ae.__docgenInfo={description:"",methods:[],displayName:"CollectionPage"};function o(){var e="/home/runner/work/smores-react/smores-react/src/Button/storybook/InteractivePlayground.tsx",n="2469023e1587889fb1712e7b4bcfa2dad37f66d0",a=window,l="__coverage__",S={path:"/home/runner/work/smores-react/smores-react/src/Button/storybook/InteractivePlayground.tsx",statementMap:{0:{start:{line:4,column:2},end:{line:6,column:5}},1:{start:{line:5,column:4},end:{line:5,column:31}},2:{start:{line:8,column:37},end:{line:16,column:1}},3:{start:{line:9,column:36},end:{line:9,column:51}},4:{start:{line:10,column:22},end:{line:14,column:3}},5:{start:{line:11,column:4},end:{line:11,column:23}},6:{start:{line:12,column:4},end:{line:12,column:21}},7:{start:{line:13,column:4},end:{line:13,column:24}},8:{start:{line:15,column:2},end:{line:15,column:134}},9:{start:{line:17,column:0},end:{line:17,column:114}}},fnMap:{0:{name:"sleep",decl:{start:{line:3,column:9},end:{line:3,column:14}},loc:{start:{line:3,column:22},end:{line:7,column:1}},line:3},1:{name:"(anonymous_1)",decl:{start:{line:4,column:21},end:{line:4,column:22}},loc:{start:{line:4,column:39},end:{line:6,column:3}},line:4},2:{name:"(anonymous_2)",decl:{start:{line:8,column:37},end:{line:8,column:38}},loc:{start:{line:8,column:43},end:{line:16,column:1}},line:8},3:{name:"(anonymous_3)",decl:{start:{line:10,column:22},end:{line:10,column:23}},loc:{start:{line:10,column:34},end:{line:14,column:3}},line:10}},branchMap:{},s:{0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0},f:{0:0,1:0,2:0,3:0},b:{},inputSourceMap:{version:3,file:null,sources:["/home/runner/work/smores-react/smores-react/src/Button/storybook/InteractivePlayground.tsx"],names:[],mappings:"AAAA,OAAO,SAAa,gBAAgB;AAEpC,SAAS,cAAc;AAEvB,SAAS,MAAM,OAAe;AAC5B,SAAO,IAAI,QAAQ,SAAU,SAAS;AACpC,eAAW,SAAS,KAAK;AAAA,EAC3B,CAAC;AACH;AAEO,aAAM,wBAA4B,MAAM;AAC7C,QAAM,CAAC,WAAW,YAAY,IAAI,SAAS,KAAK;AAEhD,QAAM,cAAc,YAAY;AAC9B,iBAAa,IAAI;AACjB,UAAM,MAAM,GAAI;AAChB,iBAAa,KAAK;AAAA,EACpB;AAEA,SACE,oCAAC,UAAO,SAAO,MAAC,MAAK,QAAO,aAA0B,SAAS,aAAW,aAE1E;AAEJ;;"},_coverageSchema:"1a1c01bbd47fc00a2c39e90264f33305004495a9",hash:"2469023e1587889fb1712e7b4bcfa2dad37f66d0"},r=a[l]||(a[l]={});(!r[e]||r[e].hash!==n)&&(r[e]=S);var d=r[e];return o=function(){return d},d}o();function ie(e){return o().f[0]++,o().s[0]++,new Promise(function(n){o().f[1]++,o().s[1]++,setTimeout(n,e)})}o().s[2]++;const oe=()=>{o().f[2]++;const[e,n]=(o().s[3]++,le.useState(!1));o().s[4]++;const a=async()=>{o().f[3]++,o().s[5]++,n(!0),o().s[6]++,await ie(1e3),o().s[7]++,n(!1)};return o().s[8]++,t.createElement(c,{primary:!0,icon:"info",handleClick:a,loading:e},"Get started")};o().s[9]++;oe.__docgenInfo={description:"",methods:[],displayName:"InteractivePlayground"};const he={title:"Button",component:c,args:{children:"Button",handleClick:w(),onClick:w()}},p={args:{primary:!0},play:async({args:e,canvasElement:n})=>{const l=C(n).getByRole("button",{name:/Button/i});await s(l.innerText).toBe("Button"),await Ae.click(l),await s(e.onClick).toHaveBeenCalled(),await s(l).toHaveStyle(`background-color: ${M.colors.marshmallowPink}`)}},g={args:{secondary:!0},play:async({canvasElement:e})=>{const a=C(e).getByRole("button",{name:/Button/i});await s(a).toHaveStyle(`background-color: ${M.colors.oatmeal}`)}},y={args:{fallbackStyle:!0},play:async({canvasElement:e})=>{const a=C(e).getByRole("button",{name:/Button/i});await s(a).toHaveStyle(`background-color: ${M.colors.cream}`)}},v={args:{textBtn:!0},play:async({canvasElement:e})=>{const a=C(e).getByRole("button",{name:/Button/i});await s(a).toHaveStyle("background-color: rgba(0, 0, 0, 0)")}},O={args:{loading:!0},play:async({args:e,canvasElement:n})=>{const a=C(n);await Ae.click(a.getByRole("button")),await s(e.onClick).not.toHaveBeenCalled()}},b={args:{forcedWidth:"300px"},play:async({canvasElement:e})=>{const a=C(e).getByRole("button",{name:/Button/i});await s(a).toHaveStyle("width: 300px")}},E={args:{primary:!0}},B={render:e=>t.createElement(oe,{...e})},f={render:e=>t.createElement(ae,{...e})};var x,I,U;p.parameters={...p.parameters,docs:{...(x=p.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    primary: true
+  },
+  play: async ({
+    args,
+    canvasElement
+  }) => {
+    const canvas = within(canvasElement);
+    const button = canvas.getByRole('button', {
+      name: /Button/i
+    });
+    await expect(button.innerText).toBe('Button');
+    await userEvent.click(button);
+    await expect(args.onClick).toHaveBeenCalled();
+    await expect(button).toHaveStyle(\`background-color: \${theme.colors.marshmallowPink}\`);
+  }
+}`,...(U=(I=p.parameters)==null?void 0:I.docs)==null?void 0:U.source}}};var W,K,_;g.parameters={...g.parameters,docs:{...(W=g.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  args: {
+    secondary: true
+  },
+  play: async ({
+    canvasElement
+  }) => {
+    const canvas = within(canvasElement);
+    const button = canvas.getByRole('button', {
+      name: /Button/i
+    });
+    await expect(button).toHaveStyle(\`background-color: \${theme.colors.oatmeal}\`);
+  }
+}`,...(_=(K=g.parameters)==null?void 0:K.docs)==null?void 0:_.source}}};var G,Q,H;y.parameters={...y.parameters,docs:{...(G=y.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  args: {
+    fallbackStyle: true
+  },
+  play: async ({
+    canvasElement
+  }) => {
+    const canvas = within(canvasElement);
+    const button = canvas.getByRole('button', {
+      name: /Button/i
+    });
+    await expect(button).toHaveStyle(\`background-color: \${theme.colors.cream}\`);
+  }
+}`,...(H=(Q=y.parameters)==null?void 0:Q.docs)==null?void 0:H.source}}};var R,P,D;v.parameters={...v.parameters,docs:{...(R=v.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    textBtn: true
+  },
+  play: async ({
+    canvasElement
+  }) => {
+    const canvas = within(canvasElement);
+    const button = canvas.getByRole('button', {
+      name: /Button/i
+    });
+    await expect(button).toHaveStyle(\`background-color: rgba(0, 0, 0, 0)\`);
+  }
+}`,...(D=(P=v.parameters)==null?void 0:P.docs)==null?void 0:D.source}}};var T,F,Y;O.parameters={...O.parameters,docs:{...(T=O.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    loading: true
+  },
+  play: async ({
+    args,
+    canvasElement
+  }) => {
+    const canvas = within(canvasElement);
+    await userEvent.click(canvas.getByRole('button'));
+    await expect(args.onClick).not.toHaveBeenCalled();
+  }
+}`,...(Y=(F=O.parameters)==null?void 0:F.docs)==null?void 0:Y.source}}};var L,J,$;b.parameters={...b.parameters,docs:{...(L=b.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    forcedWidth: '300px'
+  },
+  play: async ({
+    canvasElement
+  }) => {
+    const canvas = within(canvasElement);
+    const button = canvas.getByRole('button', {
+      name: /Button/i
+    });
+    await expect(button).toHaveStyle(\`width: 300px\`);
+  }
+}`,...($=(J=b.parameters)==null?void 0:J.docs)==null?void 0:$.source}}};var z,q,N;E.parameters={...E.parameters,docs:{...(z=E.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  args: {
+    primary: true
+  }
+}`,...(N=(q=E.parameters)==null?void 0:q.docs)==null?void 0:N.source}}};var j,V,X;B.parameters={...B.parameters,docs:{...(j=B.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  render: args => <InteractivePlayground {...args} />
+}`,...(X=(V=B.parameters)==null?void 0:V.docs)==null?void 0:X.source}}};var Z,ee,ne;f.parameters={...f.parameters,docs:{...(Z=f.parameters)==null?void 0:Z.docs,source:{originalSource:`{
+  render: args => <CollectionPage {...args} />
+}`,...(ne=(ee=f.parameters)==null?void 0:ee.docs)==null?void 0:ne.source}}};const we=["Primary","Secondary","Fallback","TextButton","Loading","ForcedWidth","Playground","InteractivePlaygroundTemplate","CollectionTemplate"];export{f as CollectionTemplate,y as Fallback,b as ForcedWidth,B as InteractivePlaygroundTemplate,O as Loading,E as Playground,p as Primary,g as Secondary,v as TextButton,we as __namedExportsOrder,he as default};
+//# sourceMappingURL=Button.stories-OS-98rfz.js.map
