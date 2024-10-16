aa94da8038df463b948596e23caf99bd970382df
DeadEnglish <git@liamcanetti.co.uk>
2024-10-16 08:37:51 +0000

    Deploying to gh-pages from @ marshmallow-insurance/smores-react@cd7a9f1058deb0640b39482fe6cc3959a674f75f 🚀

---
+++ assets/Badge.stories-CEGWo_49.js
@@ -0,0 +1,15 @@
+import{B as c,a as m,g as l}from"./exampleImg-bxndt9hI.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./styled-components.browser.esm-B8Od9Tyf.js";import"./theme-BeIETfSH.js";import"./index-Cct9KP2X.js";import"./Text-BxGERy6_.js";import"./index-Dg0pwggo.js";import"./polished.esm-D0h1-lPB.js";import"./index-CLjO9-Li.js";import"./Icon-XnBvu4j3.js";import"./focusOutline-Dc3qB3kf.js";const I={title:"Badge",component:c,args:{title:"badge"}},r={args:{borderColour:"lollipop",size:m.Lg,src:l().src}},e={args:{borderColour:"lollipop",size:m.Lg,src:l().src,disabled:!0}};var o,s,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    borderColour: "lollipop",
+    size: BadgeSize.Lg,
+    src: getExampleImg().src
+  }
+}`,...(a=(s=r.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};var t,i,p;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    borderColour: "lollipop",
+    size: BadgeSize.Lg,
+    src: getExampleImg().src,
+    disabled: true
+  }
+}`,...(p=(i=e.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};const f=["Primary","Disabled"];export{e as Disabled,r as Primary,f as __namedExportsOrder,I as default};
+//# sourceMappingURL=Badge.stories-CEGWo_49.js.map
