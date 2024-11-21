ff107d0751cbece335b42f814ed94ef649186087
Liam Canetti <liam.canetti@marshmallow.co>
2024-11-21 14:40:55 +0000

    chore: test commit?

--- src/Badge/Badge.stories.tsx
+++ src/Badge/Badge.stories.tsx
@@ -2,6 +2,7 @@ import { Meta, StoryObj } from '@storybook/react'
 import { Badge, BadgeSize } from './Badge'
 import { getExampleImg } from './storybook/exampleImg'
 
+// adding test comment
 const meta: Meta<typeof Badge> = {
   title: 'Badge',
   component: Badge,
@@ -14,18 +15,18 @@ export default meta
 type Story = StoryObj<typeof Badge>
 
 export const Primary: Story = {
-    args: {
-        borderColour: "lollipop",
-        size: BadgeSize.Lg,
-        src: getExampleImg().src
-    },
+  args: {
+    borderColour: 'lollipop',
+    size: BadgeSize.Lg,
+    src: getExampleImg().src,
+  },
 }
 
 export const Disabled: Story = {
-    args: {
-        borderColour: "lollipop",
-        size: BadgeSize.Lg,
-        src: getExampleImg().src,
-        disabled: true
-    },
+  args: {
+    borderColour: 'lollipop',
+    size: BadgeSize.Lg,
+    src: getExampleImg().src,
+    disabled: true,
+  },
 }
