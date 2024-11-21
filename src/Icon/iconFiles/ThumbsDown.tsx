b5c2c83fceccc3fa40f560dc467cb6ff250e3511
Callum Marriage <callum.marriage@marshmallow.co>
2024-11-21 10:39:39 +0000

    Update src/Icon/iconFiles/ThumbsDown.tsx
    Co-authored-by: Liam Piesley <liam.piesley@marshmallow.co>

--- src/Icon/iconFiles/ThumbsDown.tsx
+++ src/Icon/iconFiles/ThumbsDown.tsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 const ThumbsDown = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" fill="none">
-    <path
-      stroke="#292924"
-      strokeWidth="2"
-      d="M4 15.912h7.625L10.153 19.8c-.377.996-.042 2.135.802 2.73.964.679 2.264.427 2.937-.57l4.089-6.05H18v-9a4 4 0 0 0-4-4H7.3c-.79 0-1.507.467-1.828 1.189l-3.3 7.425a2 2 0 0 0-.172.812v1.575a2 2 0 0 0 2 2Z"
-    />
-    <path
-      fill="#292924"
-      stroke="#292924"
-      strokeWidth="2"
-      d="M19 2.912h2a1 1 0 0 1 1 1v11a1 1 0 0 1-1 1h-3v-12a1 1 0 0 1 1-1Z"
-    />
+        <g stroke="currentColor" strokeWidth="2">
+      <path
+        strokeWidth="2"
+        d="M4 15.912h7.625L10.153 19.8c-.377.996-.042 2.135.802 2.73.964.679 2.264.427 2.937-.57l4.089-6.05H18v-9a4 4 0 0 0-4-4H7.3c-.79 0-1.507.467-1.828 1.189l-3.3 7.425a2 2 0 0 0-.172.812v1.575a2 2 0 0 0 2 2Z"
+      />
+      <path
+        strokeWidth="2"
+        d="M19 2.912h2a1 1 0 0 1 1 1v11a1 1 0 0 1-1 1h-3v-12a1 1 0 0 1 1-1Z"
+        fill="currentColor"
+      />
+    </g>
   </svg>
 )
 
