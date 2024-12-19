ca5f0461a210f4008c221bd8de654f91c9faf40c
Mat Diak <mat.diak@marshmallow.co>
2024-12-19 15:25:46 +0000

    feat: Add disabled state for toggle (#4145)
    * feat: Add disabled state for toggle
    
    * fix: Refactor styled component props name
    
    * test: fix storybook tests

--- src/Toggle/storybook/Toggle.stories.tsx
+++ src/Toggle/storybook/Toggle.stories.tsx
@@ -1,12 +1,57 @@
+import { useArgs } from '@storybook/preview-api'
+import { Meta, StoryObj } from '@storybook/react'
+import { expect, fn, userEvent, waitFor, within } from '@storybook/test'
 import React from 'react'
-import { Toggle } from '../Toggle'
-import { Container } from './Container'
+import { Props, Toggle } from '../Toggle'
 
-export default {
-  title: 'Toggle',
-  component: Toggle,
+const Render = (args: Props) => {
+  const [{ checked }, updateArgs] = useArgs<Props>()
+
+  function onChange() {
+    args.onToggle()
+    updateArgs({ checked: !checked })
+  }
+
+  return <Toggle {...args} onToggle={onChange} checked={checked} />
 }
 
-const Template = () => <Container />
+const meta: Meta<typeof Toggle> = {
+  title: 'Toggle',
+  component: Toggle,
+  args: {
+    checked: true,
+  },
+  render: Render,
+}
 
-export const Default = Template.bind({})
+export default meta
+
+type Story = StoryObj<typeof Toggle>
+
+export const Primary: Story = {
+  args: {
+    onToggle: fn(),
+    checked: true,
+  },
+  render: Render,
+  play: async ({ args, canvasElement }) => {
+    const canvas = within(canvasElement)
+    const checkbox = canvas.getByRole('checkbox', { name: 'toggle' })
+    await userEvent.click(checkbox)
+    await waitFor(() => expect(args.onToggle).toHaveBeenCalled())
+  },
+}
+
+export const Disabled: Story = {
+  render: Render,
+  args: {
+    checked: false,
+    disabled: true,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    const checkbox = canvas.getByRole('checkbox', { name: 'toggle' })
+    await userEvent.click(checkbox)
+    await expect(checkbox).toBeDisabled()
+  },
+}
