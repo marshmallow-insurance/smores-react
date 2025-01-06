8c120f520ddcb730183487a381eddb0fb62bc7e1
matdiak <mat.diak@marshmallow.co>
2025-01-06 16:42:17 +0000

    Deploying to gh-pages from @ marshmallow-insurance/smores-react@6971af788de29455c78065a1f2940e39c7ecdfe9 🚀

---
+++ assets/SupportMessage.stories-D6cSacR0.js
@@ -0,0 +1,42 @@
+import{R as A}from"./index-BwPxMuoB.js";import"./index-BzPHsf9o.js";import{S as r}from"./SupportMessage-B1gn8pah.js";import{d as v}from"./styled-components.browser.esm-CkjnBEnw.js";import{B as o}from"./index-ufmWnqDS.js";import"./index-ZTUjpWqA.js";import{T as B,L as y}from"./Text-BoPqfnNa.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./polished.esm-CaZDTQnh.js";import"./index-Bp7Bvwfa.js";import"./Icon-CAC1LLLu.js";import"./theme-CvrCV7IQ.js";import"./isReactElement-CU3yCwfY.js";import"./space-B-76eNGj.js";import"./focusOutline-s6PVkpWM.js";function e(){var t="/home/runner/work/smores-react/smores-react/src/SupportMessage/storybook/Collection.tsx",a="5e079928be778cfee9fd2ef0a384ef15b726cbdd",n=window,p="__coverage__",L={path:"/home/runner/work/smores-react/smores-react/src/SupportMessage/storybook/Collection.tsx",statementMap:{0:{start:{line:6,column:22},end:{line:32,column:1}},1:{start:{line:33,column:30},end:{line:52,column:1}},2:{start:{line:34,column:2},end:{line:51,column:6}},3:{start:{line:35,column:4},end:{line:50,column:8}},4:{start:{line:48,column:23},end:{line:48,column:40}},5:{start:{line:53,column:20},end:{line:53,column:27}},6:{start:{line:54,column:14},end:{line:59,column:1}},7:{start:{line:60,column:14},end:{line:65,column:1}},8:{start:{line:66,column:0},end:{line:66,column:100}}},fnMap:{0:{name:"(anonymous_0)",decl:{start:{line:33,column:30},end:{line:33,column:31}},loc:{start:{line:33,column:36},end:{line:52,column:1}},line:33},1:{name:"(anonymous_1)",decl:{start:{line:34,column:105},end:{line:34,column:106}},loc:{start:{line:34,column:139},end:{line:51,column:3}},line:34},2:{name:"(anonymous_2)",decl:{start:{line:48,column:17},end:{line:48,column:18}},loc:{start:{line:48,column:23},end:{line:48,column:40}},line:48}},branchMap:{},s:{0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0},f:{0:0,1:0,2:0},b:{},inputSourceMap:{version:3,file:null,sources:["/home/runner/work/smores-react/smores-react/src/SupportMessage/storybook/Collection.tsx"],names:[],mappings:"AAAA,OAAO,WAAmB;AAC1B,OAAO,YAAY;AAEnB,SAAS,WAAW;AACpB,SAAS,YAAY;AACrB,SAAS,sBAA2C;AAEpD,MAAM,gBAAuC;AAAA,EAC3C;AAAA,IACE,MAAM;AAAA,IACN,OAAO;AAAA,IACP,aAAa;AAAA,EACf;AAAA,EACA;AAAA,IACE,MAAM;AAAA,IACN,OAAO;AAAA,IACP,aAAa;AAAA,EACf;AAAA,EACA;AAAA,IACE,MAAM;AAAA,IACN,OAAO;AAAA,IACP,aAAa;AAAA,EACf;AAAA,EACA;AAAA,IACE,MAAM;AAAA,IACN,OAAO;AAAA,IACP,aAAa;AAAA,EACf;AAAA,EACA;AAAA,IACE,MAAM;AAAA,IACN,OAAO;AAAA,IACP,aAAa;AAAA,EACf;AACF;AAEO,aAAM,iBAAqB,MAAM;AACtC,SACE,oCAAC,OAAI,MAAI,MAAC,WAAU,YACjB,cAAc,IAAI,CAAC,EAAE,MAAM,OAAO,YAAY,MAAM;AACnD,WACE,oCAAC,OAAI,KAAK,MAAM,MAAI,MAAC,WAAU,UAAS,IAAG,UACzC,oCAAC,SAAM,KAAI,QAAO,MAAK,mBAAe,UAC7B,IACT,GACA,oCAAC,OAAI,MAAI,QACP,oCAAC,SAAM,KAAI,QAAO,MAAK,kBAAc,eAErC,GACA,oCAAC,kBAAe,MAAY,aAA0B,CACxD,GACA,oCAAC,OAAI,MAAI,MAAC,IAAG,SACX,oCAAC,SAAM,KAAI,QAAO,MAAK,kBAAc,YAErC,GACA;AAAA,MAAC;AAAA;AAAA,QACC;AAAA,QACA;AAAA,QACA;AAAA;AAAA,IACF,CACF,GACA,oCAAC,OAAI,MAAI,MAAC,IAAG,SACX,oCAAC,SAAM,KAAI,QAAO,MAAK,kBAAc,WAErC,GACA;AAAA,MAAC;AAAA;AAAA,QACC;AAAA,QACA;AAAA,QACA;AAAA,QACA,SAAS,MAAM,MAAM,UAAU;AAAA;AAAA,IACjC,CACF,CACF;AAAA,EAEJ,CAAC,CACH;AAEJ;AAEA,MAAM,cAAc;AAEpB,MAAM,QAAQ,OAAO,IAAI;AAAA;AAAA;AAAA;AAAA,iBAIR,WAAW;AAAA;AAG5B,MAAM,QAAQ,OAAO,IAAI;AAAA;AAAA;AAAA,WAGd,WAAW;AAAA;AAAA;;"},_coverageSchema:"1a1c01bbd47fc00a2c39e90264f33305004495a9",hash:"5e079928be778cfee9fd2ef0a384ef15b726cbdd"},i=n[p]||(n[p]={});(!i[t]||i[t].hash!==a)&&(i[t]=L);var u=i[t];return e=function(){return u},u}e();const _=(e().s[0]++,[{type:"info",title:"Additional information",description:"Information needs to be communicated to the user."},{type:"fallbackStyle",title:"Additional information",description:"Information needs to be communicated to the user."},{type:"alert",title:"Potential issue",description:"The user should be aware and possibly exercise caution."},{type:"warning",title:"Error encountered",description:"An error has occurred, here's feedback on how to proceed."},{type:"success",title:"Error encountered",description:"An error has occurred, here's feedback on how to proceed."}]);e().s[1]++;const W=()=>(e().f[0]++,e().s[2]++,A.createElement(o,{flex:!0,direction:"column"},_.map(({type:t,title:a,description:n})=>(e().f[1]++,e().s[3]++,A.createElement(o,{key:t,flex:!0,direction:"column",mb:"32px"},A.createElement(w,{tag:"span",typo:"header-medium"},"Type: ",t),A.createElement(o,{flex:!0},A.createElement(C,{tag:"span",typo:"header-small"},"Without title"),A.createElement(r,{type:t,description:n})),A.createElement(o,{flex:!0,mt:"8px"},A.createElement(C,{tag:"span",typo:"header-small"},"With title"),A.createElement(r,{type:t,title:a,description:n})),A.createElement(o,{flex:!0,mt:"8px"},A.createElement(C,{tag:"span",typo:"header-small"},"Clickable"),A.createElement(r,{type:t,title:a,description:n,onClick:()=>(e().f[2]++,e().s[4]++,alert("Clicked!"))}))))))),Q=(e().s[5]++,"140px"),w=(e().s[6]++,v(B)`
+  margin-bottom: 12px;
+  font-size: 18px;
+  line-height: 20px;
+  margin-left: ${Q};
+`),C=(e().s[7]++,v(B)`
+  margin-bottom: 4px;
+  font-size: 14px;
+  width: ${Q};
+  flex-shrink: 0;
+`);e().s[8]++;W.__docgenInfo={description:"",methods:[],displayName:"CollectionPage"};const $={title:"SupportMessage",component:r,args:{type:"info",title:void 0,description:"Some description text"},render:t=>A.createElement(r,{...t},"Support message for customer")},s={args:{type:"info",title:"A SupportMessage using the Link component",description:A.createElement("div",null,"Some text rendered using a ",A.createElement(y,{href:""},"Link"))}},c={args:{type:"info",title:"An interactive SupportMessage",description:"Click me!",onClick:()=>alert("Clicked!")}},l={args:{type:"info",title:"An interactive SupportMessage",rightSideComponent:A.createElement(o,{ml:{custom:"auto"}},A.createElement(y,{href:void 0},"This is a right side component")),description:"Click me!"}},m={decorators:[()=>A.createElement(W,null)]};var d,M,f;s.parameters={...s.parameters,docs:{...(d=s.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    type: 'info',
+    title: 'A SupportMessage using the Link component',
+    description: <div>
+        Some text rendered using a <Link href={''}>Link</Link>
+      </div>
+  }
+}`,...(f=(M=s.parameters)==null?void 0:M.docs)==null?void 0:f.source}}};var g,h,E;c.parameters={...c.parameters,docs:{...(g=c.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    type: 'info',
+    title: 'An interactive SupportMessage',
+    description: 'Click me!',
+    onClick: () => alert('Clicked!')
+  }
+}`,...(E=(h=c.parameters)==null?void 0:h.docs)==null?void 0:E.source}}};var I,S,k;l.parameters={...l.parameters,docs:{...(I=l.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    type: 'info',
+    title: 'An interactive SupportMessage',
+    rightSideComponent: <Box ml={{
+      custom: 'auto'
+    }}>
+        <Link href={undefined as unknown as string}>
+          This is a right side component
+        </Link>
+      </Box>,
+    description: 'Click me!'
+  }
+}`,...(k=(S=l.parameters)==null?void 0:S.docs)==null?void 0:k.source}}};var O,x,b;m.parameters={...m.parameters,docs:{...(O=m.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  decorators: [() => <CollectionPage />]
+}`,...(b=(x=m.parameters)==null?void 0:x.docs)==null?void 0:b.source}}};const q=["WithCustomDescription","Clickable","WithRightSideComponent","Collection"];export{c as Clickable,m as Collection,s as WithCustomDescription,l as WithRightSideComponent,q as __namedExportsOrder,$ as default};
+//# sourceMappingURL=SupportMessage.stories-D6cSacR0.js.map
