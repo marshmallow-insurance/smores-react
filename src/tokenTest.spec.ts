a2466dee6491916b4a7fa3b40d92823539237dc4
Mat Diak <mat.diak@marshmallow.co>
2025-01-06 16:58:02 +0000

    test: Remove no longer relevant token test

--- src/tokenTest.spec.ts
+++
@@ -1,8 +0,0 @@
-describe('Token expiry', () => {
-  it('should fail after November 27th', () => {
-    const currentDate = new Date()
-    const failAfterDate = new Date(2024, 10, 27)
-
-    expect(currentDate.getTime()).toBeLessThanOrEqual(failAfterDate.getTime())
-  })
-})
