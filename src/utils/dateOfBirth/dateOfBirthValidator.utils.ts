52eaf133299063ee4de3d84f99ac9b4c8c6bc56c
David Gomes <david.gomes@marshmallow.co>
2025-01-09 16:46:55 +0000

    fix(library): Missing import

--- src/utils/dateOfBirth/dateOfBirthValidator.utils.ts
+++ src/utils/dateOfBirth/dateOfBirthValidator.utils.ts
@@ -1,6 +1,6 @@
 import { isAfter, subYears } from 'date-fns'
 
-import { DateObject } from 'TextDateOfBirthInput'
+import { DateObject } from '../../TextDateOfBirthInput'
 
 const dateFields = ['day', 'month', 'year'] as const
 
