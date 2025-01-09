126062aa850870b96a8593b0b082d4badad62f4a
david-gomes5 <63399235+david-gomes5@users.noreply.github.com>
2025-01-09 11:13:57 +0000

    Deploying to gh-pages from @ marshmallow-insurance/smores-react@6370a7eb5e27473d7d650beed9062fac1c24f7ee 🚀

---
+++ assets/Toggle.stories-JKIcXInx.js
@@ -0,0 +1,89 @@
+import{f as S,w as h,u as C,a as M,e as B}from"./index-DLuT9rNG.js";import{R as A}from"./index-CO7zwj5_.js";import{d as p,l as v}from"./styled-components.browser.esm-Dmrap0gj.js";import{B as q}from"./index-BqrQKxiI.js";import{t as m}from"./theme-CvrCV7IQ.js";import{f as E}from"./focusOutline-IVUuACd1.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./space-B-76eNGj.js";function e(){var n="/home/runner/work/smores-react/smores-react/src/Toggle/Toggle.tsx",r="fa8fc26e50ee32cf4f438de3f5a2546634976af9",a=window,l="__coverage__",s={path:"/home/runner/work/smores-react/smores-react/src/Toggle/Toggle.tsx",statementMap:{0:{start:{line:6,column:22},end:{line:29,column:1}},1:{start:{line:13,column:2},end:{line:28,column:75}},2:{start:{line:22,column:8},end:{line:24,column:9}},3:{start:{line:23,column:10},end:{line:23,column:21}},4:{start:{line:30,column:15},end:{line:41,column:1}},5:{start:{line:42,column:15},end:{line:74,column:1}},6:{start:{line:67,column:23},end:{line:73,column:9}},7:{start:{line:75,column:17},end:{line:86,column:1}},8:{start:{line:87,column:0},end:{line:87,column:14186}}},fnMap:{0:{name:"(anonymous_0)",decl:{start:{line:6,column:22},end:{line:6,column:23}},loc:{start:{line:12,column:6},end:{line:29,column:1}},line:12},1:{name:"(anonymous_1)",decl:{start:{line:21,column:17},end:{line:21,column:18}},loc:{start:{line:21,column:24},end:{line:25,column:7}},line:21},2:{name:"(anonymous_2)",decl:{start:{line:67,column:4},end:{line:67,column:5}},loc:{start:{line:67,column:23},end:{line:73,column:9}},line:67}},branchMap:{0:{loc:{start:{line:22,column:8},end:{line:24,column:9}},type:"if",locations:[{start:{line:22,column:8},end:{line:24,column:9}},{start:{line:void 0,column:void 0},end:{line:void 0,column:void 0}}],line:22},1:{loc:{start:{line:67,column:23},end:{line:73,column:9}},type:"cond-expr",locations:[{start:{line:67,column:35},end:{line:69,column:9}},{start:{line:69,column:12},end:{line:73,column:9}}],line:67}},s:{0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0},f:{0:0,1:0,2:0},b:{0:[0,0],1:[0,0]},inputSourceMap:{version:3,file:null,sources:["/home/runner/work/smores-react/smores-react/src/Toggle/Toggle.tsx"],names:[],mappings:"AAAA,OAAO,WAAmB;AAC1B,OAAO,UAAU,WAAW;AAE5B,SAAS,WAAW;AACpB,SAAS,aAAa;AACtB,SAAS,oBAAoB;AAatB,aAAM,SAAoB,CAAC;AAAA,EAChC;AAAA,EACA;AAAA,EACA;AAAA,EACA;AAAA,EACA,GAAG;AACL,MAAM;AACJ,SACE,oCAAC,UAAO,aAAY,SAAQ,SAAS,IAAK,GAAG,eAC3C;AAAA,IAAC;AAAA;AAAA,MACC;AAAA,MACA;AAAA,MACA,MAAK;AAAA,MACL;AAAA,MACA,UAAU;AAAA,MACV,WAAW,CAAC,MAAM;AAChB,YAAI,EAAE,QAAQ,SAAS;AACrB,mBAAS;AAAA,QACX;AAAA,MACF;AAAA,MACA,cAAW;AAAA;AAAA,EACb,GACA,oCAAC,UAAO,WAAW,UAAU,CAC/B;AAEJ;AAEA,MAAM,SAAS,OAAO,GAAG;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAazB,MAAM,SAAS,OAAO;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA,sBAOA,MAAM,OAAO,OAAO;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA,wBAalB,MAAM,OAAO,UAAU;AAAA;AAAA;AAAA;AAAA;AAAA,IAK3C,CAAC,EAAE,UAAU,MACb,YACI;AAAA;AAAA,YAGA;AAAA;AAAA,gCAEwB,MAAM,OAAO,QAAQ;AAAA;AAAA,SAE5C;AAAA;AAGT,MAAM,WAAW,OAAO;AAAA,IACpB,aAAa,EAAE,UAAU,qBAAqB,MAAM,GAAG,CAAC,CAAC;AAAA;AAAA,gBAE7C,MAAM;AAAA,wBACE,MAAM,OAAO,SAAS;AAAA;AAAA;AAAA;AAAA,gBAI9B,MAAM;AAAA;AAAA;AAAA;;"},_coverageSchema:"1a1c01bbd47fc00a2c39e90264f33305004495a9",hash:"fa8fc26e50ee32cf4f438de3f5a2546634976af9"},t=a[l]||(a[l]={});(!t[n]||t[n].hash!==r)&&(t[n]=s);var g=t[n];return e=function(){return g},g}e();e().s[0]++;const c=({id:n,checked:r,onToggle:a,disabled:l,...s})=>(e().f[0]++,e().s[1]++,A.createElement(P,{forwardedAs:"label",htmlFor:n,...s},A.createElement(O,{id:n,disabled:l,type:"checkbox",checked:r,onChange:a,onKeyDown:t=>{e().f[1]++,e().s[2]++,t.key==="Enter"?(e().b[0][0]++,e().s[3]++,a()):e().b[0][1]++},"aria-label":"toggle"}),A.createElement(o,{$disabled:l}))),P=(e().s[4]++,p(q)`
+  position: relative;
+  display: inline-block;
+  width: 56px;
+  height: 32px;
+
+  input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
+`),o=(e().s[5]++,p.span`
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: ${m.colors.oatmeal};
+  border: none;
+  border-radius: 28px;
+  transition: 0.2s background-color;
+  outline: none;
+
+  &:before {
+    position: absolute;
+    content: '';
+    height: 24px;
+    width: 24px;
+    left: 4px;
+    bottom: 4px;
+    background-color: ${m.colors.mascarpone};
+    transition: 0.2s transform;
+    border-radius: 50%;
+  }
+
+  ${({$disabled:n})=>(e().f[2]++,e().s[6]++,n?(e().b[1][0]++,v`
+          cursor: not-allowed;
+        `):(e().b[1][1]++,v`
+          &:hover {
+            background-color: ${m.colors.marzipan};
+          }
+        `))}
+`),O=(e().s[7]++,p.input`
+  ${E({selector:`&:focus-visible + ${o}`})}
+
+  &:checked + ${o} {
+    background-color: ${m.colors.pistachio};
+    border: none;
+  }
+
+  &:checked + ${o}:before {
+    transform: translateX(24px);
+  }
+`);e().s[8]++;c.__docgenInfo={description:"",methods:[],displayName:"Toggle",props:{id:{required:!1,tsType:{name:"string"},description:"unique ID"},checked:{required:!0,tsType:{name:"boolean"},description:"checked flag"},disabled:{required:!1,tsType:{name:"boolean"},description:""},onToggle:{required:!0,tsType:{name:"signature",type:"function",raw:"() => void",signature:{arguments:[],return:{name:"void"}}},description:"onToggle listener"},m:{required:!1,tsType:{name:"union",raw:"PropValue | PropByBreakpoint<PropValue>",elements:[{name:"union",raw:"'0' | Spacing | { custom: number | string }",elements:[{name:"literal",value:"'0'"},{name:"union",raw:"'8px' | '12px' | '16px' | '24px' | '32px' | '48px' | '64px'",elements:[{name:"literal",value:"'8px'"},{name:"literal",value:"'12px'"},{name:"literal",value:"'16px'"},{name:"literal",value:"'24px'"},{name:"literal",value:"'32px'"},{name:"literal",value:"'48px'"},{name:"literal",value:"'64px'"}]},{name:"signature",type:"object",raw:"{ custom: number | string }",signature:{properties:[{key:"custom",value:{name:"union",raw:"number | string",elements:[{name:"number"},{name:"string"}],required:!0}}]}}]},{name:"signature",type:"object",raw:"{ [key in Breakpoint]?: PropValue }",signature:{properties:[{key:{name:"unknown",required:!1},value:{name:"union",raw:"'0' | Spacing | { custom: number | string }",elements:[{name:"literal",value:"'0'"},{name:"union",raw:"'8px' | '12px' | '16px' | '24px' | '32px' | '48px' | '64px'",elements:[{name:"literal",value:"'8px'"},{name:"literal",value:"'12px'"},{name:"literal",value:"'16px'"},{name:"literal",value:"'24px'"},{name:"literal",value:"'32px'"},{name:"literal",value:"'48px'"},{name:"literal",value:"'64px'"}]},{name:"signature",type:"object",raw:"{ custom: number | string }",signature:{properties:[{key:"custom",value:{name:"union",raw:"number | string",elements:[{name:"number"},{name:"string"}],required:!0}}]}}]}}]}}]},description:""},mx:{required:!1,tsType:{name:"union",raw:"PropValue | PropByBreakpoint<PropValue>",elements:[{name:"union",raw:"SpacingProp | 'auto'",elements:[{name:"union",raw:"'0' | Spacing | { custom: number | string }",elements:[{name:"literal",value:"'0'"},{name:"union",raw:"'8px' | '12px' | '16px' | '24px' | '32px' | '48px' | '64px'",elements:[{name:"literal",value:"'8px'"},{name:"literal",value:"'12px'"},{name:"literal",value:"'16px'"},{name:"literal",value:"'24px'"},{name:"literal",value:"'32px'"},{name:"literal",value:"'48px'"},{name:"literal",value:"'64px'"}]},{name:"signature",type:"object",raw:"{ custom: number | string }",signature:{properties:[{key:"custom",value:{name:"union",raw:"number | string",elements:[{name:"number"},{name:"string"}],required:!0}}]}}]},{name:"literal",value:"'auto'"}]},{name:"signature",type:"object",raw:"{ [key in Breakpoint]?: PropValue }",signature:{properties:[{key:{name:"unknown",required:!1},value:{name:"union",raw:"SpacingProp | 'auto'",elements:[{name:"union",raw:"'0' | Spacing | { custom: number | string }",elements:[{name:"literal",value:"'0'"},{name:"union",raw:"'8px' | '12px' | '16px' | '24px' | '32px' | '48px' | '64px'",elements:[{name:"literal",value:"'8px'"},{name:"literal",value:"'12px'"},{name:"literal",value:"'16px'"},{name:"literal",value:"'24px'"},{name:"literal",value:"'32px'"},{name:"literal",value:"'48px'"},{name:"literal",value:"'64px'"}]},{name:"signature",type:"object",raw:"{ custom: number | string }",signature:{properties:[{key:"custom",value:{name:"union",raw:"number | string",elements:[{name:"number"},{name:"string"}],required:!0}}]}}]},{name:"literal",value:"'auto'"}]}}]}}]},description:""},my:{required:!1,tsType:{name:"union",raw:"PropValue | PropByBreakpoint<PropValue>",elements:[{name:"union",raw:"'0' | Spacing | { custom: number | string }",elements:[{name:"literal",value:"'0'"},{name:"union",raw:"'8px' | '12px' | '16px' | '24px' | '32px' | '48px' | '64px'",elements:[{name:"literal",value:"'8px'"},{name:"literal",value:"'12px'"},{name:"literal",value:"'16px'"},{name:"literal",value:"'24px'"},{name:"literal",value:"'32px'"},{name:"literal",value:"'48px'"},{name:"literal",value:"'64px'"}]},{name:"signature",type:"object",raw:"{ custom: number | string }",signature:{properties:[{key:"custom",value:{name:"union",raw:"number | string",elements:[{name:"number"},{name:"string"}],required:!0}}]}}]},{name:"signature",type:"object",raw:"{ [key in Breakpoint]?: PropValue }",signature:{properties:[{key:{name:"unknown",required:!1},value:{name:"union",raw:"'0' | Spacing | { custom: number | string }",elements:[{name:"literal",value:"'0'"},{name:"union",raw:"'8px' | '12px' | '16px' | '24px' | '32px' | '48px' | '64px'",elements:[{name:"literal",value:"'8px'"},{name:"literal",value:"'12px'"},{name:"literal",value:"'16px'"},{name:"literal",value:"'24px'"},{name:"literal",value:"'32px'"},{name:"literal",value:"'48px'"},{name:"literal",value:"'64px'"}]},{name:"signature",type:"object",raw:"{ custom: number | string }",signature:{properties:[{key:"custom",value:{name:"union",raw:"number | string",elements:[{name:"number"},{name:"string"}],required:!0}}]}}]}}]}}]},description:""},ml:{required:!1,tsType:{name:"union",raw:"PropValue | PropByBreakpoint<PropValue>",elements:[{name:"union",raw:"'0' | Spacing | { custom: number | string }",elements:[{name:"literal",value:"'0'"},{name:"union",raw:"'8px' | '12px' | '16px' | '24px' | '32px' | '48px' | '64px'",elements:[{name:"literal",value:"'8px'"},{name:"literal",value:"'12px'"},{name:"literal",value:"'16px'"},{name:"literal",value:"'24px'"},{name:"literal",value:"'32px'"},{name:"literal",value:"'48px'"},{name:"literal",value:"'64px'"}]},{name:"signature",type:"object",raw:"{ custom: number | string }",signature:{properties:[{key:"custom",value:{name:"union",raw:"number | string",elements:[{name:"number"},{name:"string"}],required:!0}}]}}]},{name:"signature",type:"object",raw:"{ [key in Breakpoint]?: PropValue }",signature:{properties:[{key:{name:"unknown",required:!1},value:{name:"union",raw:"'0' | Spacing | { custom: number | string }",elements:[{name:"literal",value:"'0'"},{name:"union",raw:"'8px' | '12px' | '16px' | '24px' | '32px' | '48px' | '64px'",elements:[{name:"literal",value:"'8px'"},{name:"literal",value:"'12px'"},{name:"literal",value:"'16px'"},{name:"literal",value:"'24px'"},{name:"literal",value:"'32px'"},{name:"literal",value:"'48px'"},{name:"literal",value:"'64px'"}]},{name:"signature",type:"object",raw:"{ custom: number | string }",signature:{properties:[{key:"custom",value:{name:"union",raw:"number | string",elements:[{name:"number"},{name:"string"}],required:!0}}]}}]}}]}}]},description:""},mr:{required:!1,tsType:{name:"union",raw:"PropValue | PropByBreakpoint<PropValue>",elements:[{name:"union",raw:"'0' | Spacing | { custom: number | string }",elements:[{name:"literal",value:"'0'"},{name:"union",raw:"'8px' | '12px' | '16px' | '24px' | '32px' | '48px' | '64px'",elements:[{name:"literal",value:"'8px'"},{name:"literal",value:"'12px'"},{name:"literal",value:"'16px'"},{name:"literal",value:"'24px'"},{name:"literal",value:"'32px'"},{name:"literal",value:"'48px'"},{name:"literal",value:"'64px'"}]},{name:"signature",type:"object",raw:"{ custom: number | string }",signature:{properties:[{key:"custom",value:{name:"union",raw:"number | string",elements:[{name:"number"},{name:"string"}],required:!0}}]}}]},{name:"signature",type:"object",raw:"{ [key in Breakpoint]?: PropValue }",signature:{properties:[{key:{name:"unknown",required:!1},value:{name:"union",raw:"'0' | Spacing | { custom: number | string }",elements:[{name:"literal",value:"'0'"},{name:"union",raw:"'8px' | '12px' | '16px' | '24px' | '32px' | '48px' | '64px'",elements:[{name:"literal",value:"'8px'"},{name:"literal",value:"'12px'"},{name:"literal",value:"'16px'"},{name:"literal",value:"'24px'"},{name:"literal",value:"'32px'"},{name:"literal",value:"'48px'"},{name:"literal",value:"'64px'"}]},{name:"signature",type:"object",raw:"{ custom: number | string }",signature:{properties:[{key:"custom",value:{name:"union",raw:"number | string",elements:[{name:"number"},{name:"string"}],required:!0}}]}}]}}]}}]},description:""},mt:{required:!1,tsType:{name:"union",raw:"PropValue | PropByBreakpoint<PropValue>",elements:[{name:"union",raw:"'0' | Spacing | { custom: number | string }",elements:[{name:"literal",value:"'0'"},{name:"union",raw:"'8px' | '12px' | '16px' | '24px' | '32px' | '48px' | '64px'",elements:[{name:"literal",value:"'8px'"},{name:"literal",value:"'12px'"},{name:"literal",value:"'16px'"},{name:"literal",value:"'24px'"},{name:"literal",value:"'32px'"},{name:"literal",value:"'48px'"},{name:"literal",value:"'64px'"}]},{name:"signature",type:"object",raw:"{ custom: number | string }",signature:{properties:[{key:"custom",value:{name:"union",raw:"number | string",elements:[{name:"number"},{name:"string"}],required:!0}}]}}]},{name:"signature",type:"object",raw:"{ [key in Breakpoint]?: PropValue }",signature:{properties:[{key:{name:"unknown",required:!1},value:{name:"union",raw:"'0' | Spacing | { custom: number | string }",elements:[{name:"literal",value:"'0'"},{name:"union",raw:"'8px' | '12px' | '16px' | '24px' | '32px' | '48px' | '64px'",elements:[{name:"literal",value:"'8px'"},{name:"literal",value:"'12px'"},{name:"literal",value:"'16px'"},{name:"literal",value:"'24px'"},{name:"literal",value:"'32px'"},{name:"literal",value:"'48px'"},{name:"literal",value:"'64px'"}]},{name:"signature",type:"object",raw:"{ custom: number | string }",signature:{properties:[{key:"custom",value:{name:"union",raw:"number | string",elements:[{name:"number"},{name:"string"}],required:!0}}]}}]}}]}}]},description:""},mb:{required:!1,tsType:{name:"union",raw:"PropValue | PropByBreakpoint<PropValue>",elements:[{name:"union",raw:"'0' | Spacing | { custom: number | string }",elements:[{name:"literal",value:"'0'"},{name:"union",raw:"'8px' | '12px' | '16px' | '24px' | '32px' | '48px' | '64px'",elements:[{name:"literal",value:"'8px'"},{name:"literal",value:"'12px'"},{name:"literal",value:"'16px'"},{name:"literal",value:"'24px'"},{name:"literal",value:"'32px'"},{name:"literal",value:"'48px'"},{name:"literal",value:"'64px'"}]},{name:"signature",type:"object",raw:"{ custom: number | string }",signature:{properties:[{key:"custom",value:{name:"union",raw:"number | string",elements:[{name:"number"},{name:"string"}],required:!0}}]}}]},{name:"signature",type:"object",raw:"{ [key in Breakpoint]?: PropValue }",signature:{properties:[{key:{name:"unknown",required:!1},value:{name:"union",raw:"'0' | Spacing | { custom: number | string }",elements:[{name:"literal",value:"'0'"},{name:"union",raw:"'8px' | '12px' | '16px' | '24px' | '32px' | '48px' | '64px'",elements:[{name:"literal",value:"'8px'"},{name:"literal",value:"'12px'"},{name:"literal",value:"'16px'"},{name:"literal",value:"'24px'"},{name:"literal",value:"'32px'"},{name:"literal",value:"'48px'"},{name:"literal",value:"'64px'"}]},{name:"signature",type:"object",raw:"{ custom: number | string }",signature:{properties:[{key:"custom",value:{name:"union",raw:"number | string",elements:[{name:"number"},{name:"string"}],required:!0}}]}}]}}]}}]},description:""}}};const{useArgs:T}=__STORYBOOK_MODULE_PREVIEW_API__,x=n=>{const[{checked:r},a]=T();function l(){n.onToggle(),a({checked:!r})}return A.createElement(c,{...n,onToggle:l,checked:r})},$={title:"Toggle",component:c,args:{checked:!0},render:x},i={args:{onToggle:S(),checked:!0},render:x,play:async({args:n,canvasElement:r})=>{const l=h(r).getByRole("checkbox",{name:"toggle"});await C.click(l),await M(()=>B(n.onToggle).toHaveBeenCalled())}},u={render:x,args:{checked:!1,disabled:!0},play:async({canvasElement:n})=>{const a=h(n).getByRole("checkbox",{name:"toggle"});await C.click(a),await B(a).toBeDisabled()}};var d,b,w;i.parameters={...i.parameters,docs:{...(d=i.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    onToggle: fn(),
+    checked: true
+  },
+  render: Render,
+  play: async ({
+    args,
+    canvasElement
+  }) => {
+    const canvas = within(canvasElement);
+    const checkbox = canvas.getByRole('checkbox', {
+      name: 'toggle'
+    });
+    await userEvent.click(checkbox);
+    await waitFor(() => expect(args.onToggle).toHaveBeenCalled());
+  }
+}`,...(w=(b=i.parameters)==null?void 0:b.docs)==null?void 0:w.source}}};var y,k,f;u.parameters={...u.parameters,docs:{...(y=u.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: Render,
+  args: {
+    checked: false,
+    disabled: true
+  },
+  play: async ({
+    canvasElement
+  }) => {
+    const canvas = within(canvasElement);
+    const checkbox = canvas.getByRole('checkbox', {
+      name: 'toggle'
+    });
+    await userEvent.click(checkbox);
+    await expect(checkbox).toBeDisabled();
+  }
+}`,...(f=(k=u.parameters)==null?void 0:k.docs)==null?void 0:f.source}}};const D=["Primary","Disabled"];export{u as Disabled,i as Primary,D as __namedExportsOrder,$ as default};
+//# sourceMappingURL=Toggle.stories-JKIcXInx.js.map
--- assets/index-l5LxfwhD.js
+++
@@ -1,2 +0,0 @@
-import{g as r}from"./_commonjsHelpers-Cpj98o6Y.js";import{r as o}from"./index-bM2qV1_Y.js";var t=o();const m=r(t);export{m as F,t as r};
-//# sourceMappingURL=index-l5LxfwhD.js.map
