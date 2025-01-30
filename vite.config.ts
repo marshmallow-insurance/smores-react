b617969f8dadee4927f86698576bdda13b3708de
rowanhen <rowan.henseleit@marshmallow.co>
2025-01-30 14:58:45 +0000

    feat: vite bundling test

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,20 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
+import svgrPlugin from 'vite-plugin-svgr'
+import tsConfigPaths from 'vite-tsconfig-paths'
 
-export default defineConfig({})
+export default defineConfig(() => {
+  return {
+    base: 'test_base',
+    build: {
+      lib: {
+        name: '@mrshmllw/smores-react',
+        entry: resolve(__dirname, 'dist/index.js'),
+      },
+      outDir: 'dist',
+      sourcemap: true,
+    },
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    plugins: [tsConfigPaths(), svgrPlugin()],
+  }
+})
