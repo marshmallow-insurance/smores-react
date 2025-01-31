d18985cbd56c2641beb13b57d8735034a66daded
rowanhen <57456071+rowanhen@users.noreply.github.com>
2025-01-31 11:01:37 +0000

    Deploying to gh-pages from @ marshmallow-insurance/smores-react@01487b867cee7d26cce8b7e39f9d7e13c04d669a 🚀

---
+++ assets/Divider.stories-D9rhf3KL.js
@@ -0,0 +1,4 @@
+import{R as r}from"./index-CO7zwj5_.js";import{D as m}from"./Divider-B9R9jvla.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./theme-pdmEyuPW.js";import"./index-BmqobpQ-.js";import"./space-B-76eNGj.js";const u={title:"Divider",component:m,decorators:[s=>r.createElement("div",{style:{margin:"64px"}},r.createElement(s,null))]},e={args:{}};var t,a,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {}
+}`,...(o=(a=e.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const f=["Default"];export{e as Default,f as __namedExportsOrder,u as default};
+//# sourceMappingURL=Divider.stories-D9rhf3KL.js.map
---
+++ assets/Text.stories-BLAfdJ0U.js
@@ -0,0 +1,29 @@
+import{R as e}from"./index-CO7zwj5_.js";import{d as S}from"./theme-pdmEyuPW.js";import{B as k}from"./index-BmqobpQ-.js";import{T as r,f as z}from"./Text-D_0DKx3I.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./space-B-76eNGj.js";import"./polished.esm-CaZDTQnh.js";import"./index-CjJ02WNu.js";import"./focusOutline-CPyS9WD5.js";const p=S(k)`
+  display: grid;
+  grid-template-columns: 0.4fr 1.2fr 1fr;
+  margin-bottom: 24px;
+  gap: 10px;
+`,j=({typos:t})=>e.createElement(k,null,e.createElement(p,null,e.createElement(r,{tag:"p",typo:"base"},"Name"),e.createElement(r,{tag:"p",typo:"base"},"Single Line"),e.createElement(r,{tag:"p",typo:"base"},"Paragraph")),t.map(a=>e.createElement(p,{key:a},e.createElement(r,{tag:"p",typo:"base",color:"sesame"},a),e.createElement(r,{tag:"p",typo:a,color:"liquorice"},"They waited patiently for what seemed a very long time."),!["hero-alternate","hero","heading-alternate","label"].includes(a)&&e.createElement(r,{tag:"p",typo:a,color:"liquorice"},"They waited patiently for what seemed a very long time. They waited patiently for what seemed a very long time.")))),P={title:"Text",component:r,decorators:[t=>e.createElement("div",{style:{margin:"64px"}},e.createElement(t,null))]},C=Object.keys(z),D=["header-large","header-medium","header-small","desc-heavy","desc-medium","desc-light","desc-small","base","base-small","base-xsmall"],o={args:{tag:"p"},render:t=>e.createElement(r,{...t},"The quick brown fox jumps over the lazy dog")},s={args:{tag:"label",typo:"label",color:"liquorice"},render:t=>e.createElement(r,{...t},"The quick brown fox jumps over the lazy dog")},l={args:{tag:"p",title:"a11y title tooltip"},render:t=>e.createElement(r,{...t},"The quick brown fox jumps over the lazy dog")},n={render:()=>e.createElement(j,{typos:C})},c={render:()=>e.createElement(j,{typos:D})};var m,i,d;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    tag: 'p'
+  },
+  render: args => <Text {...args}>The quick brown fox jumps over the lazy dog</Text>
+}`,...(d=(i=o.parameters)==null?void 0:i.docs)==null?void 0:d.source}}};var g,u,y;s.parameters={...s.parameters,docs:{...(g=s.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    tag: 'label',
+    typo: 'label',
+    color: 'liquorice'
+  },
+  render: args => <Text {...args}>The quick brown fox jumps over the lazy dog</Text>
+}`,...(y=(u=s.parameters)==null?void 0:u.docs)==null?void 0:y.source}}};var h,T,b;l.parameters={...l.parameters,docs:{...(h=l.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    tag: 'p',
+    title: 'a11y title tooltip'
+  },
+  render: args => <Text {...args}>The quick brown fox jumps over the lazy dog</Text>
+}`,...(b=(T=l.parameters)==null?void 0:T.docs)==null?void 0:b.source}}};var f,x,E;n.parameters={...n.parameters,docs:{...(f=n.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: () => <TypoCollection typos={typos} />
+}`,...(E=(x=n.parameters)==null?void 0:x.docs)==null?void 0:E.source}}};var w,v,q;c.parameters={...c.parameters,docs:{...(w=c.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  render: () => <TypoCollection typos={deprecatedTypos} />
+}`,...(q=(v=c.parameters)==null?void 0:v.docs)==null?void 0:q.source}}};const A=["Default","Label","WithTitle","Collection","DeprecatedTypos"];export{n as Collection,o as Default,c as DeprecatedTypos,s as Label,l as WithTitle,A as __namedExportsOrder,P as default};
+//# sourceMappingURL=Text.stories-BLAfdJ0U.js.map
