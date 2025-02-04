89a348dfe831a37acdc151ec9a244e15b7b24201
Rowan Henseleit <57456071+rowanhen@users.noreply.github.com>
2025-02-04 08:31:13 +0000

    bump: BREAKING CHANGES, type module and deleting components (#4225)
    fix: RadioGroup value type
    
    chore: Major release
    
    BREAKING CHANGES: deleting Emoji and Brandcard components, adjusting package.json to type module

--- src/RadioGroup/RadioGroup.tsx
+++ src/RadioGroup/RadioGroup.tsx
@@ -29,7 +29,7 @@ export type RadioGroupProps<Value extends BaseValueType = BaseValueType> = {
     value: Value
     bodyCopy?: string
   }>
-  onChange: (value: BaseValueType) => void
+  onChange: (value: Value) => void
   onBlur?: (e: FocusEvent<HTMLInputElement>) => void
   value: Value
   displayType?: DisplayType
@@ -89,6 +89,7 @@ const RadioGroupComponent = <Value extends BaseValueType>(
               label={option.label}
               value={option.value}
               checked={isSelected}
+              // @ts-expect-error TODO: address this type inconsistency in another PR
               onChange={onChange}
               onBlur={onBlur}
               displayType={displayType}
