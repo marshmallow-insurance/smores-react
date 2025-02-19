a582c137092896d21cbc2196c67872d106ce8d70
David Gomes <david.gomes@marshmallow.co>
2025-02-19 10:57:29 +0000

    move helper types to own file

--- src/Table/types.ts
+++ src/Table/types.ts
@@ -2,6 +2,7 @@ import React, { ReactElement, ReactNode } from 'react'
 import { ButtonProps } from '../Button/Button'
 import { IconStrictProps } from '../IconStrict'
 import { Color } from '../theme'
+import type { BoxSpacing } from './helper.types'
 
 export type TableStylesProps = {
   hasKeyline?: boolean
@@ -80,28 +81,6 @@ export interface TableColumn<T> {
   hideOverflow?: boolean
   cell?: RowCellRenderer<T>
 }
-
-/**
- * A type that represents a pixel value with the "px" suffix.
- *
- * @example '0' | '12px' | '24px'
- */
-type PixelSuffix = `${number}px`
-
-type Spacing = '0' | PixelSuffix
-/**
- * A type that represents a padding value with the "px" suffix.
- *
- * The value can be a single value, or a string with 2, 3, or 4 values.
- * The values are applied in the following order: top, right, bottom, left.
- *
- * @example '0' | '12px' | '24px' | '12px 24px' | '12px 0 36px' | '12px 24px 36px 48px'
- */
-type BoxSpacing =
-  | Spacing
-  | `${Spacing} ${Spacing}`
-  | `${Spacing} ${Spacing} ${Spacing}`
-  | `${Spacing} ${Spacing} ${Spacing} ${Spacing}`
 
 type SubElementProps = {
   bgColor?: Color
