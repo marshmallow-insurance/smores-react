2114221a52da712e5463e0eb810b55c8be835927
Mat Diak <mat.diak@marshmallow.co>
2025-02-21 15:59:39 +0000

    feat: removes deprecated typos
    BREAKING CHANGE: removes deprecated typos

--- src/Text/Text.tsx
+++ src/Text/Text.tsx
@@ -2,8 +2,7 @@ import React, { FC, forwardRef, LabelHTMLAttributes, ReactNode } from 'react'
 import styled, { css } from 'styled-components'
 
 import { Box } from '../Box'
-import { linkStyleOverride } from '../Link/Link'
-import { Color, theme } from '../theme'
+import { Color } from '../theme'
 import { MarginProps } from '../utils/space'
 import { fontStyleMapping } from './fontMapping'
 
@@ -16,23 +15,6 @@ interface IText {
   $color: Color
   $cursor: string
 }
-
-/**
- * @deprecated Use only new Typo
- */
-
-export type DeprecatedTypo =
-  | 'header-large'
-  | 'header-medium'
-  | 'header-small'
-  | 'desc-heavy'
-  | 'desc-medium'
-  | 'desc-light'
-  | 'desc-small'
-  | 'base'
-  | 'base-small'
-  | 'base-xsmall'
-  | 'label-large'
 
 export type Typo =
   | 'hero-alternate'
@@ -97,149 +79,14 @@ export const Text: FC<TextProps> = forwardRef<HTMLElement, TextProps>(
 
 Text.displayName = 'Text'
 
-const isNewTypo = (value: string): value is Typo => {
+const isTypo = (value: string): value is Typo => {
   return Object.keys(fontStyleMapping).includes(value)
 }
 
 const Container = styled(Box)<IText>(
-  ({ $align, $color, $cursor, $typo }) => css`
+  ({ $typo }) => css`
     /** TYPOGRAPHY STYLES */
 
-    ${isNewTypo($typo) && fontStyleMapping[$typo]}
-
-    /** DEPRECATED TYPOGRAPHY STYLES */
-    ${$typo === 'header-large' &&
-    css`
-      font-size: 24px;
-      line-height: 31px;
-      font-weight: 500;
-
-      @media (min-width: 768px) {
-        font-size: 42px;
-        line-height: 54px;
-      }
-    `}
-
-  ${$typo === 'header-medium' &&
-    css`
-      font-size: 21px;
-      line-height: 27px;
-      font-weight: 500;
-
-      @media (min-width: 768px) {
-        font-size: 32px;
-        line-height: 41px;
-      }
-    `}
-
-  ${$typo === 'header-small' &&
-    css`
-      font-size: 18px;
-      line-height: 23px;
-      font-weight: 500;
-
-      @media (min-width: 768px) {
-        font-size: 21px;
-        line-height: 27px;
-      }
-    `}
-
-  ${$typo === 'desc-heavy' &&
-    css`
-      font-size: 14px;
-      line-height: 20px;
-      font-weight: 700;
-
-      @media (min-width: 768px) {
-        font-size: 16px;
-        line-height: 23px;
-      }
-    `}
-
-  ${$typo === 'desc-medium' &&
-    css`
-      font-size: 14px;
-      line-height: 20px;
-      font-weight: 500;
-
-      @media (min-width: 768px) {
-        font-size: 16px;
-        line-height: 23px;
-      }
-    `}
-
-  ${$typo === 'desc-light' &&
-    css`
-      font-size: 14px;
-      line-height: 20px;
-      font-weight: 400;
-
-      @media (min-width: 768px) {
-        font-size: 16px;
-        line-height: 23px;
-      }
-    `}
-
-  ${$typo === 'desc-small' &&
-    css`
-      font-size: 12px;
-      line-height: 16px;
-      font-weight: 400;
-
-      @media (min-width: 768px) {
-        font-size: 14px;
-        line-height: 18px;
-      }
-    `}
-
-  ${$typo === 'base' &&
-    css`
-      font-size: 14px;
-      line-height: 23px;
-      font-weight: 400;
-
-      @media (min-width: 768px) {
-        font-size: 16px;
-        line-height: 26px;
-      }
-    `}
-
-  ${$typo === 'base-small' &&
-    css`
-      font-size: 12px;
-      line-height: 20px;
-      font-weight: 400;
-
-      @media (min-width: 768px) {
-        font-size: 14px;
-        line-height: 23px;
-      }
-    `}
-
-  ${$typo === 'base-xsmall' &&
-    css`
-      font-size: 10px;
-      line-height: 16px;
-      font-weight: 400;
-
-      @media (min-width: 768px) {
-        font-size: 12px;
-        line-height: 19px;
-      }
-    `}
-  
-  ${$typo === 'label-large' &&
-    css`
-      font-size: 12px;
-      line-height: 100%;
-      font-weight: 700;
-      text-transform: uppercase;
-      letter-spacing: 0.75px;
-    `}
-
-    text-align: ${$align};
-    cursor: ${$cursor};
-    color: ${theme.colors[$color]};
-    ${linkStyleOverride({ color: theme.colors[$color] })}
+    ${isTypo($typo) && fontStyleMapping[$typo]}
   `,
 )
