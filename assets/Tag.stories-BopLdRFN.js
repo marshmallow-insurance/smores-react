12cece29414dace795dc579fe8931cd34e30d345
matdiak <mat.diak@marshmallow.co>
2025-02-24 16:37:24 +0000

    Deploying to gh-pages from @ marshmallow-insurance/smores-react@d38b7d8e249018a5bb2e5f231fcc73730f00d967 🚀

---
+++ assets/Tag.stories-BopLdRFN.js
@@ -0,0 +1,32 @@
+import{R as r}from"./index-B5OHeJSP.js";import{B as f}from"./index-BHsagutR.js";import{t as g}from"./theme-BO-nAlgg.js";import{T as s}from"./Tag-BPEeNlTG.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./space-B-76eNGj.js";import"./index-512cxWno.js";import"./Text-CbaD2oOk.js";import"./polished.esm-D2Pu4BJi.js";import"./index-DjW-F613.js";import"./focusOutline-CQ36wr7y.js";const v={title:"Tag",component:s,decorators:[e=>r.createElement("div",{style:{margin:"64px"}},r.createElement(e,null))]},a={args:{bgColor:"feijoa",color:"cream",label:"Example"},render:e=>r.createElement(s,{...e})},t={render:e=>{const d=["boba","liquorice","sesame","spearmint","feijoa","strawberry","apple","compareTheMarket","confused","onfido","x","meta","ravelin","hometree"],u=["pistachio","caramel","stripe"],b=Object.keys(g.colors).map(o=>r.createElement(s,{...e,label:o,bgColor:o,color:u.includes(o)?"boba":d.includes(o)?"cream":"liquorice"}));return r.createElement(f,{flex:!0,direction:"column",gap:{custom:"4px"},width:"200px"},b)},parameters:{a11y:{config:{rules:[{id:"color-contrast",enabled:!1}]}}}};var l,c,m;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    bgColor: 'feijoa',
+    color: 'cream',
+    label: 'Example'
+  },
+  render: args => <Tag {...args} />
+}`,...(m=(c=a.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};var n,i,p;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: args => {
+    const darkCol: Color[] = ['boba', 'liquorice', 'sesame', 'spearmint', 'feijoa', 'strawberry', 'apple', 'compareTheMarket', 'confused', 'onfido', 'x', 'meta', 'ravelin', 'hometree'];
+    const otherCol: Color[] = ['pistachio', 'caramel', 'stripe'];
+    const map = Object.keys(theme.colors).map(col => {
+      return <Tag {...args} label={col} bgColor={col as Color} color={otherCol.includes(col as Color) ? 'boba' : darkCol.includes(col as Color) ? 'cream' : 'liquorice'} />;
+    });
+    return <Box flex direction="column" gap={{
+      custom: '4px'
+    }} width="200px">
+        {map}
+      </Box>;
+  },
+  parameters: {
+    a11y: {
+      config: {
+        rules: [{
+          id: 'color-contrast',
+          enabled: false
+        }]
+      }
+    }
+  }
+}`,...(p=(i=t.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};const O=["Default","Colors"];export{t as Colors,a as Default,O as __namedExportsOrder,v as default};
+//# sourceMappingURL=Tag.stories-BopLdRFN.js.map
