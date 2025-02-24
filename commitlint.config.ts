558487f9a95dd49c540b4e3eeb7c5027e6620fed
Mat Diak <mat.diak@marshmallow.co>
2025-02-24 14:46:17 +0000

    chore: add exception in commitlint to allow breaking change in commit title

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,6 +2,7 @@ import { RuleConfigSeverity } from '@commitlint/types'
 
 export default {
   parserPreset: 'conventional-changelog-conventionalcommits',
+  ignores: [(commit: string) => /^BREAKING(?:[- ]CHANGE(?:S)?):/.test(commit)],
   rules: {
     'type-case': [RuleConfigSeverity.Error, 'always', 'lower-case'],
     'type-empty': [RuleConfigSeverity.Error, 'never'],
