49dca4d822e5a8d1b02a608c7a7ca92b1b11eb53
DeadEnglish <git@liamcanetti.co.uk>
2025-03-25 09:32:33 +0000

    Deploying to gh-pages from @ marshmallow-insurance/smores-react@47e81be91c925a9eb0538558fef682746609fc44 🚀

---
+++ assets/colors.stories-CJ_dro2T.js
@@ -0,0 +1,18 @@
+import{R as e}from"./index-Dxs5m6lS.js";import{d as i,t as l}from"./theme-BXEoD8b-.js";import{B as o}from"./index-C1oJge18.js";import{D as y}from"./Divider-sKAkQSF5.js";import"./index-DoT6b7c8.js";import{T as m}from"./Text-Bbj5LYJq.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./space-B-76eNGj.js";import"./polished.esm-loEtNAQt.js";import"./index-BTZCzcOv.js";import"./focusOutline-C3BFCsJi.js";function b(){var r="/home/runner/work/smores-react/smores-react/src/Divider/index.ts",a="edd9b3a6839547ebf0f708c875d5b67630a15637",c=window,p="__coverage__",x={path:"/home/runner/work/smores-react/smores-react/src/Divider/index.ts",statementMap:{},fnMap:{},branchMap:{},s:{},f:{},b:{},inputSourceMap:{version:3,file:null,sources:["/home/runner/work/smores-react/smores-react/src/Divider/index.ts"],names:[],mappings:"AAAA,SAAS,eAAe;"},_coverageSchema:"1a1c01bbd47fc00a2c39e90264f33305004495a9",hash:"edd9b3a6839547ebf0f708c875d5b67630a15637"},n=c[p]||(c[p]={});(!n[r]||n[r].hash!==a)&&(n[r]=x);var d=n[r];return b=function(){return d},d}b();const v=["lollipop","marshmallowPink","bubblegum","fairyFloss"],E=["boba","liquorice","sesame","chia"],w=["custard","mascarpone","coconut","cream"],C=["spearmint","feijoa","blueberry","macaroon","pistachio","matcha","caramel","peanut","marzipan","oatmeal"],k=["strawberry","watermelon","apple","mint","lemon","sherbert","tangerine"],_=["compareTheMarket","confused","onfido","x","premfina","checkout","meta","stripe","intercom","ravelin","rac","hometree"],A=({colorName:r})=>{const a=l.colors[r];return e.createElement(P,{width:"224px"},e.createElement(B,{$colorName:r,width:"100%",pt:{custom:"50%"}}),e.createElement(y,null),e.createElement(o,{px:"16px",py:{custom:4}},e.createElement(m,{tag:"span",typo:"body-regular"},r),e.createElement(m,{tag:"span",typo:"caption",color:"sesame"},a)))},t=({title:r,colors:a})=>e.createElement(o,{width:"100%"},e.createElement(o,{mb:"16px"},e.createElement(m,{tag:"span",typo:"headline-small"},r)),e.createElement(T,null,a.map(c=>e.createElement(A,{key:c,colorName:c})))),g=()=>e.createElement(D,null,e.createElement(t,{title:"Core primary",colors:v}),e.createElement(t,{title:"Core secondary",colors:E}),e.createElement(t,{title:"Core tertiary",colors:w}),e.createElement(t,{title:"Brand secondary",colors:C}),e.createElement(t,{title:"Traffic light",colors:k}),e.createElement(t,{title:"Third party brand",colors:_})),G={title:"Foundation/Colors",components:g},S=()=>e.createElement(g,null),s=S.bind({}),T=i(o)`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  gap: 16px;
+`,D=i(o)`
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+`,P=i(o)`
+  background-color: ${l.colors.cream};
+  border: 1px solid ${l.colors.chia};
+  border-radius: 8px;
+  overflow: hidden;
+`,B=i(o)`
+  background-color: ${({$colorName:r})=>l.colors[r]};
+`;var u,f,h;s.parameters={...s.parameters,docs:{...(u=s.parameters)==null?void 0:u.docs,source:{originalSource:"() => <ColorsPage />",...(h=(f=s.parameters)==null?void 0:f.docs)==null?void 0:h.source}}};const H=["List"];export{s as List,H as __namedExportsOrder,G as default};
+//# sourceMappingURL=colors.stories-CJ_dro2T.js.map
