edcc4025e50e87b46da224c8c4a08106c023f68c
DeadEnglish <git@liamcanetti.co.uk>
2025-04-09 15:25:59 +0000

    Deploying to gh-pages from @ marshmallow-insurance/smores-react@f99ef45995cbfc700da0883d663fcc6dc0b2f66a 🚀

--- assets/Divider.stories-CFK5LxBW.js
+++
@@ -1,4 +0,0 @@
-import{R as r}from"./index-F2Fta7eo.js";import{D as m}from"./Divider-CHQFw--9.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./theme-DC42ieJR.js";import"./index-C-N0nR6d.js";import"./space-B-76eNGj.js";const u={title:"Divider",component:m,decorators:[s=>r.createElement("div",{style:{margin:"64px"}},r.createElement(s,null))]},e={args:{}};var t,a,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {}
-}`,...(o=(a=e.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const f=["Default"];export{e as Default,f as __namedExportsOrder,u as default};
-//# sourceMappingURL=Divider.stories-CFK5LxBW.js.map
