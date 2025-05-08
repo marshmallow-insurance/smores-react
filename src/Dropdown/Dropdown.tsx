dbabfc55c7ca35bd975d069307eae7db1c8f650b
Gergö Szabo <gergo.szabo@marshmallow.co>
2025-05-08 13:29:28 +0200

    refactor: ternary to short circuit

--- src/Dropdown/Dropdown.tsx
+++ src/Dropdown/Dropdown.tsx
@@ -126,7 +126,7 @@ export const Dropdown = forwardRef(function Dropdown(
           name={name}
           $frontIcon={frontIcon}
           $fallbackStyle={fallbackStyle}
-          value={value ? value : ''}
+          value={value ?? ''}
         >
           {hasOptGroups ? (
             <optgroup label={defaultOptionLabel()}>
