7881fd96832075d11b00079572caf45194b0d6f1
snegyeliczky <44034297+snegyeliczky@users.noreply.github.com>
2025-06-18 12:04:03 +0000

    Deploying to gh-pages from @ marshmallow-insurance/smores-react@208517069063b7111fbc0fd120d2fc3fc8ad1e83 🚀

---
+++ assets/CheckBox-DAhRzz5U.js
@@ -0,0 +1,90 @@
+import{r as p,R as r}from"./index-F2Fta7eo.js";import{t as e,d as m}from"./theme-DopFg7tV.js";import{f as C}from"./focusOutline-Dq6pk-S8.js";import"./index-Cmdc06k_.js";import{u as b}from"./id-C0eNdix5.js";import{T as x}from"./Text-DPlHS_m4.js";function A(){var n="/home/runner/work/smores-react/smores-react/src/CheckBox/CheckBox.tsx",i="e4dbb68d3368e1b51962bc3cc7e4e7bb7975637d",c=globalThis,a="__coverage__",s={path:"/home/runner/work/smores-react/smores-react/src/CheckBox/CheckBox.tsx",statementMap:{0:{start:{line:7,column:24},end:{line:28,column:1}},1:{start:{line:9,column:15},end:{line:9,column:34}},2:{start:{line:10,column:4},end:{line:26,column:159}},3:{start:{line:29,column:18},end:{line:67,column:1}},4:{start:{line:34,column:28},end:{line:34,column:115}},5:{start:{line:68,column:21},end:{line:114,column:1}},6:{start:{line:115,column:17},end:{line:119,column:1}},7:{start:{line:120,column:0},end:{line:120,column:715}}},fnMap:{0:{name:"CheckBox2",decl:{start:{line:8,column:11},end:{line:8,column:20}},loc:{start:{line:8,column:86},end:{line:27,column:3}},line:8},1:{name:"(anonymous_1)",decl:{start:{line:34,column:12},end:{line:34,column:13}},loc:{start:{line:34,column:28},end:{line:34,column:115}},line:34}},branchMap:{0:{loc:{start:{line:15,column:15},end:{line:15,column:49}},type:"cond-expr",locations:[{start:{line:15,column:23},end:{line:15,column:35}},{start:{line:15,column:38},end:{line:15,column:49}}],line:15},1:{loc:{start:{line:26,column:75},end:{line:26,column:157}},type:"binary-expr",locations:[{start:{line:26,column:75},end:{line:26,column:80}},{start:{line:26,column:84},end:{line:26,column:92}},{start:{line:26,column:112},end:{line:26,column:157}}],line:26},2:{loc:{start:{line:34,column:28},end:{line:34,column:115}},type:"cond-expr",locations:[{start:{line:34,column:37},end:{line:34,column:75}},{start:{line:34,column:78},end:{line:34,column:115}}],line:34}},s:{0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0},f:{0:0,1:0},b:{0:[0,0],1:[0,0,0],2:[0,0]},inputSourceMap:{version:3,file:null,sources:["/home/runner/work/smores-react/smores-react/src/CheckBox/CheckBox.tsx"],names:["CheckBox"],mappings:"AAAA,OAAO,SAAS,kBAA6B;AAC7C,OAAO,YAAY;AACnB,SAAS,oBAAoB;AAE7B,SAAS,YAAY;AACrB,SAAS,aAAa;AACtB,SAAS,mBAAmB;AAWrB,aAAM,WAAW;AAAA,EACtB,SAASA,UACP,EAAE,IAAI,QAAQ,SAAS,UAAU,QAAQ,OAAO,SAAS,GACzD,KACA;AACA,UAAM,KAAK,YAAY,MAAM;AAC7B,WACE,0DACE,oCAAC,gBAAa,MACZ;AAAA,MAAC;AAAA;AAAA,QACC,KAAI;AAAA,QACJ,MAAK;AAAA,QACL,OAAO,QAAQ,eAAe;AAAA;AAAA,MAE7B;AAAA,IACH,GAEA;AAAA,MAAC;AAAA;AAAA,QACC;AAAA,QACA,MAAK;AAAA,QACL;AAAA,QACA,UAAU;AAAA;AAAA,IACZ,GACA,oCAAC,aAAU,QAAQ,OAAO,CAC5B,GACC,SAAS,YAAY,oCAAC,gBAAU,QAAS,CAC5C;AAAA,EAEJ;AACF;AAEA,MAAM,YAAY,OAAO;AAAA;AAAA;AAAA;AAAA;AAAA,YAKb,CAAC,EAAE,OAAO,MAClB,SACI,aAAa,MAAM,OAAO,UAAU,KACpC,aAAa,MAAM,OAAO,SAAS,EAAE;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA,wBAarB,MAAM,OAAO,KAAK;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA,wBAelB,MAAM,OAAO,KAAK;AAAA;AAAA;AAAA;AAAA;AAAA;AAO1C,MAAM,eAAe,OAAO;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA,wBAaJ,MAAM,OAAO,KAAK;AAAA;AAAA,kBAExB,SAAS;AAAA,0BACD,MAAM,OAAO,SAAS;AAAA,0BACtB,MAAM,OAAO,SAAS;AAAA;AAAA;AAAA,kBAG9B,SAAS;AAAA;AAAA;AAAA;AAAA,kBAIT,SAAS;AAAA;AAAA;AAAA;AAAA,MAIrB,aAAa,EAAE,UAAU,qBAAqB,SAAS,GAAG,CAAC,CAAC;AAAA;AAAA;AAAA;AAAA,MAI5D,SAAS;AAAA,0BACW,MAAM,OAAO,OAAO;AAAA,0BACpB,MAAM,OAAO,SAAS;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAchD,MAAM,WAAW,OAAO;AAAA;AAAA;AAAA,WAGb,MAAM,OAAO,UAAU;AAAA;;"},_coverageSchema:"1a1c01bbd47fc00a2c39e90264f33305004495a9",hash:"e4dbb68d3368e1b51962bc3cc7e4e7bb7975637d"},o=c[a]||(c[a]={});(!o[n]||o[n].hash!==i)&&(o[n]=s);var l=o[n];return A=function(){return l},l}A();const h=(A().s[0]++,p.forwardRef(function({id:i,checked:c,children:a,toggle:s,error:o,errorMsg:l},d){A().f[0]++;const u=(A().s[1]++,b(i));return A().s[2]++,r.createElement(r.Fragment,null,r.createElement(B,{id:u},r.createElement(x,{tag:"span",typo:"body-regular",color:o?(A().b[0][0]++,"strawberry"):(A().b[0][1]++,"liquorice")},a),r.createElement("input",{ref:d,type:"checkbox",checked:c,onChange:s}),r.createElement(t,{$error:o})),(A().b[1][0]++,o&&(A().b[1][1]++,l)&&(A().b[1][2]++,r.createElement(f,null,l))))})),t=(A().s[3]++,m.span`
+  position: absolute;
+  left: 0;
+  width: 24px;
+  height: 24px;
+  border: ${({$error:n})=>(A().f[1]++,A().s[4]++,n?(A().b[2][0]++,`solid 2px ${e.colors.strawberry}`):(A().b[2][1]++,`solid 2px ${e.colors.liquorice}`))};
+  box-sizing: border-box;
+  border-radius: 1px;
+
+  &:before {
+    content: '';
+    position: absolute;
+    display: none;
+    top: 9px;
+    left: 5px;
+    width: 3px;
+    height: 8px;
+    border-radius: 4px;
+    background-color: ${e.colors.cream};
+    -webkit-transform: rotate(316deg);
+    -ms-transform: rotate(316deg);
+    transform: rotate(316deg);
+  }
+
+  &:after {
+    content: '';
+    position: absolute;
+    display: none;
+    top: 3px;
+    left: 11px;
+    width: 3px;
+    height: 15px;
+    border-radius: 4px;
+    background-color: ${e.colors.cream};
+    -webkit-transform: rotate(43deg);
+    -ms-transform: rotate(43deg);
+    transform: rotate(43deg);
+  }
+`),B=(A().s[5]++,m.label`
+  position: relative;
+  display: flex;
+  align-items: flex-start;
+  padding-left: 32px;
+  user-select: none;
+  cursor: pointer;
+  min-height: 24px;
+
+  input {
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+    background-color: ${e.colors.cream};
+
+    &:checked ~ ${t} {
+      background-color: ${e.colors.liquorice};
+      border: solid 2px ${e.colors.liquorice};
+    }
+
+    &:checked ~ ${t}:before {
+      display: block;
+    }
+
+    &:checked ~ ${t}:after {
+      display: block;
+    }
+
+    ${C({selector:`&:focus-visible ~ ${t}`})}
+  }
+
+  &:hover {
+    ${t} {
+      background-color: ${e.colors.coconut};
+      border: solid 2px ${e.colors.liquorice};
+    }
+  }
+
+  @media (min-width: 768px) {
+    padding-left: 32px;
+  }
+
+  span {
+    border-radius: 6px;
+    line-height: 24px;
+  }
+`),f=(A().s[6]++,m.div`
+  margin-top: 4px;
+  font-size: 12px;
+  color: ${e.colors.strawberry};
+`);A().s[7]++;h.__docgenInfo={description:"",methods:[],displayName:"CheckBox",props:{id:{required:!1,tsType:{name:"string"},description:""},checked:{required:!0,tsType:{name:"boolean"},description:""},children:{required:!0,tsType:{name:"ReactNode"},description:""},toggle:{required:!0,tsType:{name:"signature",type:"function",raw:"() => void",signature:{arguments:[],return:{name:"void"}}},description:""},error:{required:!1,tsType:{name:"boolean"},description:""},errorMsg:{required:!1,tsType:{name:"string"},description:""}}};export{h as C};
+//# sourceMappingURL=CheckBox-DAhRzz5U.js.map
