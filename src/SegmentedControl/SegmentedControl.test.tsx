b68bef84c83b46e9b3dc6173c4a3f8b0dac5bf5f
Ella Taylor <ella.taylor@marshmallow.co>
2025-09-03 17:17:36 +0100

    feat(testing): add more snapshots (pr number 4)

---
+++ src/SegmentedControl/SegmentedControl.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { expect, it } from 'vitest'
+import { render } from '@testing-library/react'
+import { SegmentedControl } from './SegmentedControl'
+import { noop } from '../utils/noop'
+
+describe('SegmentedControl', () => {
+  it('renders correctly with default props and a tag', () => {
+    const { container } = render(
+      <SegmentedControl
+        options={[
+          { label: 'Option 1', value: 'option1' },
+          { label: 'Option 2', value: 'option2', tag: 'tag 2' },
+        ]}
+        value="option1"
+        onChange={noop}
+        showTag={true}
+      />,
+    )
+    expect(container).toMatchSnapshot()
+  })
+})
