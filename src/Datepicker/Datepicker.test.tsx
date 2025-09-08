afb5084872cfe382f55ef533a207222eda4ed1d9
Ella Taylor <ella.taylor@marshmallow.co>
2025-09-08 13:13:39 +0100

    chore(datepicker test): use date in past for consistency

--- src/Datepicker/Datepicker.test.tsx
+++ src/Datepicker/Datepicker.test.tsx
@@ -6,7 +6,13 @@ import { noop } from '../utils/noop'
 
 describe('Datepicker', () => {
   it('renders correctly with default props', () => {
-    const { container } = render(<Datepicker onDateSelect={noop} />)
+    const { container } = render(
+      <Datepicker
+        onDateSelect={noop}
+        fromDate={new Date('2020-07-01')}
+        range={14}
+      />,
+    )
     expect(container).toMatchSnapshot()
   })
 })
