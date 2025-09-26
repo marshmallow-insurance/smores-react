1b6b3f3f17915185b1a63288a658d8e8e2f51213
Liam Canetti <git@liamcanetti.co.uk>
2025-09-26 14:41:37 +0100

    test: add utils for rendering

---
+++ src/testUtils/index.tsx
@@ -0,0 +1,15 @@
+import React, { ReactElement } from 'react'
+import { render, RenderOptions } from '@testing-library/react'
+import { SmoresThemeProvider } from '../ThemeProvider/ThemeProvider'
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return <SmoresThemeProvider>{children}</SmoresThemeProvider>
+}
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>,
+) => render(ui, { wrapper: Providers, ...options })
+
+export * from '@testing-library/react'
+export { customRender as render }
