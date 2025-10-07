9e61cc4adc88d2d6b1956aec8a4270ee75ba359d
Ella Taylor <ella.taylor@marshmallow.co>
2025-10-07 15:57:11 +0100

    chore(text): add tests and jest-dom

---
+++ src/Text/Text.test.tsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import { render, screen } from '../testUtils'
+import { it, describe } from 'vitest'
+import { Text } from './Text'
+
+describe('Text component', () => {
+  it('renders correctly with default props', () => {
+    const { container } = render(
+      <Text>The quick brown fox jumps over the lazy dog</Text>,
+    )
+    expect(
+      screen.getByText('The quick brown fox jumps over the lazy dog'),
+    ).toBeTruthy()
+
+    expect(container).toMatchSnapshot()
+
+    expect(container.firstChild).toHaveStyle({ color: '#292924' })
+  })
+
+  it('renders color.feedback.negative.200 / strawberry correctly with NEW color name', () => {
+    const { container } = render(
+      <Text color="color.feedback.negative.200">
+        The quick brown fox jumps over the lazy dog
+      </Text>,
+    )
+    expect(
+      screen.getByText('The quick brown fox jumps over the lazy dog'),
+    ).toBeTruthy()
+
+    expect(container).toMatchSnapshot()
+
+    expect(container.firstChild).toHaveStyle({ color: '#d03c30' })
+  })
+
+  it('renders color.feedback.negative.200 / strawberry correctly with OLD color name', () => {
+    const { container } = render(
+      <Text color="strawberry">
+        The quick brown fox jumps over the lazy dog
+      </Text>,
+    )
+    expect(
+      screen.getByText('The quick brown fox jumps over the lazy dog'),
+    ).toBeTruthy()
+
+    expect(container).toMatchSnapshot()
+
+    expect(container.firstChild).toHaveStyle({ color: '#d03c30' })
+  })
+
+  it('renders color.feedback.positive.200 / apple correctly with NEW color name', () => {
+    const { container } = render(
+      <Text color="color.feedback.positive.200">
+        The quick brown fox jumps over the lazy dog
+      </Text>,
+    )
+    expect(
+      screen.getByText('The quick brown fox jumps over the lazy dog'),
+    ).toBeTruthy()
+
+    expect(container).toMatchSnapshot()
+
+    expect(container.firstChild).toHaveStyle({ color: '#317a4c' })
+  })
+
+  it('renders color.feedback.positive.200 / apple correctly with OLD color name', () => {
+    const { container } = render(
+      <Text color="apple">The quick brown fox jumps over the lazy dog</Text>,
+    )
+    expect(
+      screen.getByText('The quick brown fox jumps over the lazy dog'),
+    ).toBeTruthy()
+
+    expect(container).toMatchSnapshot()
+
+    expect(container.firstChild).toHaveStyle({ color: '#317a4c' })
+  })
+})
