326a5127546a13e421921e6860e6cf68c9c55430
DeadEnglish <git@liamcanetti.co.uk>
2025-10-07 15:59:43 +0000

    Deploying to gh-pages from @ marshmallow-insurance/smores-react@7513f77b98662e3db35bd3101e080d2e18b23d99 🚀

---
+++ assets/Badge.stories-BWUq-i8n.js
@@ -0,0 +1,15 @@
+import{B as c,g as m,a as l}from"./exampleImg-Dtx8HmbF.js";import"./index-DbOFzPHX.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./styled-components.browser.esm-BbZQrDuP.js";import"./theme-D-5TnJVB.js";import"./index-COZvmTcE.js";import"./Text-BOGL1RzE.js";import"./index-DskQjIgy.js";import"./space-BLajQSvN.js";import"./polished.esm-CYsXSD1d.js";import"./index-CHrSHfjR.js";import"./focusOutline-Cfflnwlf.js";import"./variables-DcGMWv_V.js";const f={title:"Badge",component:c,args:{title:"badge"}},r={args:{borderColour:"lollipop",size:l.Lg,src:m().src}},e={args:{borderColour:"lollipop",size:l.Lg,src:m().src,disabled:!0}};var o,s,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    borderColour: 'lollipop',
+    size: BadgeSize.Lg,
+    src: getExampleImg().src
+  }
+}`,...(a=(s=r.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};var t,i,p;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    borderColour: 'lollipop',
+    size: BadgeSize.Lg,
+    src: getExampleImg().src,
+    disabled: true
+  }
+}`,...(p=(i=e.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};const y=["Primary","Disabled"];export{e as Disabled,r as Primary,y as __namedExportsOrder,f as default};
+//# sourceMappingURL=Badge.stories-BWUq-i8n.js.map
---
+++ assets/Link.stories-IxiadjpL.js
@@ -0,0 +1,52 @@
+import{R as t}from"./index-DbOFzPHX.js";import"./index-COZvmTcE.js";import{L as f,T as I}from"./Text-BOGL1RzE.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./styled-components.browser.esm-BbZQrDuP.js";import"./index-DskQjIgy.js";import"./space-BLajQSvN.js";import"./polished.esm-CYsXSD1d.js";import"./index-CHrSHfjR.js";import"./theme-D-5TnJVB.js";import"./focusOutline-Cfflnwlf.js";import"./variables-DcGMWv_V.js";const v={title:"Link",component:f,argTypes:{onClick:{action:"clicked"}},decorators:[a=>t.createElement("div",{style:{margin:"64px"}},t.createElement(a,null))]},e={args:{href:"https://www.google.com",openInNewTab:!0,children:"Google link",typo:"regular"}},r={args:{href:"https://www.google.com",openInNewTab:!0,children:"Google link",typo:"regular",highlight:!0},parameters:{a11y:{config:{rules:[{id:"color-contrast",enabled:!1}]}}}},o={args:{href:"https://www.google.com",openInNewTab:!0,children:"Google link",typo:"regular",iconToRender:"new-window",isTrailingIcon:!1}},n={args:{href:"https://www.google.com",openInNewTab:!0,children:"Google link",typo:"regular",href:"https://en.wikipedia.org/wiki/Lorem_ipsum",children:"here"},render:a=>t.createElement(I,{tag:"p",color:"liquorice"},"Lorem Ipsum is simply dummy text of the printing and typesetting industry, more details ",t.createElement(f,{...a}),". Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.")};var s,i,l;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    href: 'https://www.google.com',
+    openInNewTab: true,
+    children: 'Google link',
+    typo: 'regular'
+  }
+}`,...(l=(i=e.parameters)==null?void 0:i.docs)==null?void 0:l.source}}};var p,c,g;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    href: 'https://www.google.com',
+    openInNewTab: true,
+    children: 'Google link',
+    typo: 'regular',
+    highlight: true
+  },
+  parameters: {
+    a11y: {
+      config: {
+        rules: [{
+          id: 'color-contrast',
+          enabled: false
+        }]
+      }
+    }
+  }
+}`,...(g=(c=r.parameters)==null?void 0:c.docs)==null?void 0:g.source}}};var m,d,h;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    href: 'https://www.google.com',
+    openInNewTab: true,
+    children: 'Google link',
+    typo: 'regular',
+    iconToRender: 'new-window',
+    isTrailingIcon: false
+  }
+}`,...(h=(d=o.parameters)==null?void 0:d.docs)==null?void 0:h.source}}};var u,w,y;n.parameters={...n.parameters,docs:{...(u=n.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    ...{
+      href: 'https://www.google.com',
+      openInNewTab: true,
+      children: 'Google link',
+      typo: 'regular'
+    },
+    href: 'https://en.wikipedia.org/wiki/Lorem_ipsum',
+    children: 'here'
+  },
+  render: args => <Text tag="p" color="liquorice">
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry,
+      more details <Link {...args} />. Lorem Ipsum has been the industrys
+      standard dummy text ever since the 1500s.
+    </Text>
+}`,...(y=(w=n.parameters)==null?void 0:w.docs)==null?void 0:y.source}}};const q=["Playground","Highlighted","LeadingIcon","InParagraph"];export{r as Highlighted,n as InParagraph,o as LeadingIcon,e as Playground,q as __namedExportsOrder,v as default};
+//# sourceMappingURL=Link.stories-IxiadjpL.js.map
