7743442d36e3fa70c7b0454d22d0a51a08af245a
DeadEnglish <git@liamcanetti.co.uk>
2025-10-08 16:32:48 +0000

    Deploying to gh-pages from @ marshmallow-insurance/smores-react@a31dc32236412701e74ef2f2237aaa306bfbc3aa 🚀

---
+++ assets/Modal.stories-CBS09fha.js
@@ -0,0 +1,43 @@
+import{R as t,r as w}from"./index-DbOFzPHX.js";import{d as g}from"./styled-components.browser.esm-BbZQrDuP.js";import{B as s}from"./index-DskQjIgy.js";import"./index-qDtCLj2-.js";import{I as b}from"./index-CHrSHfjR.js";import{M as u}from"./Modal-ByIm1pcA.js";import{B as E}from"./Button-DyqqEkGw.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./space-BLajQSvN.js";import"./theme-D-5TnJVB.js";import"./index-DQTZIOkN.js";import"./index-BQeLGKNK.js";import"./index-DOTlaHaJ.js";import"./IconStrict-lSUh57Q2.js";import"./polished.esm-CYsXSD1d.js";import"./focusOutline-Cfflnwlf.js";import"./index-Bww6GOw6.js";import"./Text-K9LyMIiD.js";import"./variables-DcGMWv_V.js";import"./index-CNfLl1e4.js";import"./index-D3CETfXj.js";import"./Loader-DqW9abCT.js";const k=g(s)`
+  width: ${e=>e.width||"100%"};
+  height: ${e=>e.height};
+  transition: height 0.3s ease-in-out;
+`,f=e=>{const[a,i]=w.useState(!1),o=()=>{i(!a)};return t.createElement(t.Fragment,null,t.createElement(u,{...e,showModal:a,handleClick:o},e.children),t.createElement(E,{primary:!0,onClick:o},"Show modal"))},K={title:"Modal",component:u,argTypes:{rightPanel:{description:"Pass react nodes to display on the right side of the modal before the close button"}},decorators:[e=>t.createElement("div",{style:{padding:"20px"}},t.createElement(e,null))]},r={args:{title:"Generic modal",showModal:!1},render:e=>t.createElement(f,{...e},t.createElement(s,null,"[A modal window] creates a mode that disables the main window, but keeps it visible with the modal window as a child window in front of it."))},n={args:{icon:"calendar",title:"Hello world i'm a beautiful modal",showModal:!1,drawer:!0,cross:!0,closeOnOverlayClick:!1},render:e=>{const[a,i]=w.useState(!1),o=t.createElement(s,{onClick:()=>i(x=>!x)},t.createElement(b,{render:a?"collpase":"expand"}));return t.createElement(f,{rightPanel:o,width:a?"500px":"300px",...e},t.createElement(k,{height:a?"500px":"200px"},"[A modal window] creates a mode that disables the main window, but keeps it visible with the modal window as a child window in front of it."))}};var l,d,c;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    title: 'Generic modal',
+    showModal: false
+  },
+  render: args => {
+    return <Container {...args}>
+        <Box>
+          [A modal window] creates a mode that disables the main window, but
+          keeps it visible with the modal window as a child window in front of
+          it.
+        </Box>
+      </Container>;
+  }
+}`,...(c=(d=r.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};var m,p,h;n.parameters={...n.parameters,docs:{...(m=n.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    icon: 'calendar',
+    title: "Hello world i'm a beautiful modal",
+    showModal: false,
+    drawer: true,
+    cross: true,
+    closeOnOverlayClick: false
+  },
+  render: args => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [expanded, setExpanded] = useState(false);
+    const rightPanel = <Box onClick={() => setExpanded(current => !current)}>
+        <Icon render={expanded ? 'collpase' : 'expand'} />
+      </Box>;
+    return <Container rightPanel={rightPanel} width={expanded ? '500px' : '300px'} {...args}>
+        <StyledBox height={expanded ? '500px' : '200px'}>
+          [A modal window] creates a mode that disables the main window, but
+          keeps it visible with the modal window as a child window in front of
+          it.
+        </StyledBox>
+      </Container>;
+  }
+}`,...(h=(p=n.parameters)==null?void 0:p.docs)==null?void 0:h.source}}};const L=["Default","Interactive"];export{r as Default,n as Interactive,L as __namedExportsOrder,K as default};
+//# sourceMappingURL=Modal.stories-CBS09fha.js.map
