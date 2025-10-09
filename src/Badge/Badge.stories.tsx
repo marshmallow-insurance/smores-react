a3e45f092439adbe86af6c41899580d267b16a90
Liam Canetti <liam.canetti@marshmallow.co>
2025-10-09 09:44:23 +0100

    feat(badge): update to use theme provider [DES-44] (#4581)
    ## What does this do?
    - Updates the badge component to use the theme provided colours
    
    ## Screenshot / Video
    ### left - prod | right - dev
    
    
    https://github.com/user-attachments/assets/da1389dd-aa74-449b-8b66-ef340dfbe8fe
    
    
    ## Relevant tickets / Documentation
    N/A
    
    ## Testing
    - Visual

--- src/Badge/Badge.stories.tsx
+++ src/Badge/Badge.stories.tsx
@@ -1,12 +1,19 @@
 import { Meta, StoryObj } from '@storybook/react'
 import { Badge, BadgeSize } from './Badge'
 import { getExampleImg } from './storybook/exampleImg'
+import { colourOptions } from '../utils/storybookHelpers/colourOptions'
 
 const meta: Meta<typeof Badge> = {
   title: 'Badge',
   component: Badge,
   args: {
     title: 'badge',
+  },
+  argTypes: {
+    borderColour: {
+      control: 'select',
+      options: colourOptions,
+    },
   },
 }
 
