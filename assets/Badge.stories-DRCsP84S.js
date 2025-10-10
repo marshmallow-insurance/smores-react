d0c473f2fbe821dafcf1bcae56fd466dd6f09a66
DeadEnglish <git@liamcanetti.co.uk>
2025-10-10 09:30:06 +0000

    Deploying to gh-pages from @ marshmallow-insurance/smores-react@47dd5d7d421d8104fffff9b11932df51a54f1b26 🚀

---
+++ assets/Badge.stories-DRCsP84S.js
@@ -0,0 +1,15 @@
+import{B as c,g as m,a as l}from"./exampleImg-CRs0u966.js";import{c as d}from"./colourOptions-CbOJdSSM.js";import"./index-DbOFzPHX.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./styled-components.browser.esm-Cttt-Tea.js";import"./index-L5AcRSsk.js";import"./Text-DbgpC2ZR.js";import"./index-Cxnk3SxV.js";import"./space-BLajQSvN.js";import"./polished.esm-CYsXSD1d.js";import"./index-BWfv7pzw.js";import"./theme-D-5TnJVB.js";import"./focusOutline-tjk0UCOR.js";import"./colourMap-BqneUWMU.js";import"./variables-DcGMWv_V.js";const O={title:"Badge",component:c,args:{title:"badge"},argTypes:{borderColour:{control:"select",options:d}}},r={args:{borderColour:"lollipop",size:l.Lg,src:m().src}},o={args:{borderColour:"lollipop",size:l.Lg,src:m().src,disabled:!0}};var e,s,a;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    borderColour: 'lollipop',
+    size: BadgeSize.Lg,
+    src: getExampleImg().src
+  }
+}`,...(a=(s=r.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};var t,i,p;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    borderColour: 'lollipop',
+    size: BadgeSize.Lg,
+    src: getExampleImg().src,
+    disabled: true
+  }
+}`,...(p=(i=o.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};const P=["Primary","Disabled"];export{o as Disabled,r as Primary,P as __namedExportsOrder,O as default};
+//# sourceMappingURL=Badge.stories-DRCsP84S.js.map
---
+++ assets/Fieldset.stories-CmUkvZWS.js
@@ -0,0 +1,68 @@
+import{R as s}from"./index-DbOFzPHX.js";import{d as c}from"./styled-components.browser.esm-Cttt-Tea.js";import{B as W}from"./index-Cxnk3SxV.js";import{t as i}from"./theme-D-5TnJVB.js";import{F as E}from"./Fieldset-C601q9s9.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./space-BLajQSvN.js";import"./InternalField-DdBLNP7w.js";import"./index-BWfv7pzw.js";import"./index-L5AcRSsk.js";import"./Text-DbgpC2ZR.js";import"./polished.esm-CYsXSD1d.js";import"./focusOutline-tjk0UCOR.js";import"./colourMap-BqneUWMU.js";import"./variables-DcGMWv_V.js";function e(){var r="/home/runner/work/smores-react/smores-react/src/fields/components/Placeholder.tsx",F="ac3e34e7a14294c0ae98755810361265f3a87808",m=globalThis,d="__coverage__",G={path:"/home/runner/work/smores-react/smores-react/src/fields/components/Placeholder.tsx",statementMap:{0:{start:{line:5,column:27},end:{line:7,column:1}},1:{start:{line:6,column:2},end:{line:6,column:321}},2:{start:{line:8,column:16},end:{line:13,column:1}},3:{start:{line:14,column:12},end:{line:17,column:1}},4:{start:{line:18,column:13},end:{line:21,column:1}},5:{start:{line:22,column:0},end:{line:22,column:94}}},fnMap:{0:{name:"(anonymous_0)",decl:{start:{line:5,column:27},end:{line:5,column:28}},loc:{start:{line:5,column:33},end:{line:7,column:1}},line:5}},branchMap:{},s:{0:0,1:0,2:0,3:0,4:0,5:0},f:{0:0},b:{},inputSourceMap:{version:3,file:null,sources:["/home/runner/work/smores-react/smores-react/src/fields/components/Placeholder.tsx"],names:[],mappings:"AAAA,OAAO,WAAW;AAClB,OAAO,YAAY;AAEnB,SAAS,WAAW;AACpB,SAAS,aAAa;AAEf,aAAM,cAAc,MAAM;AAC/B,SACE,oCAAC,eACC,oCAAC,OAAI,OAAM,gCACT,oCAAC,QAAK,IAAG,KAAI,IAAG,KAAI,IAAG,QAAO,IAAG,QAAO,GACxC,oCAAC,QAAK,IAAG,QAAO,IAAG,KAAI,IAAG,KAAI,IAAG,QAAO,CAC1C,CACF;AAEJ;AAEA,MAAM,UAAU,OAAO,GAAG;AAAA,sBACJ,MAAM,OAAO,OAAO;AAAA,sBACpB,MAAM,OAAO,IAAI;AAAA;AAAA;AAAA;AAKvC,MAAM,MAAM,OAAO;AAAA;AAAA;AAAA;AAKnB,MAAM,OAAO,OAAO;AAAA;AAAA,YAER,MAAM,OAAO,IAAI;AAAA;;"},_coverageSchema:"1a1c01bbd47fc00a2c39e90264f33305004495a9",hash:"ac3e34e7a14294c0ae98755810361265f3a87808"},t=m[d]||(m[d]={});(!t[r]||t[r].hash!==F)&&(t[r]=G);var u=t[r];return e=function(){return u},u}e();e().s[0]++;const w=()=>(e().f[0]++,e().s[1]++,s.createElement(_,null,s.createElement(y,{xmlns:"http://www.w3.org/2000/svg"},s.createElement(p,{x1:"0",y1:"0",x2:"100%",y2:"100%"}),s.createElement(p,{x1:"100%",y1:"0",x2:"0",y2:"100%"})))),_=(e().s[2]++,c(W)`
+  background-color: ${i.colors.coconut};
+  border: 2px solid ${i.colors.chia};
+  height: 40px;
+  width: auto;
+`),y=(e().s[3]++,c.svg`
+  width: 100%;
+  height: 100%;
+`),p=(e().s[4]++,c.line`
+  stroke-width: 2px;
+  stroke: ${i.colors.chia};
+`);e().s[5]++;w.__docgenInfo={description:"",methods:[],displayName:"Placeholder"};const z={title:"Fieldset",component:E},l={render:r=>s.createElement(E,{...r},s.createElement(w,null))},a={...l,args:{label:"Fieldset label",assistiveText:"Some assistive text to help the user",renderAsTitle:!0,required:!1}},A={...l,args:{label:"Fieldset label",assistiveText:"Some assistive text to help the user",renderAsTitle:!0,required:!1,error:!0,errorMsg:"This is an error message describing what is wrong"},parameters:{a11y:{config:{rules:[{id:"color-contrast",enabled:!1}]}}}},o={...l,args:{label:"Fieldset label",assistiveText:"Some assistive text to help the user",renderAsTitle:!1,required:!1}},n={...l,args:{label:"Fieldset label",assistiveText:"Some assistive text to help the user",renderAsTitle:!0,required:!1,completed:!0},parameters:{a11y:{config:{rules:[{id:"color-contrast",enabled:!1}]}}}};var h,f,g;a.parameters={...a.parameters,docs:{...(h=a.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  ...Template,
+  args: {
+    label: 'Fieldset label',
+    assistiveText: 'Some assistive text to help the user',
+    renderAsTitle: true,
+    required: false
+  }
+}`,...(g=(f=a.parameters)==null?void 0:f.docs)==null?void 0:g.source}}};var v,b,x;A.parameters={...A.parameters,docs:{...(v=A.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  ...Template,
+  args: {
+    label: 'Fieldset label',
+    assistiveText: 'Some assistive text to help the user',
+    renderAsTitle: true,
+    required: false,
+    error: true,
+    errorMsg: 'This is an error message describing what is wrong'
+  },
+  parameters: {
+    a11y: {
+      config: {
+        rules: [{
+          id: 'color-contrast',
+          enabled: false
+        }]
+      }
+    }
+  }
+}`,...(x=(b=A.parameters)==null?void 0:b.docs)==null?void 0:x.source}}};var C,O,T;o.parameters={...o.parameters,docs:{...(C=o.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  ...Template,
+  args: {
+    label: 'Fieldset label',
+    assistiveText: 'Some assistive text to help the user',
+    renderAsTitle: false,
+    required: false
+  }
+}`,...(T=(O=o.parameters)==null?void 0:O.docs)==null?void 0:T.source}}};var M,S,I;n.parameters={...n.parameters,docs:{...(M=n.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  ...Template,
+  args: {
+    label: 'Fieldset label',
+    assistiveText: 'Some assistive text to help the user',
+    renderAsTitle: true,
+    required: false,
+    completed: true
+  },
+  parameters: {
+    a11y: {
+      config: {
+        rules: [{
+          id: 'color-contrast',
+          enabled: false
+        }]
+      }
+    }
+  }
+}`,...(I=(S=n.parameters)==null?void 0:S.docs)==null?void 0:I.source}}};const H=["Default","WithError","WithSmallLabel","WithCompletedStatus"];export{a as Default,n as WithCompletedStatus,A as WithError,o as WithSmallLabel,H as __namedExportsOrder,z as default};
+//# sourceMappingURL=Fieldset.stories-CmUkvZWS.js.map
