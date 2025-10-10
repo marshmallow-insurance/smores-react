b56508d0b507df2ad3f60517eb9bdafc1077297a
DeadEnglish <git@liamcanetti.co.uk>
2025-10-10 10:06:07 +0000

    Deploying to gh-pages from @ marshmallow-insurance/smores-react@c974dc8b5d786d3d0b60e001b62f4ce14a5f8b64 🚀

---
+++ assets/BadgeList.stories-2zKoXnR0.js
@@ -0,0 +1,118 @@
+import{R as l}from"./index-DbOFzPHX.js";import{a as o,B as b,g as K}from"./exampleImg-CGy7pkhj.js";import{d as G}from"./styled-components.browser.esm-Cttt-Tea.js";import"./index-DnrTJhCE.js";import"./index-DTb3F2ul.js";import{T as L}from"./Tooltip-zfUF1MZK.js";import{T as Y}from"./Text-COXihRIt.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./colourMap-BqneUWMU.js";import"./variables-DcGMWv_V.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-DQTZIOkN.js";import"./index-BQeLGKNK.js";import"./index-Cxnk3SxV.js";import"./space-BLajQSvN.js";import"./theme-D-5TnJVB.js";import"./polished.esm-CYsXSD1d.js";import"./index-EED6E-3c.js";import"./focusOutline-tjk0UCOR.js";function e(){var n="/home/runner/work/smores-react/smores-react/src/BadgeList/BadgeList.tsx",a="2c24f6abfd2c81d698d55ae7498a1f89cdff5fcd",r=globalThis,s="__coverage__",t={path:"/home/runner/work/smores-react/smores-react/src/BadgeList/BadgeList.tsx",statementMap:{0:{start:{line:7,column:25},end:{line:7,column:38}},1:{start:{line:10,column:4},end:{line:10,column:64}},2:{start:{line:12,column:20},end:{line:12,column:46}},3:{start:{line:13,column:26},end:{line:13,column:72}},4:{start:{line:14,column:2},end:{line:40,column:5}},5:{start:{line:14,column:161},end:{line:24,column:3}},6:{start:{line:43,column:2},end:{line:43,column:181}},7:{start:{line:45,column:29},end:{line:53,column:1}},8:{start:{line:51,column:37},end:{line:51,column:66}},9:{start:{line:54,column:20},end:{line:67,column:1}},10:{start:{line:55,column:2},end:{line:65,column:3}},11:{start:{line:56,column:4},end:{line:64,column:7}},12:{start:{line:66,column:2},end:{line:66,column:147}},13:{start:{line:68,column:27},end:{line:72,column:1}},14:{start:{line:73,column:18},end:{line:89,column:1}},15:{start:{line:80,column:31},end:{line:80,column:62}},16:{start:{line:90,column:0},end:{line:90,column:4192}}},fnMap:{0:{name:"BadgeList",decl:{start:{line:6,column:16},end:{line:6,column:25}},loc:{start:{line:6,column:66},end:{line:41,column:1}},line:6},1:{name:"(anonymous_1)",decl:{start:{line:14,column:127},end:{line:14,column:128}},loc:{start:{line:14,column:161},end:{line:24,column:3}},line:14},2:{name:"ExcessBadge",decl:{start:{line:42,column:9},end:{line:42,column:20}},loc:{start:{line:42,column:33},end:{line:44,column:1}},line:42},3:{name:"(anonymous_3)",decl:{start:{line:51,column:22},end:{line:51,column:23}},loc:{start:{line:51,column:37},end:{line:51,column:66}},line:51},4:{name:"(anonymous_4)",decl:{start:{line:54,column:20},end:{line:54,column:21}},loc:{start:{line:54,column:58},end:{line:67,column:1}},line:54},5:{name:"(anonymous_5)",decl:{start:{line:80,column:20},end:{line:80,column:21}},loc:{start:{line:80,column:31},end:{line:80,column:62}},line:80}},branchMap:{0:{loc:{start:{line:6,column:43},end:{line:6,column:62}},type:"default-arg",locations:[{start:{line:6,column:50},end:{line:6,column:62}}],line:6},1:{loc:{start:{line:10,column:4},end:{line:10,column:64}},type:"binary-expr",locations:[{start:{line:10,column:4},end:{line:10,column:9}},{start:{line:10,column:14},end:{line:10,column:63}}],line:10},2:{loc:{start:{line:10,column:14},end:{line:10,column:63}},type:"cond-expr",locations:[{start:{line:10,column:38},end:{line:10,column:59}},{start:{line:10,column:62},end:{line:10,column:63}}],line:10},3:{loc:{start:{line:12,column:20},end:{line:12,column:46}},type:"cond-expr",locations:[{start:{line:12,column:28},end:{line:12,column:37}},{start:{line:12,column:40},end:{line:12,column:46}}],line:12},4:{loc:{start:{line:13,column:26},end:{line:13,column:72}},type:"binary-expr",locations:[{start:{line:13,column:26},end:{line:13,column:48}},{start:{line:13,column:52},end:{line:13,column:72}}],line:13},5:{loc:{start:{line:14,column:89},end:{line:14,column:121}},type:"cond-expr",locations:[{start:{line:14,column:103},end:{line:14,column:112}},{start:{line:14,column:115},end:{line:14,column:121}}],line:14},6:{loc:{start:{line:17,column:11},end:{line:17,column:60}},type:"cond-expr",locations:[{start:{line:17,column:43},end:{line:17,column:52}},{start:{line:17,column:55},end:{line:17,column:60}}],line:17},7:{loc:{start:{line:24,column:6},end:{line:40,column:3}},type:"binary-expr",locations:[{start:{line:24,column:6},end:{line:24,column:21}},{start:{line:24,column:41},end:{line:40,column:3}}],line:24},8:{loc:{start:{line:55,column:2},end:{line:65,column:3}},type:"if",locations:[{start:{line:55,column:2},end:{line:65,column:3}},{start:{line:void 0,column:void 0},end:{line:void 0,column:void 0}}],line:55},9:{loc:{start:{line:59,column:18},end:{line:59,column:46}},type:"binary-expr",locations:[{start:{line:59,column:18},end:{line:59,column:34}},{start:{line:59,column:38},end:{line:59,column:46}}],line:59}},s:{0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0,15:0,16:0},f:{0:0,1:0,2:0,3:0,4:0,5:0},b:{0:[0],1:[0,0],2:[0,0],3:[0,0],4:[0,0],5:[0,0],6:[0,0],7:[0,0],8:[0,0],9:[0,0]},inputSourceMap:{version:3,file:null,sources:["/home/runner/work/smores-react/smores-react/src/BadgeList/BadgeList.tsx"],names:[],mappings:"AAAA,OAAO,WAAW;AAClB,SAAS,OAAO,iBAAkC;AAClD,OAAO,YAAY;AACnB,SAAS,eAAe;AAExB,SAAS,YAAY;AAoBd,gBAAS,UAAU,EAAE,QAAQ,OAAO,OAAO,UAAU,GAAG,GAAU;AACvE,QAAM,iBAAiB,OAAO;AAC9B,QAAM;AAAA;AAAA,IAEJ,UAAU,OAAO,SAAS,QAAQ,OAAO,SAAS,QAAQ;AAAA;AAE5D,QAAM,YAAY,QAAQ,QAAQ,IAAI;AACtC,QAAM,kBAAkB,gBAAgB,UAAa,QAAQ,WAAW;AAExE,SACE,oCAAC,aAAU,OAAO,QACf,OACE,MAAM,GAAG,cAAc,YAAY,MAAS,EAC5C,IAAI,CAAC,OAAO,UACX;AAAA,IAAC;AAAA;AAAA,MACC,KAAK,OAAO,MAAM,QAAQ,WAAW,MAAM,MAAM;AAAA,MACjD,OAAO;AAAA,QACL,GAAG;AAAA,QACH,QAAQ,iBAAiB;AAAA,QACzB;AAAA,MACF;AAAA;AAAA,EACF,CACD,GAEF,mBACC;AAAA,IAAC;AAAA;AAAA,MACC,WAAU;AAAA,MACV,OAAO,EAAE,QAAQ,gBAAgB,YAAY,MAAM;AAAA;AAAA,IAEnD;AAAA,MAAC;AAAA;AAAA,QACC,OAAO,IAAI,WAAW;AAAA,QACtB,cAAa;AAAA,QACb;AAAA,QACA,KAAK,oCAAC,eAAY,QAAQ,cAAc,GAAG;AAAA,QAC3C,UAAO;AAAA;AAAA,IACT;AAAA,EACF,CAEJ;AAEJ;AAMA,SAAS,YAAY,EAAE,OAAO,GAAqB;AACjD,SACE,oCAAC,4BACC,oCAAC,QAAK,MAAK,WAAU,OAAO,EAAE,YAAY,OAAO,KAAE,KAC/C,MACJ,CACF;AAEJ;AAEA,MAAM,uBAAuB,OAAO;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA,sBAMd,CAAC,EAAE,MAAM,MAAM,MAAM,MAAM,QAAQ,KAAK,GAAG,CAAC;AAAA;AAAA;AAQlE,MAAM,cAAc,CAAC,EAAE,OAAO,EAAE,SAAS,GAAG,MAAM,EAAE,MAAwB;AAC1E,MAAI,SAAS;AACX,WACE,oCAAC,SAAI,OAAO,EAAE,QAAQ,MAAM,OAAO,KACjC;AAAA,MAAC;AAAA;AAAA,QACC,UAAU,QAAQ,YAAY;AAAA,QAC9B,OAAO,SAAS;AAAA,QAChB,SAAS,QAAQ;AAAA;AAAA,MAEjB,oCAAC,SAAO,GAAG,OAAO;AAAA,IACpB,CACF;AAAA,EAEJ;AAEA,SACE,oCAAC,SAAI,OAAO,EAAE,QAAQ,MAAM,OAAO,KACjC,oCAAC,SAAO,GAAG,OAAO,CACpB;AAEJ;AAEA,MAAM,qBAAqB;AAAA,EACzB,CAAC,UAAU,EAAE,GAAG;AAAA,EAChB,CAAC,UAAU,EAAE,GAAG;AAAA,EAChB,CAAC,UAAU,EAAE,GAAG;AAClB;AAEA,MAAM,YAAY,OAAO;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA,oBAOL,CAAC,UAAU,mBAAmB,MAAM,KAAK,CAAC;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;;"},_coverageSchema:"1a1c01bbd47fc00a2c39e90264f33305004495a9",hash:"2c24f6abfd2c81d698d55ae7498a1f89cdff5fcd"},i=r[s]||(r[s]={});(!i[n]||i[n].hash!==a)&&(i[n]=t);var m=i[n];return e=function(){return m},m}e();function U({badges:n,limit:a,size:r=(e().b[0][0]++,o.Lg)}){e().f[0]++;const s=(e().s[0]++,n.length),t=(e().s[1]++,e().b[1][0]++,a&&(e().b[1][1]++,n.length>a?(e().b[2][0]++,n.length-a):(e().b[2][1]++,0))),i=(e().s[2]++,a?(e().b[3][0]++,a-1):(e().b[3][1]++,void 0)),m=(e().s[3]++,e().b[4][0]++,t!==void 0&&(e().b[4][1]++,!!t));return e().s[4]++,l.createElement(J,{$size:r},n.slice(0,t?(e().b[5][0]++,i):(e().b[5][1]++,void 0)).map((C,B)=>(e().f[1]++,e().s[5]++,l.createElement(_,{key:typeof C.src=="string"?(e().b[6][0]++,C.src):(e().b[6][1]++,B),badge:{...C,zIndex:s-B,size:r}}))),(e().b[7][0]++,m&&(e().b[7][1]++,l.createElement("div",{className:"limit-badge",style:{zIndex:s,marginLeft:"4px"}},l.createElement(b,{title:`+${t}`,borderColour:"oatmeal",size:r,src:l.createElement(W,{excess:t+1}),disabled:!0})))))}function W({excess:n}){return e().f[2]++,e().s[6]++,l.createElement(q,null,l.createElement(Y,{typo:"caption",style:{fontWeight:"bold"}},"+",n))}const q=(e().s[7]++,G.div`
+  display: flex;
+  align-items: center;
+  place-content: center;
+  width: 100%;
+  height: 100%;
+  background-color: ${({theme:n})=>(e().f[3]++,e().s[8]++,n.color.surface.base[400])};
+  pointer-events: none;
+`);e().s[9]++;const _=({badge:{tooltip:n,...a}})=>(e().f[4]++,e().s[10]++,n?(e().b[8][0]++,e().s[11]++,l.createElement("div",{style:{zIndex:a.zIndex}},l.createElement(L,{position:(e().b[9][0]++,n.position??(e().b[9][1]++,"bottom")),title:n==null?void 0:n.title,content:n.content},l.createElement(b,{...a})))):(e().b[8][1]++,e().s[12]++,l.createElement("div",{style:{zIndex:a.zIndex}},l.createElement(b,{...a})))),j=(e().s[13]++,{[o.Lg]:"-18px",[o.Md]:"-14px",[o.Sm]:"-11px"}),J=(e().s[14]++,G.div`
+  display: flex;
+
+  > * {
+    transition:
+      margin 0.2s ease-in-out,
+      padding 0.2s ease-in-out;
+    margin-right: ${n=>(e().f[5]++,e().s[15]++,j[n.$size])};
+
+    &:hover:not(:first-child):not(.limit-badge) {
+      padding-left: 10px;
+      &:not(:last-child) {
+        padding-right: 5px;
+      }
+    }
+  }
+`);e().s[16]++;U.__docgenInfo={description:`### A list of badges with optional tooltips
+
+@param limit - The maximum number of badges to display. If the number of badges exceeds the limit, a badge will be displayed indicating the number of hidden excess badges.`,methods:[],displayName:"BadgeList",props:{size:{required:!1,tsType:{name:"BadgeSize",raw:"BadgeProps['size']"},description:"",defaultValue:{value:"BadgeSize.Lg",computed:!0}},limit:{required:!1,tsType:{name:"number"},description:""},badges:{required:!0,tsType:{name:"Array",elements:[{name:"Omit",elements:[{name:"intersection",raw:"BadgeProps & { tooltip?: WithTooltip }",elements:[{name:"signature",type:"object",raw:`{
+  src: string | JSX.Element
+  title?: string
+  borderColour?: ColorTypes
+  size?: BadgeSize
+  disabled?: boolean
+  zIndex?: number
+}`,signature:{properties:[{key:"src",value:{name:"union",raw:"string | JSX.Element",elements:[{name:"string"},{name:"JSX.Element"}],required:!0}},{key:"title",value:{name:"string",required:!1}},{key:"borderColour",value:{name:"union",raw:"Color | NewColor",elements:[{name:"union",raw:"keyof typeof colors",elements:[{name:"literal",value:"lollipop"},{name:"literal",value:"marshmallowPink"},{name:"literal",value:"bubblegum"},{name:"literal",value:"fairyFloss"},{name:"literal",value:"boba"},{name:"literal",value:"liquorice"},{name:"literal",value:"sesame"},{name:"literal",value:"chia"},{name:"literal",value:"custard"},{name:"literal",value:"mascarpone"},{name:"literal",value:"coconut"},{name:"literal",value:"cream"},{name:"literal",value:"spearmint"},{name:"literal",value:"feijoa"},{name:"literal",value:"blueberry"},{name:"literal",value:"macaroon"},{name:"literal",value:"pistachio"},{name:"literal",value:"matcha"},{name:"literal",value:"caramel"},{name:"literal",value:"peanut"},{name:"literal",value:"marzipan"},{name:"literal",value:"oatmeal"},{name:"literal",value:"satsuma"},{name:"literal",value:"strawberry"},{name:"literal",value:"watermelon"},{name:"literal",value:"apple"},{name:"literal",value:"mint"},{name:"literal",value:"lemon"},{name:"literal",value:"sherbert"},{name:"literal",value:"tangerine"},{name:"literal",value:"compareTheMarket"},{name:"literal",value:"confused"},{name:"literal",value:"onfido"},{name:"literal",value:"x"},{name:"literal",value:"premfina"},{name:"literal",value:"checkout"},{name:"literal",value:"meta"},{name:"literal",value:"stripe"},{name:"literal",value:"intercom"},{name:"literal",value:"ravelin"},{name:"literal",value:"rac"},{name:"literal",value:"hometree"}]},{name:"intersection",raw:`{
+  [K in keyof T]: T[K]
+} & {}`,elements:[{name:"signature",type:"object",raw:`{
+  [K in keyof T]: T[K]
+}`,signature:{properties:[{key:{name:"unknown",required:!0},value:{name:"unknown[K]",raw:"T[K]"}}]}},{name:"signature",type:"object",raw:"{}",signature:{properties:[]}}]}],required:!1}},{key:"size",value:{name:"BadgeSize",required:!1}},{key:"disabled",value:{name:"boolean",required:!1}},{key:"zIndex",value:{name:"number",required:!1}}]}},{name:"signature",type:"object",raw:"{ tooltip?: WithTooltip }",signature:{properties:[{key:"tooltip",value:{name:"WithTooltip",required:!1}}]}}]},{name:"literal",value:"'zIndex'"}],raw:"Omit<BadgeListBadge, 'zIndex'>"}],raw:"Omit<BadgeListBadge, 'zIndex'>[]"},description:""}}};const se={title:"BadgeList",component:U},A=()=>{const{name:n,src:a}=K();return{src:a,title:n,tooltip:{title:"This is a tooltip",content:"This is the content of the tooltip"}}},c={args:{badges:[A(),{...A(),disabled:!0},{...A(),disabled:!0},{...A(),disabled:!0}]}},d={args:{size:o.Md,badges:[A(),A(),{...A(),disabled:!0},{...A(),disabled:!0},{...A(),disabled:!0}]}},u={args:{size:o.Sm,badges:[A(),A(),A(),{...A(),disabled:!0},{...A(),disabled:!0},{...A(),disabled:!0},{...A(),disabled:!0}]}},p={args:{limit:5,badges:[{...A(),src:""},{...A(),src:"example/404.jpg"},A(),A(),A(),A()]}},g={args:{limit:4,badges:[A(),{...A()},{...A()},{...A(),disabled:!0},{...A(),disabled:!0},{...A(),disabled:!0},{...A(),disabled:!0},{...A(),disabled:!0}]}};var E,v,f;c.parameters={...c.parameters,docs:{...(E=c.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    badges: [createTemplateBadge(), {
+      ...createTemplateBadge(),
+      disabled: true
+    }, {
+      ...createTemplateBadge(),
+      disabled: true
+    }, {
+      ...createTemplateBadge(),
+      disabled: true
+    }]
+  }
+}`,...(f=(v=c.parameters)==null?void 0:v.docs)==null?void 0:f.source}}};var M,O,y;d.parameters={...d.parameters,docs:{...(M=d.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  args: {
+    size: BadgeSize.Md,
+    badges: [createTemplateBadge(), createTemplateBadge(), {
+      ...createTemplateBadge(),
+      disabled: true
+    }, {
+      ...createTemplateBadge(),
+      disabled: true
+    }, {
+      ...createTemplateBadge(),
+      disabled: true
+    }]
+  }
+}`,...(y=(O=d.parameters)==null?void 0:O.docs)==null?void 0:y.source}}};var S,h,T;u.parameters={...u.parameters,docs:{...(S=u.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    size: BadgeSize.Sm,
+    badges: [createTemplateBadge(), createTemplateBadge(), createTemplateBadge(), {
+      ...createTemplateBadge(),
+      disabled: true
+    }, {
+      ...createTemplateBadge(),
+      disabled: true
+    }, {
+      ...createTemplateBadge(),
+      disabled: true
+    }, {
+      ...createTemplateBadge(),
+      disabled: true
+    }]
+  }
+}`,...(T=(h=u.parameters)==null?void 0:h.docs)==null?void 0:T.source}}};var x,Q,z;p.parameters={...p.parameters,docs:{...(x=p.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    limit: 5,
+    badges: [{
+      ...createTemplateBadge(),
+      src: ''
+    }, {
+      ...createTemplateBadge(),
+      src: 'example/404.jpg'
+    }, createTemplateBadge(), createTemplateBadge(), createTemplateBadge(), createTemplateBadge()]
+  }
+}`,...(z=(Q=p.parameters)==null?void 0:Q.docs)==null?void 0:z.source}}};var I,w,k;g.parameters={...g.parameters,docs:{...(I=g.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    limit: 4,
+    badges: [createTemplateBadge(), {
+      ...createTemplateBadge()
+    }, {
+      ...createTemplateBadge()
+    }, {
+      ...createTemplateBadge(),
+      disabled: true
+    }, {
+      ...createTemplateBadge(),
+      disabled: true
+    }, {
+      ...createTemplateBadge(),
+      disabled: true
+    }, {
+      ...createTemplateBadge(),
+      disabled: true
+    }, {
+      ...createTemplateBadge(),
+      disabled: true
+    }]
+  }
+}`,...(k=(w=g.parameters)==null?void 0:w.docs)==null?void 0:k.source}}};const me=["Primary","MediumSize","SmallSize","BadgeFallback","BadgeLimit"];export{p as BadgeFallback,g as BadgeLimit,d as MediumSize,c as Primary,u as SmallSize,me as __namedExportsOrder,se as default};
+//# sourceMappingURL=BadgeList.stories-2zKoXnR0.js.map
---
+++ assets/Text.stories-P6dV62Xs.js
@@ -0,0 +1,27 @@
+import{R as e}from"./index-DbOFzPHX.js";import{d as w}from"./styled-components.browser.esm-Cttt-Tea.js";import{B as E}from"./index-Cxnk3SxV.js";import{T as t,f as v}from"./Text-COXihRIt.js";import{c as k}from"./colourOptions-CbOJdSSM.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./space-BLajQSvN.js";import"./polished.esm-CYsXSD1d.js";import"./index-EED6E-3c.js";import"./colourMap-BqneUWMU.js";import"./variables-DcGMWv_V.js";import"./theme-D-5TnJVB.js";import"./focusOutline-tjk0UCOR.js";const c=w(E)`
+  display: grid;
+  grid-template-columns: 0.4fr 1.2fr 1fr;
+  margin-bottom: 24px;
+  gap: 10px;
+`,j=({typos:r})=>e.createElement(E,null,e.createElement(c,null,e.createElement(t,{tag:"p",typo:"body-regular"},"Name"),e.createElement(t,{tag:"p",typo:"body-regular"},"Single Line"),e.createElement(t,{tag:"p",typo:"body-regular"},"Paragraph")),r.map(s=>e.createElement(c,{key:s},e.createElement(t,{tag:"p",typo:"body-regular",color:"color.text.subtle"},s),e.createElement(t,{tag:"p",typo:"body-regular",color:"color.text.base"},"They waited patiently for what seemed a very long time."),!["hero-alternate","hero","heading-alternate","label"].includes(s)&&e.createElement(t,{tag:"p",typo:"body-regular",color:"color.text.base"},"They waited patiently for what seemed a very long time. They waited patiently for what seemed a very long time.")))),P={title:"Text",component:t,argTypes:{color:{control:"select",options:k}},decorators:[r=>e.createElement("div",{style:{margin:"64px"}},e.createElement(r,null))]},q=Object.keys(v),o={args:{tag:"p"},render:r=>e.createElement(t,{...r},"The quick brown fox jumps over the lazy dog")},a={args:{tag:"label",typo:"label",color:"color.text.base"},render:r=>e.createElement(t,{...r},"The quick brown fox jumps over the lazy dog")},l={args:{tag:"p",title:"a11y title tooltip"},render:r=>e.createElement(t,{...r},"The quick brown fox jumps over the lazy dog")},n={render:()=>e.createElement(j,{typos:q})};var p,m,i;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    tag: 'p'
+  },
+  render: args => <Text {...args}>The quick brown fox jumps over the lazy dog</Text>
+}`,...(i=(m=o.parameters)==null?void 0:m.docs)==null?void 0:i.source}}};var g,d,u;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    tag: 'label',
+    typo: 'label',
+    color: 'color.text.base'
+  },
+  render: args => <Text {...args}>The quick brown fox jumps over the lazy dog</Text>
+}`,...(u=(d=a.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};var y,b,x;l.parameters={...l.parameters,docs:{...(y=l.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    tag: 'p',
+    title: 'a11y title tooltip'
+  },
+  render: args => <Text {...args}>The quick brown fox jumps over the lazy dog</Text>
+}`,...(x=(b=l.parameters)==null?void 0:b.docs)==null?void 0:x.source}}};var h,T,f;n.parameters={...n.parameters,docs:{...(h=n.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: () => <TypoCollection typos={typos} />
+}`,...(f=(T=n.parameters)==null?void 0:T.docs)==null?void 0:f.source}}};const A=["Default","Label","WithTitle","Collection"];export{n as Collection,o as Default,a as Label,l as WithTitle,A as __namedExportsOrder,P as default};
+//# sourceMappingURL=Text.stories-P6dV62Xs.js.map
