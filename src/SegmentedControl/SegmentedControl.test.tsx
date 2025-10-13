6e498be02503e401c8caf4da122b10f1f310a006
Ella Taylor <ella.taylor@marshmallow.co>
2025-10-13 15:05:50 +0100

    feat(segmented-control): update to new theme colours [DES-73] (#4584)
    ## What does this do?
    <!-- 
    - A short description of what the PR changes and why it's necessary
    - Give the reviewers context
    - Is the impact global or localised?
    - Any tech debt created or discovered?
    -->
    
    Updates the `SegmentedControl` component to use the new theme colours
    
    ## Screenshot / Video
    <!--
    - applicable for UI changes, can be skipped if not relevant. You can
    drag and drop images/videos to upload them to GitHub.
    - Example with image with fixed width 
    - <img
    src="https://github.com/marshmallow-insurance/uk-auto-signup-www/assets/57456071/3dbf1ec1-2363-4e20-8236-b1f1581b0953"
    width="300px" />
    shortcuts:
     - Screenshot: cmd + shift + 4
     - Screen recording: cmd + shift + 5
    -->
    
    **(Left is local / Right is Prod)**
    
    
    
    https://github.com/user-attachments/assets/0edc0a6c-e8a0-4358-a033-abe173881aa6
    
    
    ## Relevant tickets / Documentation
    <!--
    - Link to any relevant issue tracking software (jira, GitHub etc),
    documentation (PRDs, engineering plans, Figma designs) or slack threads
    - If applicable, list any new documentation that has been
    created/updated e.g. diagrams/flows etc
    -->
    N/A
    
    ## Testing
    <!--
    - Tests should have been updated/created to cover the changes made in
    this PR, if not please explain why
    -->
    ✅ Snapshots updated
    ✅ Unit tests added

--- src/SegmentedControl/SegmentedControl.test.tsx
+++ src/SegmentedControl/SegmentedControl.test.tsx
@@ -19,4 +19,103 @@ describe('SegmentedControl', () => {
     )
     expect(container).toMatchSnapshot()
   })
+
+  it('renders default style correctly when no styles properties are passed', () => {
+    const { container, getByText } = render(
+      <SegmentedControl
+        options={[
+          { label: 'Option 1', value: 'option1' },
+          { label: 'Option 2', value: 'option2' },
+        ]}
+        value="option1"
+        onChange={noop}
+        showTag={true}
+      />,
+    )
+    // Selected
+    expect(getByText('Option 1')).toHaveStyle({
+      color: '#ffffff', // color.surface.base.000
+    })
+    expect(container.querySelector('[togglecolor]')).toHaveStyle({
+      'background-color': '#292924', // color.text.base
+    })
+
+    // Unselected
+    expect(getByText('Option 2')).toHaveStyle({
+      color: '#292924', // color.text.base
+    })
+    expect(container.firstChild).toHaveStyle({
+      'background-color': '#F1E9DC', // color.surface.base.300
+    })
+  })
+
+  it('renders correctly with custom styles', () => {
+    const { container, getByText } = render(
+      <SegmentedControl
+        options={[
+          { label: 'Option 1', value: 'option1' },
+          { label: 'Option 2', value: 'option2' },
+        ]}
+        value="option1"
+        onChange={noop}
+        showTag={true}
+        styles={{
+          selectedText: 'color.text.base',
+          text: 'color.text.base',
+          background: 'color.surface.brand.300',
+          toggle: 'color.surface.base.000',
+        }}
+      />,
+    )
+    // Selected
+    expect(getByText('Option 1')).toHaveStyle({
+      color: '#292924', // color.text.base
+    })
+    expect(container.querySelector('[togglecolor]')).toHaveStyle({
+      'background-color': '#FFFFFF', // color.surface.base.000
+    })
+
+    // Unselected
+    expect(getByText('Option 2')).toHaveStyle({
+      color: '#292924', // color.text.base
+    })
+    expect(container.firstChild).toHaveStyle({
+      'background-color': '#ff88c8', // color.surface.brand.300
+    })
+  })
+
+  it('renders correctly with custom styles and old colour names', () => {
+    const { container, getByText } = render(
+      <SegmentedControl
+        options={[
+          { label: 'Option 1', value: 'option1' },
+          { label: 'Option 2', value: 'option2' },
+        ]}
+        value="option1"
+        onChange={noop}
+        showTag={true}
+        styles={{
+          selectedText: 'liquorice',
+          text: 'liquorice',
+          background: 'marshmallowPink',
+          toggle: 'cream',
+        }}
+      />,
+    )
+    // Selected
+    expect(getByText('Option 1')).toHaveStyle({
+      color: '#292924', // liquorice
+    })
+    expect(container.querySelector('[togglecolor]')).toHaveStyle({
+      'background-color': '#FFFFFF', // cream
+    })
+
+    // Unselected
+    expect(getByText('Option 2')).toHaveStyle({
+      color: '#292924', // liquorice
+    })
+    expect(container.firstChild).toHaveStyle({
+      'background-color': '#ff88c8', // marshmallowPink
+    })
+  })
 })
