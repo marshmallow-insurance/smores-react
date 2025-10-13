3bdfb8e14e766c1371a6853f3323533bf4cc5f07
DeadEnglish <git@liamcanetti.co.uk>
2025-10-13 13:59:03 +0000

    Deploying to gh-pages from @ marshmallow-insurance/smores-react@fe3605f63a098958d4b3674a75f81a9e0733ce87 🚀

---
+++ assets/Loader.stories-DEAu4I6E.js
@@ -0,0 +1,11 @@
+import{R as o}from"./index-DbOFzPHX.js";import{L as p}from"./Loader-CryEej5M.js";import{c as l}from"./colourOptions-CbOJdSSM.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./styled-components.browser.esm-Cttt-Tea.js";import"./colourMap-BqneUWMU.js";import"./variables-DcGMWv_V.js";const E={title:"Loader",component:p,decorators:[i=>o.createElement("div",{style:{margin:"64px"}},o.createElement(i,null))],argTypes:{color:{control:"select",options:l}}},r={args:{height:"32"}},e={args:{height:"54",color:"color.text.base"}};var t,a,s;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    height: '32'
+  }
+}`,...(s=(a=r.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};var c,n,m;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    height: '54',
+    color: 'color.text.base'
+  }
+}`,...(m=(n=e.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};const b=["Default","BigLoader"];export{e as BigLoader,r as Default,b as __namedExportsOrder,E as default};
+//# sourceMappingURL=Loader.stories-DEAu4I6E.js.map
