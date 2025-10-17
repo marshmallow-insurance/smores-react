f834827c313a6aa89d036e05a152a5dcc4a28211
ellajay <ella.taylor@marshmallow.co>
2025-10-17 14:52:30 +0000

    Deploying to gh-pages from @ marshmallow-insurance/smores-react@6d0c72c74809c1ec7a7bbe5e85a13fd60fba3495 🚀

---
+++ assets/CheckBox-D4hUtrKm.js
@@ -0,0 +1,89 @@
+import{r as d,R as o}from"./index-DbOFzPHX.js";import{d as s}from"./styled-components.browser.esm-Cttt-Tea.js";import{f as C}from"./focusOutline-tjk0UCOR.js";import"./index-C2AVfQPl.js";import{u as p}from"./id-Cm6Ij0Ql.js";import{T as M}from"./Text-C-7Pf0TR.js";function A(){var e="/home/runner/work/smores-react/smores-react/src/CheckBox/CheckBox.tsx",l="c473ac2f43630d0123bd2ff127273c93d3881a1c",c=globalThis,a="__coverage__",i={path:"/home/runner/work/smores-react/smores-react/src/CheckBox/CheckBox.tsx",statementMap:{0:{start:{line:6,column:24},end:{line:27,column:1}},1:{start:{line:8,column:15},end:{line:8,column:34}},2:{start:{line:9,column:4},end:{line:25,column:159}},3:{start:{line:28,column:18},end:{line:66,column:1}},4:{start:{line:33,column:35},end:{line:33,column:138}},5:{start:{line:46,column:39},end:{line:46,column:70}},6:{start:{line:61,column:39},end:{line:61,column:70}},7:{start:{line:67,column:21},end:{line:112,column:1}},8:{start:{line:80,column:39},end:{line:80,column:70}},9:{start:{line:83,column:41},end:{line:83,column:62}},10:{start:{line:84,column:41},end:{line:84,column:62}},11:{start:{line:100,column:41},end:{line:100,column:79}},12:{start:{line:113,column:17},end:{line:117,column:1}},13:{start:{line:116,column:26},end:{line:116,column:60}},14:{start:{line:118,column:0},end:{line:118,column:715}}},fnMap:{0:{name:"CheckBox2",decl:{start:{line:7,column:11},end:{line:7,column:20}},loc:{start:{line:7,column:86},end:{line:26,column:3}},line:7},1:{name:"(anonymous_1)",decl:{start:{line:33,column:12},end:{line:33,column:13}},loc:{start:{line:33,column:35},end:{line:33,column:138}},line:33},2:{name:"(anonymous_2)",decl:{start:{line:46,column:24},end:{line:46,column:25}},loc:{start:{line:46,column:39},end:{line:46,column:70}},line:46},3:{name:"(anonymous_3)",decl:{start:{line:61,column:24},end:{line:61,column:25}},loc:{start:{line:61,column:39},end:{line:61,column:70}},line:61},4:{name:"(anonymous_4)",decl:{start:{line:80,column:24},end:{line:80,column:25}},loc:{start:{line:80,column:39},end:{line:80,column:70}},line:80},5:{name:"(anonymous_5)",decl:{start:{line:83,column:26},end:{line:83,column:27}},loc:{start:{line:83,column:41},end:{line:83,column:62}},line:83},6:{name:"(anonymous_6)",decl:{start:{line:84,column:26},end:{line:84,column:27}},loc:{start:{line:84,column:41},end:{line:84,column:62}},line:84},7:{name:"(anonymous_7)",decl:{start:{line:100,column:26},end:{line:100,column:27}},loc:{start:{line:100,column:41},end:{line:100,column:79}},line:100},8:{name:"(anonymous_8)",decl:{start:{line:116,column:11},end:{line:116,column:12}},loc:{start:{line:116,column:26},end:{line:116,column:60}},line:116}},branchMap:{0:{loc:{start:{line:14,column:15},end:{line:14,column:49}},type:"cond-expr",locations:[{start:{line:14,column:23},end:{line:14,column:35}},{start:{line:14,column:38},end:{line:14,column:49}}],line:14},1:{loc:{start:{line:25,column:75},end:{line:25,column:157}},type:"binary-expr",locations:[{start:{line:25,column:75},end:{line:25,column:80}},{start:{line:25,column:84},end:{line:25,column:92}},{start:{line:25,column:112},end:{line:25,column:157}}],line:25},2:{loc:{start:{line:33,column:35},end:{line:33,column:138}},type:"cond-expr",locations:[{start:{line:33,column:44},end:{line:33,column:93}},{start:{line:33,column:96},end:{line:33,column:138}}],line:33}},s:{0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0},f:{0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0},b:{0:[0,0],1:[0,0,0],2:[0,0]},inputSourceMap:{version:3,file:null,sources:["/home/runner/work/smores-react/smores-react/src/CheckBox/CheckBox.tsx"],names:["CheckBox"],mappings:"AAAA,OAAO,SAAS,kBAA6B;AAC7C,OAAO,YAAY;AACnB,SAAS,oBAAoB;AAE7B,SAAS,YAAY;AACrB,SAAS,mBAAmB;AAWrB,aAAM,WAAW;AAAA,EACtB,SAASA,UACP,EAAE,IAAI,QAAQ,SAAS,UAAU,QAAQ,OAAO,SAAS,GACzD,KACA;AACA,UAAM,KAAK,YAAY,MAAM;AAC7B,WACE,0DACE,oCAAC,gBAAa,MACZ;AAAA,MAAC;AAAA;AAAA,QACC,KAAI;AAAA,QACJ,MAAK;AAAA,QACL,OAAO,QAAQ,eAAe;AAAA;AAAA,MAE7B;AAAA,IACH,GAEA;AAAA,MAAC;AAAA;AAAA,QACC;AAAA,QACA,MAAK;AAAA,QACL;AAAA,QACA,UAAU;AAAA;AAAA,IACZ,GACA,oCAAC,aAAU,QAAQ,OAAO,CAC5B,GACC,SAAS,YAAY,oCAAC,gBAAU,QAAS,CAC5C;AAAA,EAEJ;AACF;AAEA,MAAM,YAAY,OAAO;AAAA;AAAA;AAAA;AAAA;AAAA,YAKb,CAAC,EAAE,QAAQ,MAAM,MACzB,SACI,aAAa,MAAM,MAAM,SAAS,SAAS,GAAG,CAAC,KAC/C,aAAa,MAAM,MAAM,OAAO,QAAQ,EAAE;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA,wBAa1B,CAAC,EAAE,MAAM,MAAM,MAAM,MAAM,QAAQ,KAAK,KAAK,CAAC;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA,wBAe9C,CAAC,EAAE,MAAM,MAAM,MAAM,MAAM,QAAQ,KAAK,KAAK,CAAC;AAAA;AAAA;AAAA;AAAA;AAAA;AAOtE,MAAM,eAAe,OAAO;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA,wBAaJ,CAAC,EAAE,MAAM,MAAM,MAAM,MAAM,QAAQ,KAAK,KAAK,CAAC;AAAA;AAAA,kBAEpD,SAAS;AAAA,0BACD,CAAC,EAAE,MAAM,MAAM,MAAM,MAAM,KAAK,IAAI;AAAA,0BACpC,CAAC,EAAE,MAAM,MAAM,MAAM,MAAM,KAAK,IAAI;AAAA;AAAA;AAAA,kBAG5C,SAAS;AAAA;AAAA;AAAA;AAAA,kBAIT,SAAS;AAAA;AAAA;AAAA;AAAA,MAIrB,aAAa,EAAE,UAAU,qBAAqB,SAAS,GAAG,CAAC,CAAC;AAAA;AAAA;AAAA;AAAA,MAI5D,SAAS;AAAA,0BACW,CAAC,EAAE,MAAM,MAC3B,MAAM,MAAM,YAAY,SAAS,KAAK;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAc9C,MAAM,WAAW,OAAO;AAAA;AAAA;AAAA,WAGb,CAAC,EAAE,MAAM,MAAM,MAAM,MAAM,SAAS,SAAS,GAAG,CAAC;AAAA;;"},_coverageSchema:"1a1c01bbd47fc00a2c39e90264f33305004495a9",hash:"c473ac2f43630d0123bd2ff127273c93d3881a1c"},n=c[a]||(c[a]={});(!n[e]||n[e].hash!==l)&&(n[e]=i);var t=n[e];return A=function(){return t},t}A();const f=(A().s[0]++,d.forwardRef(function({id:l,checked:c,children:a,toggle:i,error:n,errorMsg:t},u){A().f[0]++;const m=(A().s[1]++,p(l));return A().s[2]++,o.createElement(o.Fragment,null,o.createElement(b,{id:m},o.createElement(M,{tag:"span",typo:"body-regular",color:n?(A().b[0][0]++,"strawberry"):(A().b[0][1]++,"liquorice")},a),o.createElement("input",{ref:u,type:"checkbox",checked:c,onChange:i}),o.createElement(r,{$error:n})),(A().b[1][0]++,n&&(A().b[1][1]++,t)&&(A().b[1][2]++,o.createElement(x,null,t))))})),r=(A().s[3]++,s.span`
+  position: absolute;
+  left: 0;
+  width: 24px;
+  height: 24px;
+  border: ${({$error:e,theme:l})=>(A().f[1]++,A().s[4]++,e?(A().b[2][0]++,`solid 2px ${l.color.feedback.negative[200]}`):(A().b[2][1]++,`solid 2px ${l.color.border.contrast}`))};
+  box-sizing: border-box;
+  border-radius: 1px;
+
+  &:before {
+    content: '';
+    position: absolute;
+    display: none;
+    top: 9px;
+    left: 5px;
+    width: 3px;
+    height: 8px;
+    border-radius: 4px;
+    background-color: ${({theme:e})=>(A().f[2]++,A().s[5]++,e.color.surface.base["000"])};
+    -webkit-transform: rotate(316deg);
+    -ms-transform: rotate(316deg);
+    transform: rotate(316deg);
+  }
+
+  &:after {
+    content: '';
+    position: absolute;
+    display: none;
+    top: 3px;
+    left: 11px;
+    width: 3px;
+    height: 15px;
+    border-radius: 4px;
+    background-color: ${({theme:e})=>(A().f[3]++,A().s[6]++,e.color.surface.base["000"])};
+    -webkit-transform: rotate(43deg);
+    -ms-transform: rotate(43deg);
+    transform: rotate(43deg);
+  }
+`),b=(A().s[7]++,s.label`
+  position: relative;
+  display: flex;
+  align-items: flex-start;
+  padding-left: 32px;
+  user-select: none;
+  cursor: pointer;
+  min-height: 24px;
+
+  input {
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+    background-color: ${({theme:e})=>(A().f[4]++,A().s[8]++,e.color.surface.base["000"])};
+
+    &:checked ~ ${r} {
+      background-color: ${({theme:e})=>(A().f[5]++,A().s[9]++,e.color.icon.base)};
+      border: solid 2px ${({theme:e})=>(A().f[6]++,A().s[10]++,e.color.icon.base)};
+    }
+
+    &:checked ~ ${r}:before {
+      display: block;
+    }
+
+    &:checked ~ ${r}:after {
+      display: block;
+    }
+
+    ${C({selector:`&:focus-visible ~ ${r}`})}
+  }
+
+  &:hover {
+    ${r} {
+      background-color: ${({theme:e})=>(A().f[7]++,A().s[11]++,e.color.interactive.tertiary.hover)};
+    }
+  }
+
+  @media (min-width: 768px) {
+    padding-left: 32px;
+  }
+
+  span {
+    border-radius: 6px;
+    line-height: 24px;
+  }
+`),x=(A().s[12]++,s.div`
+  margin-top: 4px;
+  font-size: 12px;
+  color: ${({theme:e})=>(A().f[8]++,A().s[13]++,e.color.feedback.negative[200])};
+`);A().s[14]++;f.__docgenInfo={description:"",methods:[],displayName:"CheckBox",props:{id:{required:!1,tsType:{name:"string"},description:""},checked:{required:!0,tsType:{name:"boolean"},description:""},children:{required:!0,tsType:{name:"ReactNode"},description:""},toggle:{required:!0,tsType:{name:"signature",type:"function",raw:"() => void",signature:{arguments:[],return:{name:"void"}}},description:""},error:{required:!1,tsType:{name:"boolean"},description:""},errorMsg:{required:!1,tsType:{name:"string"},description:""}}};export{f as C};
+//# sourceMappingURL=CheckBox-D4hUtrKm.js.map
---
+++ assets/Tooltip.stories-CngXa4nX.js
@@ -0,0 +1,146 @@
+import{R as e,r as L}from"./index-DbOFzPHX.js";import{d as u}from"./styled-components.browser.esm-Cttt-Tea.js";import{B as r}from"./index-Cxnk3SxV.js";import"./index-ZqqtKKMC.js";import"./index-QyfclVjn.js";import"./index-C2AVfQPl.js";import{T as n}from"./Tooltip-Ckl03x_t.js";import{M as O}from"./Modal-X1xGAZBm.js";import{B as F}from"./Button-BUpUKb93.js";import{T as t}from"./Text-C-7Pf0TR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./space-BLajQSvN.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-DQTZIOkN.js";import"./index-BQeLGKNK.js";import"./colourMap-BqneUWMU.js";import"./variables-DcGMWv_V.js";import"./index-TLZeLsFC.js";import"./IconStrict-DGiZIh5p.js";import"./polished.esm-CYsXSD1d.js";import"./index-EED6E-3c.js";import"./focusOutline-tjk0UCOR.js";import"./index-CNfLl1e4.js";import"./index-B1qSEQfA.js";import"./Loader-CryEej5M.js";import"./theme-D-5TnJVB.js";const xe={title:"Tooltip",component:n,args:{title:"Harry Hill",content:"Tooltip content, this is some text, maybe even more text if you feel like it.",underline:!0},argTypes:{variant:{options:["primary","fallback","bubblegum"],control:{type:"select",options:["primary","fallback","bubblegum"]}}},render:o=>e.createElement(r,{px:{custom:250},py:{custom:150},flex:!0},e.createElement(n,{...o},e.createElement(r,null,"Harry Hill")))},P=e.createElement(r,null,e.createElement(t,{typo:"body-regular",color:"liquorice"},"20 Cherry Blossom Lane,"),e.createElement(t,{typo:"body-regular",color:"liquorice"},"Aintree,"),e.createElement(t,{typo:"body-regular",color:"liquorice"},"Manchester,"),e.createElement(t,{typo:"body-regular",color:"liquorice"},"T: 07123456789")),l={args:{title:"Long text example",children:e.createElement(t,{typo:"body-regular",color:"liquorice"},"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),underline:!0}},i={args:{children:e.createElement(r,null,e.createElement(t,{typo:"body-regular",color:"liquorice"},"20 Cherry Blossom Lane,"),e.createElement(t,{typo:"body-regular",color:"liquorice"},"Aintree,"),e.createElement(t,{typo:"body-regular",color:"liquorice"},"Manchester,"),e.createElement(t,{typo:"body-regular",color:"liquorice"},"T: 07123456789")),underline:!0}},a={render:o=>e.createElement(N,{tabIndex:0},e.createElement(r,{flex:!0,direction:"column",gap:"32px",justifyContent:"center",alignItems:"center"},e.createElement(n,{...o,position:"left"},e.createElement(t,null,"Harry Hill (left)")),e.createElement(n,{...o,position:"right"},e.createElement(t,null,"Harry Hill (right)")),e.createElement(n,{...o,position:"bottom"},e.createElement(t,null,"Harry Hill (bottom)")),e.createElement(n,{...o,position:"top"},e.createElement(t,null,"Harry Hill (top)")))),args:{children:P,underline:!0},parameters:{layout:"fullscreen"}},N=u(r)`
+  padding: 200px 0;
+  height: 150vh;
+  width: 200%;
+  overflow: scroll;
+  background: ${({theme:o})=>o.color.surface.base[100]};
+`,s={args:{children:P,underline:!0},render:o=>e.createElement(D,{...o})},D=o=>{const[d,C]=L.useState(!1),x=()=>{C(!d)};return e.createElement(r,{height:"900px"},e.createElement(O,{handleClick:x,showModal:d},e.createElement(n,{...o},e.createElement(r,null,"Harry Hill"))),e.createElement(F,{primary:!0,handleClick:x},"Show Modal with Mobile Drawer"))},c={render:o=>e.createElement(r,{width:"200px"},e.createElement(t,null,"Marshmallow car insurance, the best insurance in town."," ",e.createElement(n,{...o},"Hover here")," to learn more as the Policy Holder."),e.createElement(t,{my:"12px"},"Nunc cursus fermentum libero vel iaculis. Praesent vel dignissim purus, a iaculis mauris."," ",e.createElement(n,{...o,position:"right"},"In tempor nibh ut elit sollicitudin, vel ultricies elit volutpat. Etiam accumsan tristique ante, at mollis neque laoreet eget.")," ","Duis rhoncus molestie felis a cursus.")),args:{inline:!0}},p={render:()=>e.createElement(W,null,e.createElement(r,null,e.createElement(m,null,e.createElement(n,{variant:"fallback",underline:!0,content:e.createElement(T,{typo:"caption",color:"color.surface.base.000"},"An adjustment changes the ",e.createElement("b",null,"cost")," of the policy. This field should ",e.createElement("b",null,"not")," be used to adjust for invoices created after a failed up front payment.",e.createElement("br",null),e.createElement("br",null),"For annual and Premfina policies, use the `MTA only` transaction option when issuing an invoice.",e.createElement("br",null),e.createElement("br",null),"For instalment policies, use `spread entire amount` transaction option when issuing an invoice."),position:"top"},e.createElement(t,{typo:"body-regular"},"Adjustment amount"),e.createElement(t,{typo:"caption"},"(Adjusts the policy cost)")))),e.createElement(m,null,e.createElement(t,{typo:"body-regular"},"MTA admin fee")),e.createElement(m,null,e.createElement(n,{underline:!0,variant:"fallback",content:e.createElement(T,{typo:"caption",color:"color.surface.base.000"},"This is the policies total new retained refund after this MTA is complete."),position:"top"},e.createElement(t,null,"Predicted retained refund")),e.createElement(t,null,"£1,345")))},W=u(r)`
+  flex-direction: column;
+  gap: 16px;
+  position: relative;
+  width: 100%;
+  padding: 24px;
+  border-radius: 16px;
+  background: ${({theme:o})=>o.color.surface.base[300]};
+  display: flex;
+
+  @media (max-width: 1000px) {
+    width: 100%;
+    flex-direction: column;
+  }
+`,T=u(t)`
+  white-space: normal;
+  width: 200px;
+`,m=u(r)`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  margin: 8px 0px;
+`;var y,h,g;l.parameters={...l.parameters,docs:{...(y=l.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    title: 'Long text example',
+    children: <Text typo="body-regular" color="liquorice">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </Text>,
+    underline: true
+  }
+}`,...(g=(h=l.parameters)==null?void 0:h.docs)==null?void 0:g.source}}};var b,f,E;i.parameters={...i.parameters,docs:{...(b=i.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    children: <Box>
+        <Text typo="body-regular" color="liquorice">
+          20 Cherry Blossom Lane,
+        </Text>
+        <Text typo="body-regular" color="liquorice">
+          Aintree,
+        </Text>
+        <Text typo="body-regular" color="liquorice">
+          Manchester,
+        </Text>
+        <Text typo="body-regular" color="liquorice">
+          T: 07123456789
+        </Text>
+      </Box>,
+    underline: true
+  }
+}`,...(E=(f=i.parameters)==null?void 0:f.docs)==null?void 0:E.source}}};var w,v,H;a.parameters={...a.parameters,docs:{...(w=a.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  render: (props: TooltipProps) => <OverflowHiddenBox tabIndex={0}>
+      <Box flex direction="column" gap="32px" justifyContent="center" alignItems="center">
+        <Tooltip {...props} position="left">
+          <Text>Harry Hill (left)</Text>
+        </Tooltip>
+
+        <Tooltip {...props} position="right">
+          <Text>Harry Hill (right)</Text>
+        </Tooltip>
+
+        <Tooltip {...props} position="bottom">
+          <Text>Harry Hill (bottom)</Text>
+        </Tooltip>
+
+        <Tooltip {...props} position="top">
+          <Text>Harry Hill (top)</Text>
+        </Tooltip>
+      </Box>
+    </OverflowHiddenBox>,
+  args: {
+    children: tooltipContent,
+    underline: true
+  },
+  parameters: {
+    layout: 'fullscreen'
+  }
+}`,...(H=(v=a.parameters)==null?void 0:v.docs)==null?void 0:H.source}}};var M,q,B;s.parameters={...s.parameters,docs:{...(M=s.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  args: {
+    children: tooltipContent,
+    underline: true
+  },
+  render: (props: TooltipProps) => <ModalExampleWrapper {...props} />
+}`,...(B=(q=s.parameters)==null?void 0:q.docs)==null?void 0:B.source}}};var A,S,j;c.parameters={...c.parameters,docs:{...(A=c.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  render: props => <Box width="200px">
+      <Text>
+        Marshmallow car insurance, the best insurance in town.{' '}
+        <Tooltip {...props}>Hover here</Tooltip> to learn more as the Policy
+        Holder.
+      </Text>
+      <Text my="12px">
+        Nunc cursus fermentum libero vel iaculis. Praesent vel dignissim purus,
+        a iaculis mauris.{' '}
+        <Tooltip {...props} position="right">
+          In tempor nibh ut elit sollicitudin, vel ultricies elit volutpat.
+          Etiam accumsan tristique ante, at mollis neque laoreet eget.
+        </Tooltip>{' '}
+        Duis rhoncus molestie felis a cursus.
+      </Text>
+    </Box>,
+  args: {
+    inline: true
+  }
+}`,...(j=(S=c.parameters)==null?void 0:S.docs)==null?void 0:j.source}}};var I,R,k;p.parameters={...p.parameters,docs:{...(I=p.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  render: () => <InnerSection>
+      <Box>
+        <Row>
+          <Tooltip variant="fallback" underline content={<TooltipText typo="caption" color="color.surface.base.000">
+                An adjustment changes the <b>cost</b> of the policy. This field
+                should <b>not</b> be used to adjust for invoices created after a
+                failed up front payment.
+                <br />
+                <br />
+                For annual and Premfina policies, use the \`MTA only\` transaction
+                option when issuing an invoice.
+                <br />
+                <br />
+                For instalment policies, use \`spread entire amount\` transaction
+                option when issuing an invoice.
+              </TooltipText>} position="top">
+            <Text typo="body-regular">Adjustment amount</Text>
+            <Text typo="caption">(Adjusts the policy cost)</Text>
+          </Tooltip>
+        </Row>
+      </Box>
+      <Row>
+        <Text typo="body-regular">MTA admin fee</Text>
+      </Row>
+      <Row>
+        <Tooltip underline variant="fallback" content={<TooltipText typo="caption" color="color.surface.base.000">
+              This is the policies total new retained refund after this MTA is
+              complete.
+            </TooltipText>} position="top">
+          <Text>Predicted retained refund</Text>
+        </Tooltip>
+        <Text>£1,345</Text>
+      </Row>
+    </InnerSection>
+}`,...(k=(R=p.parameters)==null?void 0:R.docs)==null?void 0:k.source}}};const Te=["LongTextExample","ReactNodeExample","OverflowExample","ModalExample","InlineText","MTAExample"];export{c as InlineText,l as LongTextExample,p as MTAExample,s as ModalExample,a as OverflowExample,i as ReactNodeExample,Te as __namedExportsOrder,xe as default};
+//# sourceMappingURL=Tooltip.stories-CngXa4nX.js.map
