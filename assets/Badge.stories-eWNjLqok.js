ef76f5702b74645d648899c40a0318e72823c998
ellajay <ella.taylor@marshmallow.co>
2025-11-18 17:17:59 +0000

    Deploying to gh-pages from @ marshmallow-insurance/smores-react@03d7a2e4f4d66539894b6ff9a6e71488ab7e1bc2 🚀

---
+++ assets/Badge.stories-eWNjLqok.js
@@ -0,0 +1,15 @@
+import{B as d,g as m,a as p}from"./exampleImg-0twmClpn.js";import{c as g}from"./colourOptions-CbOJdSSM.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-DbOFzPHX.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./styled-components.browser.esm-Cttt-Tea.js";import"./index-rlYdeEZ-.js";import"./Text-DegufsO8.js";import"./index-BX4QO5EF.js";import"./space-BLajQSvN.js";import"./polished.esm-CYsXSD1d.js";import"./index-L8F7x2IQ.js";import"./colourMap-BqneUWMU.js";import"./variables-DcGMWv_V.js";import"./theme-D-5TnJVB.js";import"./focusOutline-tjk0UCOR.js";const P={title:"Badge",component:d,args:{title:"badge"},argTypes:{borderColour:{control:"select",options:g}}},r={args:{borderColour:"color.surface.base.400",size:p.Lg,src:m().src}},e={args:{borderColour:"color.surface.base.400",size:p.Lg,src:m().src,disabled:!0}};var o,s,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    borderColour: 'color.surface.base.400',
+    size: BadgeSize.Lg,
+    src: getExampleImg().src
+  }
+}`,...(a=(s=r.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};var t,c,i;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    borderColour: 'color.surface.base.400',
+    size: BadgeSize.Lg,
+    src: getExampleImg().src,
+    disabled: true
+  }
+}`,...(i=(c=e.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};const _=["Primary","Disabled"];export{e as Disabled,r as Primary,_ as __namedExportsOrder,P as default};
+//# sourceMappingURL=Badge.stories-eWNjLqok.js.map
---
+++ assets/Box.stories-gMHeIzTk.js
@@ -0,0 +1,18 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{I as c}from"./index-L8F7x2IQ.js";import"./index-rlYdeEZ-.js";import{B as r}from"./index-BX4QO5EF.js";import{T as m}from"./Text-DegufsO8.js";import"./styled-components.browser.esm-Cttt-Tea.js";import"./index-DbOFzPHX.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./colourMap-BqneUWMU.js";import"./variables-DcGMWv_V.js";import"./space-BLajQSvN.js";import"./polished.esm-CYsXSD1d.js";import"./theme-D-5TnJVB.js";import"./focusOutline-tjk0UCOR.js";const S={title:"Box",component:r,args:{flex:!0,direction:"row",justifyContent:"flex-start",alignItems:"flex-start",m:"16px",p:"16px"},argTypes:{as:{table:{disable:!0}},forwardedAs:{table:{disable:!0}}}},s={render:x=>e.jsx(r,{...x,children:e.jsx(m,{children:"Some lovely content."})})},t={render:()=>e.jsxs(r,{flex:!0,direction:{small:"column",medium:"row"},gap:{small:"12px",medium:"16px"},children:[e.jsx(o,{}),e.jsx(o,{}),e.jsx(o,{})]})},o=()=>e.jsxs(r,{px:{small:"12px",medium:"24px"},pt:{small:"8px",medium:"16px"},pb:{small:"24px",medium:"32px"},style:{backgroundColor:"#FBF8F5",borderRadius:12},children:[e.jsxs(r,{flex:!0,direction:{small:"column",medium:"row"},gap:{small:{custom:2},medium:"8px"},alignItems:{medium:"center"},mb:"8px",children:[e.jsx(c,{render:"info",size:24}),e.jsx(m,{typo:"heading-small",children:"Card content."})]}),e.jsx(m,{color:"sesame",children:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."})]});var n,a,i;s.parameters={...s.parameters,docs:{...(n=s.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: args => <Box {...args}>
+      <Text>Some lovely content.</Text>
+    </Box>
+}`,...(i=(a=s.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};var p,l,d;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: () => <Box flex direction={{
+    small: 'column',
+    medium: 'row'
+  }} gap={{
+    small: '12px',
+    medium: '16px'
+  }}>
+      <Card />
+      <Card />
+      <Card />
+    </Box>
+}`,...(d=(l=t.parameters)==null?void 0:l.docs)==null?void 0:d.source}}};const F=["Default","ResponsiveProps"];export{s as Default,t as ResponsiveProps,F as __namedExportsOrder,S as default};
+//# sourceMappingURL=Box.stories-gMHeIzTk.js.map
