b54e7029cc8566a3000ec41831ec07f1647a2cf7
DeadEnglish <git@liamcanetti.co.uk>
2026-03-04 13:52:26 +0000

    Deploying to gh-pages from @ marshmallow-insurance/smores-react@9088075691916ea9cc735c83a4956a0da58f959e 🚀

---
+++ assets/Badge.stories-C0fEfMha.js
@@ -0,0 +1,15 @@
+import{c as a}from"./colourOptions-DPnwkmSQ.js";import{B as o,a as t,g as s}from"./exampleImg-CMnummk_.js";import"./colourMap-Cu7Eftdi.js";import"./iframe-ByKebmUz.js";import"./preload-helper-PPVm8Dsz.js";import"./getFromObject-CS2Xcxjx.js";import"./index-BqbiszIm.js";import"./Text-SzuZYTg9.js";import"./index-DYu6EHJk.js";import"./space-qkSJaf43.js";import"./polished.esm-CYsXSD1d.js";import"./index-OtOh3E85.js";import"./focusOutline-DyfplWfm.js";import"./shared.styles-njA0natf.js";const C={title:"Badge",component:t,args:{title:"badge"},argTypes:{borderColour:{control:"select",options:a}}},r={args:{borderColour:"color.surface.base.400",size:o.Lg,src:s().src}},e={args:{borderColour:"color.surface.base.400",size:o.Lg,src:s().src,disabled:!0}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    borderColour: 'color.surface.base.400',
+    size: BadgeSize.Lg,
+    src: getExampleImg().src
+  }
+}`,...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    borderColour: 'color.surface.base.400',
+    size: BadgeSize.Lg,
+    src: getExampleImg().src,
+    disabled: true
+  }
+}`,...e.parameters?.docs?.source}}};const S=["Primary","Disabled"];export{e as Disabled,r as Primary,S as __namedExportsOrder,C as default};
+//# sourceMappingURL=Badge.stories-C0fEfMha.js.map
